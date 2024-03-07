541fc964ba931a791599735d3a40cfdc56acd23c
Yutao Xu <yutao.xu@intel.com>
2024-03-07 18:01:55 +0800

    Create UnaryOps.cpp

---
+++ src/aten/UnaryOps.cpp
@@ -0,0 +1,23 @@
+#include <ATen/core/Tensor.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/ScalarOps.h>
+#include <torch/library.h>
+
+#include <aten/sycl/UnaryKernels.h>
+#include <iostream>
+
+namespace at {
+namespace native {
+namespace xpu {
+
+Tensor & abs_out(const Tensor & self, Tensor & out) {
+    auto iter = TensorIterator::unary_op(out, self);
+    native::xpu::abs_kernel(iter);
+    return out;
+}
+
+TORCH_LIBRARY_IMPL(aten, XPU, m) {
+  m.impl(TORCH_SELECTIVE_NAME("aten::abs.out"), TORCH_FN(abs_out));
+}
+
+}}} // at::native::xpu
