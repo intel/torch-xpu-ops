258516391fdc08b218ff9619f0c93e0839f57e49
Yutao Xu <yutao.xu@intel.com>
2024-03-07 18:07:10 +0800

    Create test_resize.py

---
+++ test/python/test_resize.py
@@ -0,0 +1,20 @@
+import torch
+from torch.testing._internal.common_utils import TestCase
+
+cpu_device = torch.device("cpu")
+dpcpp_device = torch.device("xpu")
+
+
+class TestNNMethod(TestCase):
+    def test_resize(self, dtype=torch.float):
+        x = torch.ones([2, 2, 4, 3], device=dpcpp_device, dtype=dtype)
+        x.resize_(1, 2, 3, 4)
+
+        y = torch.ones([2, 2, 4, 3], device=cpu_device, dtype=dtype)
+        y.resize_(1, 2, 3, 4)
+
+        print("dpcpp: ")
+        print(x.to("cpu"))
+        print("cpu: ")
+        print(y)
+        self.assertEqual(y, x.cpu())
