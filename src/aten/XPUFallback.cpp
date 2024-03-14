a5f8aab547df2e6fc6f57ffc2d72595de229275b
xinan.lin <xinan.lin@intel.com>
2024-03-13 18:35:29 -0700

    code format

--- src/aten/XPUFallback.cpp
+++ src/aten/XPUFallback.cpp
@@ -1,25 +1,23 @@
 #include <ATen/native/CPUFallback.h>
-#include <ATen/core/Tensor.h>
-#include <ATen/native/TensorIterator.h>
-#include <ATen/ScalarOps.h>
-#include <torch/library.h>
 
-#include <aten/sycl/UnaryKernels.h>
 namespace at {
 
-
-
-static void xpu_fallback(const c10::OperatorHandle& op, torch::jit::Stack* stack) {
-  TORCH_WARN_ONCE("The operator '",
-                  op.schema().operator_name(),
-                  "' is not currently supported ",
-                  "on the XPU backend and will fall back to run on the CPU.",
-                  " This may have performance implications.");
+static void xpu_fallback(
+    const c10::OperatorHandle& op,
+    torch::jit::Stack* stack) {
+  TORCH_WARN_ONCE(
+      "The operator '",
+      op.schema().operator_name(),
+      "' is not currently supported ",
+      "on the XPU backend and will fall back to run on the CPU.",
+      " This may have performance implications.");
 
   native::cpu_fallback(op, stack);
 }
 
-static void xpu_error_fallback(const c10::OperatorHandle& op, torch::jit::Stack* stack) {
+static void xpu_error_fallback(
+    const c10::OperatorHandle& op,
+    torch::jit::Stack* stack) {
   TORCH_CHECK_NOT_IMPLEMENTED(
       false,
       "The operator '",
@@ -32,23 +30,23 @@ static void xpu_error_fallback(const c10::OperatorHandle& op, torch::jit::Stack*
       "on XPU.");
 }
 
-
-
 TORCH_LIBRARY_IMPL(_, XPU, m) {
-  static const char* enable_xpu_fallback = getenv("PYTORCH_ENABLE_XPU_FALLBACK");
+  static const char* enable_xpu_fallback =
+      getenv("PYTORCH_ENABLE_XPU_FALLBACK");
   if (!enable_xpu_fallback || std::stoi(enable_xpu_fallback) == 0) {
-    m.fallback(torch::CppFunction::makeFromBoxedFunction<&xpu_error_fallback>());
+    m.fallback(
+        torch::CppFunction::makeFromBoxedFunction<&xpu_error_fallback>());
   } else {
     m.fallback(torch::CppFunction::makeFromBoxedFunction<&xpu_fallback>());
   }
 }
 
 TORCH_LIBRARY_IMPL(aten, XPU, m) {
-  // These ops are not supported via XPU backend currently, and we fallback to run on CPU.
-  // For the rest of unsupported ops the user needs to pass 'PYTORCH_ENABLE_XPU_FALLBACK=1'
-  // to fallback on CPU, otherwise we will error out.
-  // m.impl("div.Scalar", torch::CppFunction::makeFromBoxedFunction<&xpu_fallback>());
+  // These ops are not supported via XPU backend currently, and we fallback to
+  // run on CPU. For the rest of unsupported ops the user needs to pass
+  // 'PYTORCH_ENABLE_XPU_FALLBACK=1' to fallback on CPU, otherwise we will error
+  // out. m.impl("div.Scalar",
+  // torch::CppFunction::makeFromBoxedFunction<&xpu_fallback>());
 }
-
 
 } // namespace at
