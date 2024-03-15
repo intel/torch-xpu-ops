1623a8a7971eb6ba16c75d093ba02370db00b883
Yutao Xu <yutao.xu@intel.com>
2024-03-15 16:15:50 +0800

    Create BinaryBitwiseOpsKernels.h

---
+++ src/aten/sycl/BinaryBitwiseOpsKernels.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <ATen/native/TensorIterator.h>
+
+namespace at {
+namespace native {
+namespace xpu {
+
+void bitwise_and_kernel(TensorIteratorBase& iter);
+
+void bitwise_or_kernel(TensorIteratorBase& iter);
+
+void bitwise_xor_kernel(TensorIteratorBase& iter);
+
+void bitwise_not_kernel(TensorIteratorBase& iter);
+
+} // namespace xpu
+} // namespace native
+} // namespace at
