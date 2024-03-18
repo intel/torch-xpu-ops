833702fc634c504c3598b45360b288ba92bd5602
Yutao Xu <yutao.xu@intel.com>
2024-03-18 14:33:08 +0800

    Create IndexingOpsKernels.cpp

---
+++ src/aten/sycl/IndexingOpsKernels.cpp
@@ -0,0 +1,42 @@
+#include <ATen/ATen.h>
+#include <ATen/AccumulateType.h>
+#include <ATen/Dispatch.h>
+#include <ATen/native/TensorIterator.h>
+#include <aten/sycl/Loops.h>
+#include <comm/SYCLContext.h>
+
+namespace at {
+namespace native {
+namespace xpu {
+
+template <typename scalar_t>
+struct MaskedFillFunctor {
+  scalar_t operator()(scalar_t self, bool mask) const {
+    if (mask) {
+      return value_;
+    }
+    return self;
+  }
+  MaskedFillFunctor(scalar_t value) : value_(value) {}
+
+ private:
+  scalar_t value_;
+};
+
+void masked_fill_kernel(TensorIterator& iter, const Scalar& value) {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND4(
+      kBool,
+      kHalf,
+      kBFloat16,
+      kComplexHalf,
+      iter.common_dtype(),
+      "masked_fill_",
+      [&]() {
+        const auto value_ = value.to<scalar_t>();
+        gpu_kernel(iter, MaskedFillFunctor<scalar_t>(value_));
+      });
+}
+
+} // namespace xpu
+} // namespace native
+} // namespace at
