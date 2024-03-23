afd4258a45a414f90c366756b8b596511a2fd314
Feng Yuan <feng1.yuan@intel.com>
2024-03-23 18:08:41 +0800

    Support deterministic for empty and empyt_strided
    Signed-off-by: Feng Yuan <feng1.yuan@intel.com>

--- src/aten/TensorFactories.cpp
+++ src/aten/TensorFactories.cpp
@@ -29,11 +29,10 @@ Tensor empty_xpu(
       pin_memory_opt,
       memory_format_opt);
   // See Note [Enabling Deterministic Operations]
-  TORCH_CHECK(
-      !(C10_UNLIKELY(
-          at::globalContext().deterministicAlgorithms() &&
-          at::globalContext().deterministicFillUninitializedMemory())),
-      "XPU backend doesn't support deterministic implementation for empty ...")
+  if (C10_UNLIKELY(at::globalContext().deterministicAlgorithms()
+      && at::globalContext().deterministicFillUninitializedMemory())) {
+    fill_empty_deterministic_(result);
+  }
   return result;
 }
 
@@ -47,11 +46,10 @@ Tensor empty_strided_xpu(
   Tensor result = at::detail::empty_strided_xpu(
       size, stride, dtype_opt, layout_opt, device_opt, pin_memory_opt);
   // See Note [Enabling Deterministic Operations]
-  TORCH_CHECK(
-      !(C10_UNLIKELY(
-          at::globalContext().deterministicAlgorithms() &&
-          at::globalContext().deterministicFillUninitializedMemory())),
-      "XPU backend doesn't support deterministic implementation for empty_strided ...")
+  if (C10_UNLIKELY(at::globalContext().deterministicAlgorithms()
+      && at::globalContext().deterministicFillUninitializedMemory())) {
+    fill_empty_deterministic_(result);
+  }
   return result;
 }
 
