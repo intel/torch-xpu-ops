5a12f5d21c700f22d3fd6196fd233d3191f4c7c9
Yutao Xu <yutao.xu@intel.com>
2024-04-02 10:01:52 +0800

    Update Pow.cpp

--- src/aten/Pow.cpp
+++ src/aten/Pow.cpp
@@ -7,7 +7,7 @@
 
 namespace at {
 
-TensorIterator pow_Tensor_Tensor_meta(
+TensorIterator pow_tensor_tensor_meta(
     const Tensor& base,
     const Tensor& exp,
     Tensor& out) {
@@ -16,7 +16,7 @@ TensorIterator pow_Tensor_Tensor_meta(
   return iter;
 }
 
-TensorIterator pow_Tensor_Scalar_meta(
+TensorIterator pow_tensor_scalar_meta(
     const Tensor& base,
     const Scalar& exp,
     Tensor& out) {
@@ -35,13 +35,13 @@ TensorIterator pow_Tensor_Scalar_meta(
 
 Tensor XPUNativeFunctions::pow(const Tensor& self, const Tensor& exponent) {
   Tensor out;
-  auto iter = pow_Tensor_Tensor_meta(self, exponent, out);
+  auto iter = pow_tensor_tensor_meta(self, exponent, out);
   native::xpu::pow_tensor_tensor_kernel(iter);
   return iter.output();
 }
 
 Tensor& XPUNativeFunctions::pow_(Tensor& self, const Tensor& exponent) {
-  auto iter = pow_Tensor_Tensor_meta(self, exponent, self);
+  auto iter = pow_tensor_tensor_meta(self, exponent, self);
   native::xpu::pow_tensor_tensor_kernel(iter);
   return self;
 }
@@ -50,20 +50,20 @@ Tensor& XPUNativeFunctions::pow_out(
     const Tensor& base,
     const Tensor& exp,
     Tensor& out) {
-  auto iter = pow_Tensor_Tensor_meta(base, exp, out);
+  auto iter = pow_tensor_tensor_meta(base, exp, out);
   native::xpu::pow_tensor_tensor_kernel(iter);
   return out;
 }
 
 Tensor XPUNativeFunctions::pow(const Tensor& self, const Scalar& exponent) {
   Tensor out;
-  auto iter = pow_Tensor_Scalar_meta(self, exponent, out);
+  auto iter = pow_tensor_scalar_meta(self, exponent, out);
   native::xpu::pow_tensor_scalar_kernel(iter, exponent);
   return iter.output();
 }
 
 Tensor& XPUNativeFunctions::pow_(Tensor& self, const Scalar& exponent) {
-  auto iter = pow_Tensor_Scalar_meta(self, exponent, self);
+  auto iter = pow_tensor_scalar_meta(self, exponent, self);
   if (exponent.equal(0.0) || exponent.equal(false)) {
     self.fill_(1);
   } else if (exponent.equal(1.0) || exponent.equal(true)) {
@@ -77,7 +77,7 @@ Tensor& XPUNativeFunctions::pow_out(
     const Tensor& self,
     const Scalar& exponent,
     Tensor& out) {
-  auto iter = pow_Tensor_Scalar_meta(self, exponent, out);
+  auto iter = pow_tensor_scalar_meta(self, exponent, out);
   if (exponent.equal(0.0) || exponent.equal(false)) {
     out.fill_(1);
   } else if (exponent.equal(1.0) || exponent.equal(true)) {
