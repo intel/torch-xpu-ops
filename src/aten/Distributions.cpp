617961d8eb25e657121cf81bb2a3f6f6994707cc
xytintel <yutao.xu@intel.com>
2024-04-03 09:02:58 +0000

    reuse native::templates

--- src/aten/Distributions.cpp
+++ src/aten/Distributions.cpp
@@ -4,6 +4,7 @@
 #include <ATen/ScalarOps.h>
 #include <ATen/XPUNativeFunctions.h>
 #include <ATen/core/Tensor.h>
+#include <ATen/native/DistributionTemplates.h>
 #include <ATen/native/TensorIterator.h>
 #include <aten/sycl/DistributionKernels.h>
 
@@ -48,7 +49,7 @@ Tensor& XPUNativeFunctions::uniform_(
         at::ScalarType::Half,
         at::ScalarType::BFloat16,
         self.scalar_type(),
-        "check_uniform_bounds",
+        "check_uniform_bounds_xpu",
         [&] {
           const auto dtype = self.dtype();
           const auto min =
@@ -116,100 +117,6 @@ Tensor& XPUNativeFunctions::bernoulli_(
         "This warning will become an error in version 1.7 release, please fix the code in advance"); \
   }
 
-static void check_from_to_in_range(
-    int64_t from,
-    int64_t to_inc,
-    caffe2::TypeMeta dtype) {
-  const auto scalar_type = typeMetaToScalarType(dtype);
-  if (isFloatingType(scalar_type)) {
-    AT_DISPATCH_FLOATING_TYPES_AND2(
-        at::ScalarType::Half,
-        at::ScalarType::BFloat16,
-        scalar_type,
-        "check_random_fp_bounds",
-        [&] {
-          const auto min =
-              static_cast<double>(std::numeric_limits<scalar_t>::lowest());
-          const auto max =
-              static_cast<double>(std::numeric_limits<scalar_t>::max());
-          CHECK_OUT_OF_BOUNDS(from, "from", min, max, dtype);
-          CHECK_OUT_OF_BOUNDS(to_inc, "to - 1", min, max, dtype);
-
-          constexpr auto digits = std::numeric_limits<scalar_t>::digits;
-          WARN_OUT_OF_BOUNDS(from, "from", digits, dtype);
-          WARN_OUT_OF_BOUNDS(to_inc, "to - 1", digits, dtype);
-        });
-  } else if (scalar_type == kUInt64) {
-    // When you do a comparison between int64_t and uint64_t, the usual
-    // arithmetic conversions say that the int64_t value is promoted to
-    // unsigned. But this conversion wraps around: if I had -1 as my int64_t,
-    // then it will promote to 0xFFFFFFFFFFFFFFFF in uint64_t. This is never
-    // the right thing to do.
-    CHECK_OUT_OF_BOUNDS(from, "from", 0, INT64_MAX, dtype);
-    CHECK_OUT_OF_BOUNDS(to_inc, "to - 1", 0, INT64_MAX, dtype);
-  } else if (isIntegralType(scalar_type, /*includeBool=*/true)) {
-    AT_DISPATCH_V2(
-        scalar_type,
-        "check_random_integral_bounds",
-        AT_WRAP([&]() {
-          const auto min =
-              static_cast<int64_t>(std::numeric_limits<scalar_t>::lowest());
-          const auto max =
-              static_cast<int64_t>(std::numeric_limits<scalar_t>::max());
-          CHECK_OUT_OF_BOUNDS(from, "from", min, max, dtype);
-          CHECK_OUT_OF_BOUNDS(to_inc, "to - 1", min, max, dtype);
-        }),
-        AT_EXPAND(AT_INTEGRAL_TYPES),
-        kUInt16,
-        kUInt32,
-        kBool);
-  } else {
-    TORCH_CHECK(
-        false,
-        "check_random_bounds handles only integral, floating-point and boolean types");
-  }
-}
-
-template <typename scalar_t>
-int64_t update_from(int64_t from) {
-  static_assert(
-      std::is_floating_point<scalar_t>::value ||
-          std::is_same<scalar_t, at::Half>::value ||
-          std::is_same<scalar_t, at::BFloat16>::value,
-      "scalar_t must be floating-point type");
-  const auto from_plus_1 =
-      static_cast<int64_t>(static_cast<scalar_t>(from + 1));
-  if (from_plus_1 < from) {
-    int64_t from_ = std::abs(from + 1);
-    int n = 0;
-    while (from_ >>= 1)
-      ++n;
-    // NOLINTNEXTLINE(clang-analyzer-core.UndefinedBinaryOperatorResult)
-    from =
-        from_plus_1 + (1LL << (n - std::numeric_limits<scalar_t>::digits + 1));
-  }
-  return from;
-}
-
-template <typename scalar_t>
-int64_t update_to(int64_t to) {
-  static_assert(
-      std::is_floating_point<scalar_t>::value ||
-          std::is_same<scalar_t, at::Half>::value ||
-          std::is_same<scalar_t, at::BFloat16>::value,
-      "scalar_t must be floating-point type");
-  const auto to_minus_1 = static_cast<int64_t>(static_cast<scalar_t>(to - 1));
-  if (to_minus_1 >= to) {
-    int64_t to_ = std::abs(to - 1);
-    int n = 0;
-    while (to_ >>= 1)
-      ++n;
-    // NOLINTNEXTLINE(clang-analyzer-core.UndefinedBinaryOperatorResult)
-    to = to_minus_1 - (1LL << (n - std::numeric_limits<scalar_t>::digits + 1));
-  }
-  return to;
-}
-
 Tensor& XPUNativeFunctions::random_(
     Tensor& self,
     int64_t from,
@@ -233,8 +140,8 @@ Tensor& XPUNativeFunctions::random_(
           self.scalar_type(),
           "random_update_from_to",
           [&] {
-            from = update_from<scalar_t>(from);
-            to = update_to<scalar_t>(to);
+            from = native::templates::update_from<scalar_t>(from);
+            to = native::templates::update_to<scalar_t>(to);
             TORCH_CHECK(
                 from < to,
                 "random_ expects 'from' casted to dtype to be less than 'to' casted to dtype, but got from=",
@@ -243,7 +150,7 @@ Tensor& XPUNativeFunctions::random_(
                 to);
           });
     }
-    check_from_to_in_range(from, to - 1, self.dtype());
+    native::templates::check_from_to_in_range(from, to - 1, self.dtype());
     CHECK_EMPTY_AND_RETURN(self);
     range = static_cast<uint64_t>(to) - static_cast<uint64_t>(from);
     native::xpu::random_from_to_kernel(iter, range, from, generator);
@@ -262,7 +169,7 @@ Tensor& XPUNativeFunctions::random_(
             to_inc = scalar_t_max > std::numeric_limits<int64_t>::max()
                 ? std::numeric_limits<int64_t>::max()
                 : static_cast<int64_t>(scalar_t_max);
-            from = update_from<scalar_t>(from);
+            from = native::templates::update_from<scalar_t>(from);
             TORCH_CHECK(
                 from < to_inc,
                 "random_ expects 'from' casted to dtype to be less than or equal to 'to_inc' casted to dtype, but got from=",
@@ -289,7 +196,7 @@ Tensor& XPUNativeFunctions::random_(
           false,
           "random_from_to_impl handles only integral, floating-point and boolean types");
     }
-    check_from_to_in_range(from, to_inc, self.dtype());
+    native::templates::check_from_to_in_range(from, to_inc, self.dtype());
     CHECK_EMPTY_AND_RETURN(self);
     range = static_cast<uint64_t>(to_inc) - static_cast<uint64_t>(from) + 1;
     native::xpu::random_from_to_kernel(iter, range, from, generator);
