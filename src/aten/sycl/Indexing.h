b4dd1f6456a374c21822f3c04acb97db3ea22d3a
Yutao Xu <yutao.xu@intel.com>
2024-04-09 23:09:40 +0800

    Update Indexing.h

--- src/aten/sycl/Indexing.h
+++ src/aten/sycl/Indexing.h
@@ -6,4 +6,12 @@ namespace at::native::xpu {
 
 void masked_fill_kernel(TensorIterator& iter, const Scalar& value);
 
+void index_add_impl(
+    const Tensor& self,
+    int64_t dim,
+    const Tensor& index,
+    const Tensor& source,
+    const Scalar& alpha,
+    const Tensor& result);
+
 } // namespace at::native::xpu
