9db18bdb43520fc7a77e77363bb1e900bc9c94fc
Yutao Xu <yutao.xu@intel.com>
2024-04-11 17:22:23 +0800

    Create LayerNormKernel.h

---
+++ src/aten/sycl/LayerNormKernel.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <ATen/native/TensorIterator.h>
+
+namespace at {
+namespace native {
+namespace xpu {
+
+std::tuple<Tensor, Tensor, Tensor> native_layer_norm(
+    const Tensor& input,
+    at::IntArrayRef normalized_shape,
+    const c10::optional<at::Tensor>& weight_opt,
+    const c10::optional<at::Tensor>& bias_opt,
+    double epsilon);
+
+std::tuple<Tensor, Tensor, Tensor> native_layer_norm_backward(
+    const Tensor& grad_output,
+    const Tensor& input,
+    at::IntArrayRef normalized_shape,
+    const Tensor& mean,
+    const Tensor& rstd,
+    const c10::optional<at::Tensor>& weight_opt,
+    const c10::optional<at::Tensor>& bias_opt,
+    std::array<bool, 3> grad_input_mask);
+
+} // namespace xpu
+} // namespace native
+} // namespace at
