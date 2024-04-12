e617ab24ac1d0e0e8fb6f908e12bfe75d2d1f493
Yongliang <yongliang.guo@intel.com>
2024-04-12 02:49:46 -0700

    refine

--- src/comm/MemoryFormat.h
+++ src/comm/MemoryFormat.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <ATen/core/Tensor.h>
-// #include <tensor/Tensor.h>
 #include <comm/SYCLContext.h>
 
 // Channels last 1D is only supported by IPEX GPU
@@ -106,61 +105,6 @@ inline at::MemoryFormat suggest_memory_format_sycl(
   return at::MemoryFormat::Contiguous;
 }
 
-// Note: Only all input tensors are channels last format, cat's output would be
-// channels last tensor. This may cause perf issue in the future.
-inline c10::MemoryFormat cat_compute_output_memory_format(
-    const MaterializedITensorListRef& tensors,
-    bool channels_last_strides_exact_match = false) {
-  c10::optional<c10::MemoryFormat> format = c10::nullopt;
-  for (auto mt : tensors) {
-    auto t = mt.get();
-    auto f = suggest_memory_format_sycl(t, channels_last_strides_exact_match);
-    if (f == c10::MemoryFormat::Contiguous) {
-      return f;
-    }
-    if (format.has_value() && format.value() != f) {
-      return c10::MemoryFormat::Contiguous;
-    }
-    format = f;
-  }
-  return format.value();
-}
-
-inline Tensor& convert_tensor_to_channels_last_1d(Tensor& t) {
-  const auto ndim = t.ndimension();
-  if (3 != ndim) {
-    return t;
-  }
-
-  if (1 == t.size(0)) {
-    t = t.transpose(1, -1).contiguous().transpose(1, -1);
-    return t;
-  }
-
-  if (1 == t.size(1)) {
-    t = t.as_strided(t.sizes(), {t.size(1) * t.size(-1), 1, t.size(1)});
-    return t;
-  }
-
-  t = t.view({t.size(0), t.size(1), 1, t.size(2)});
-  t = t.contiguous(at::MemoryFormat::ChannelsLast);
-  t = t.view({t.size(0), t.size(1), t.size(3)});
-  return t;
-}
-
-inline Tensor empty_like_sycl(
-    const Tensor& self,
-    const TensorOptions& options,
-    c10::optional<c10::MemoryFormat> optional_memory_format) {
-  auto ndim = self.ndimension();
-  if (3 != ndim || CHANNELSLAST1D_SYCL != optional_memory_format) {
-    return empty_like(self, options, optional_memory_format);
-  }
-  // ipex gpu channels last 1d
-  Tensor tmp = empty_like(self);
-  return convert_tensor_to_channels_last_1d(tmp);
-}
-
 inline bool is_channels_last(at::MemoryFormat fmt) {
   if (
 #ifdef USE_CHANNELS_LAST_1D
@@ -185,24 +129,6 @@ inline bool is_smf_channels_last(const Tensor& t) {
   }
 
   return is_channels_last(suggest_memory_format_sycl(t));
-}
-
-inline MemoryFormat get_cl_tag_by_ndim(const int64_t ndim) {
-  TORCH_CHECK(
-      3 == ndim || 4 == ndim || 5 == ndim,
-      "ndim must be 3, 4 or 5 when get cl tag");
-  if (3 == ndim) {
-#ifdef USE_CHANNELS_LAST_1D
-    return CHANNELSLAST1D_SYCL;
-#else
-    // if doesn't enable channels last 1d
-    return at::MemoryFormat::Contiguous;
-#endif
-  } else if (5 == ndim) {
-    return at::MemoryFormat::ChannelsLast3d;
-  } else {
-    return at::MemoryFormat::ChannelsLast;
-  }
 }
 
 } // namespace sycl
