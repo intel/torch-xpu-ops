a74e590b3fdb0b6f82203b8ac55147b4a8f5c454
Yutao Xu <yutao.xu@intel.com>
2024-04-16 10:03:50 +0800

    Update XPUScalar.cpp

--- src/aten/XPUScalar.cpp
+++ src/aten/XPUScalar.cpp
@@ -1,15 +1,8 @@
 #define TORCH_ASSERT_ONLY_METHOD_OPERATORS
 #include <ATen/Dispatch_v2.h>
 #include <ATen/EmptyTensor.h>
-#include <ATen/core/Tensor.h>
-
-#ifndef AT_PER_OPERATOR_HEADERS
-#include <ATen/NativeFunctions.h>
-#else
-#include <ATen/ops/_local_scalar_dense_native.h>
-#endif
-
 #include <ATen/XPUNativeFunctions.h>
+#include <ATen/core/Tensor.h>
 #include <comm/SYCLContext.h>
 
 namespace at {
@@ -20,23 +13,21 @@ Scalar XPUNativeFunctions::_local_scalar_dense(const Tensor& self) {
       self.scalar_type(),
       "_local_scalar_dense",
       AT_WRAP([&] {
-        // Create pinned memory for the scalar value to avoid implicit
-        // locking/sync in xpu library due to pageable memory
         auto value = at::detail::empty_cpu(
             {1}, /* size */
             c10::CppTypeToScalarType<scalar_t>(), /* dtype */
             c10::nullopt, /* layout */
             c10::nullopt, /* device */
-            true, /* pin_memory */
+            false, /* pin_memory */
             c10::nullopt /* memory format */
         );
 
         auto queue = at::xpu::getCurrentSYCLQueue();
-        queue.memcpy(
+        auto e = queue.memcpy(
             (void*)value.const_data_ptr<scalar_t>(),
             self.const_data_ptr<scalar_t>(),
             sizeof(scalar_t));
-        queue.wait();
+        e.wait();
 
         r = Scalar(*value.const_data_ptr<scalar_t>());
       }),
