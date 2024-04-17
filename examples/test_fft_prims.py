0430b10aff2b648c276934e7f85120c25f877662
Xu Yutao <yutao.xu@intel.com>
2024-04-17 02:18:32 +0000

    Enable fft prims

---
+++ examples/test_fft_prims.py
@@ -0,0 +1,75 @@
+import torch
+from torch.testing._internal.common_utils import TestCase
+import itertools
+
+device = torch.device("xpu")
+
+class TestTorchMethod(TestCase):
+    def test_fft_r2c(self):
+        test_dtypes = [torch.float, torch.double]
+
+        def test_one_fft_r2c(dtype):
+            size = (2, 3, 40, 40)
+            input_ref = torch.randn(size, dtype=dtype, device="cpu")
+            input = input_ref.to(device)
+
+            res = torch._fft_r2c(input, (2, 3), normalization=0, onesided=True)
+            res_ref = torch._fft_r2c(input_ref, (2, 3), normalization=0, onesided=True)
+            self.assertEqual(res, res_ref)
+
+            out = torch.empty_like(res)
+            torch._fft_r2c(input, (2, 3), normalization=0, onesided=True, out=out)
+            out_ref = torch.empty_like(res_ref)
+            torch._fft_r2c(input_ref, (2, 3), normalization=0, onesided=True, out=out_ref)
+            self.assertEqual(out, out_ref)
+
+        for dtype in test_dtypes:
+            test_one_fft_r2c(dtype)
+
+    def test_fft_c2r(self):
+        test_dtypes = [torch.cfloat, torch.cdouble]        
+
+        def test_one_fft_c2r(dtype):
+            size = (2, 3, 40, 40)
+
+            input_ref = torch.randn(size, dtype=dtype, device="cpu")
+            input = input_ref.to(device)
+
+            res = torch._fft_c2r(input, (2, 3), normalization=0, last_dim_size=0)
+            res_ref = torch._fft_c2r(input_ref, (2, 3), normalization=0, last_dim_size=0)
+            self.assertEqual(res, res_ref)
+
+            out = torch.empty_like(res)
+            torch._fft_c2r(input, (2, 3), normalization=0, last_dim_size=0, out=out)
+            out_ref = torch.empty_like(res_ref)
+            torch._fft_c2r(input_ref, (2, 3), normalization=0, last_dim_size=0, out=out_ref)
+            self.assertEqual(out, out_ref)
+        
+        for dtype in test_dtypes:
+            test_one_fft_c2r(dtype)
+
+    def test_fft_c2c(self):
+        test_dtypes = [torch.cfloat, torch.cdouble]
+
+        def test_one_fft_c2c(dtype):
+            for fwd in [True, False] :
+                
+                size = (2, 3, 40, 40)
+
+                input_ref = torch.randn(size, dtype=dtype, device="cpu")
+                input = input_ref.to(device)
+
+                res = torch._fft_c2c(input, (2, 3), normalization=0, forward=fwd)
+                res_ref = torch._fft_c2c(input_ref, (2, 3), normalization=0, forward=fwd)
+                self.assertEqual(res, res_ref)
+
+                out = torch.empty_like(res)
+                torch._fft_c2c(input, (2, 3), normalization=0, forward=fwd, out=out)
+                out_ref = torch.empty_like(res_ref)
+                torch._fft_c2c(input_ref, (2, 3), normalization=0, forward=fwd, out=out_ref)
+                self.assertEqual(out, out_ref)
+        
+        for dtype in test_dtypes:
+            test_one_fft_c2c(dtype)
+
+        
---
+++ src/aten/SpectralOps.cpp
@@ -0,0 +1,76 @@
+
+#include <ATen/ATen.h>
+#include <ATen/XPUNativeFunctions.h>
+#include <ATen/core/Tensor.h>
+
+namespace at {
+
+Tensor& XPUNativeFunctions::_fft_r2c_out(
+    const Tensor& self,
+    IntArrayRef dim,
+    int64_t normalization,
+    bool onesided,
+    Tensor& out) {
+  Tensor out_cpu = out.to(Device(at::kCPU));
+  at::_fft_r2c_out(
+      out_cpu, self.to(Device(at::kCPU)), dim, normalization, onesided);
+  out.copy_(out_cpu);
+  return out;
+}
+
+Tensor& XPUNativeFunctions::_fft_c2r_out(
+    const Tensor& self,
+    IntArrayRef dim,
+    int64_t normalization,
+    int64_t last_dim_size,
+    Tensor& out) {
+  Tensor out_cpu = out.to(Device(at::kCPU));
+  Tensor self_cpu = self.to(Device(at::kCPU));
+  at::_fft_c2r_out(out_cpu, self_cpu, dim, normalization, last_dim_size);
+  out.copy_(out_cpu);
+  return out;
+}
+
+Tensor& XPUNativeFunctions::_fft_c2c_out(
+    const Tensor& self,
+    IntArrayRef dim,
+    int64_t normalization,
+    bool forward,
+    Tensor& out) {
+  Tensor out_cpu = out.to(Device(at::kCPU));
+  Tensor self_cpu = self.to(Device(at::kCPU));
+  at::_fft_c2c_out(out_cpu, self_cpu, dim, normalization, forward);
+  return out.copy_(out_cpu);
+}
+
+Tensor XPUNativeFunctions::_fft_c2r(
+    const Tensor& self,
+    IntArrayRef dim,
+    int64_t normalization,
+    int64_t last_dim_size) {
+  Tensor out_cpu = at::_fft_c2r(
+      self.to(Device(at::kCPU)), dim, normalization, last_dim_size);
+  return out_cpu.to(Device(at::kXPU));
+}
+
+Tensor XPUNativeFunctions::_fft_r2c(
+    const Tensor& self,
+    IntArrayRef dim,
+    int64_t normalization,
+    bool onesided) {
+  Tensor out_cpu =
+      at::_fft_r2c(self.to(Device(at::kCPU)), dim, normalization, onesided);
+  return out_cpu.to(Device(at::kXPU));
+}
+
+Tensor XPUNativeFunctions::_fft_c2c(
+    const Tensor& self,
+    IntArrayRef dim,
+    int64_t normalization,
+    bool forward) {
+  Tensor out_cpu =
+      at::_fft_c2c(self.to(Device(at::kCPU)), dim, normalization, forward);
+  return out_cpu.to(Device(at::kXPU));
+}
+
+} // namespace at
