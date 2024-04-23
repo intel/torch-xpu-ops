62531d2018a5af4db5804e9f28c9ba65b13cec89
Yongliang <yongliang.guo@intel.com>
2024-04-22 22:39:11 -0700

    refine

--- src/aten/BatchLinearAlgebra.cpp
+++ src/aten/BatchLinearAlgebra.cpp
@@ -15,4 +15,17 @@ Tensor& XPUNativeFunctions::tril_out(
   return out;
 }
 
+Tensor XPUNativeFunctions::tril(const Tensor& self, int64_t diagonal) {
+  Tensor self_cpu = self.to(Device(kCPU));
+  Tensor out = at::tril(self_cpu, diagonal);
+  return out.to(Device(kXPU));
+}
+
+Tensor& XPUNativeFunctions::tril_(Tensor& self, int64_t diagonal) {
+  Tensor self_cpu = self.to(Device(kCPU));
+  Tensor out_cpu = at::tril_out(self_cpu, self_cpu, diagonal);
+  self.copy_(out_cpu);
+  return self;
+}
+
 } // namespace at
--- src/aten/SoftMax.cpp
+++ src/aten/SoftMax.cpp
@@ -4,29 +4,14 @@
 
 namespace at {
 
-// Following three functions are currently not use by sdpa
-// TODO: Do we need to register them as well?
-// Tensor softmax(const Tensor& input_, const int64_t dim_,
-// c10::optional<ScalarType> dtype) {
-//   Tensor output_ = softmax(input_.to(Device(kCPU)), dim_, dtype);
-//   return output_.to(Device(kXPU));
-// }
-
-// Tensor& softmax_out(
-//     const Tensor& input_,
-//     const int64_t dim_,
-//     c10::optional<ScalarType> dtype,
-//     Tensor& output_) {
-//   Tensor output_cpu = output_.to(Device(kCPU));
-//   at::softmax_out(input_.to(Device(kCPU)), dim_, dtype, output_cpu);
-//   output_.copy_(output_cpu);
-//   return output_;
-// }
-
-// Tensor _softmax(const Tensor & self, int64_t dim, bool half_to_float) {
-//   Tensor out = at::_softmax(self.to(Device(kCPU)), dim, half_to_float);
-//   return out.to(Device(kXPU));
-// }
+Tensor XPUNativeFunctions::_softmax(
+    const Tensor& self,
+    int64_t dim,
+    bool half_to_float) {
+  Tensor self_cpu = self.to(Device(kCPU));
+  Tensor out = at::_softmax(self_cpu, dim, half_to_float);
+  return out.to(Device(kXPU));
+}
 
 Tensor& XPUNativeFunctions::_softmax_out(
     const Tensor& self,
@@ -40,19 +25,17 @@ Tensor& XPUNativeFunctions::_softmax_out(
   return out;
 }
 
-// TODO: This one too
-// Tensor XPUNativeFunctions::_softmax_backward_data(
-//     const Tensor & grad_output,
-//     const Tensor & output,
-//     int64_t dim,
-//     ScalarType input_dtype) {
-//   Tensor grad_input = at::_softmax_backward_data(
-//       grad_output.to(Device(kCPU)),
-//       output.to(Device(kCPU)),
-//       dim,
-//       input_dtype);
-//   return grad_input.to(Device(kCPU));
-// }
+Tensor XPUNativeFunctions::_softmax_backward_data(
+    const Tensor& grad_output,
+    const Tensor& output,
+    int64_t dim,
+    ScalarType input_dtype) {
+  Tensor grad_output_cpu = grad_output.to(Device(kCPU));
+  Tensor output_cpu = output.to(Device(kCPU));
+  Tensor grad_input =
+      at::_softmax_backward_data(grad_output_cpu, output_cpu, dim, input_dtype);
+  return grad_input.to(Device(kXPU));
+}
 
 Tensor& XPUNativeFunctions::_softmax_backward_data_out(
     const Tensor& grad_output,
