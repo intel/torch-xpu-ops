6a6a6118f09a242ad3432b4c75a65eae13316d22
Yongliang <yongliang.guo@intel.com>
2024-04-26 00:17:53 -0700

    refine

--- src/aten/SoftMax.cpp
+++ src/aten/SoftMax.cpp
@@ -2,13 +2,11 @@
 #include <ATen/XPUNativeFunctions.h>
 #include <ATen/core/Tensor.h>
 #include <ATen/core/op_registration/adaption.h>
-// #include <c10/core/SymIntArrayRef.h>
 #include <comm/RegisterUtils.h>
-// #include <torch/library.h>
 
 namespace at {
 
-void torch_meta_func__softmax(
+void meta_func__softmax(
     Tensor& out,
     const Tensor& input,
     const int64_t dim,
@@ -26,8 +24,11 @@ void torch_meta_func__softmax(
   TORCH_CHECK(
       dim_ >= 0 && dim_ < input_dim,
       "dim must be non-negative and less than input dimensions");
-
-  at::xpu::resize_out(out, input.sizes(), {}, output_options);
+  if (out.defined()) {
+    at::xpu::resize_out(out, input.sizes(), {}, output_options);
+  } else {
+    out = at::xpu::create_out(input.sizes(), {}, output_options);
+  }
 }
 
 Tensor& XPUNativeFunctions::_softmax_out(
@@ -40,17 +41,31 @@ Tensor& XPUNativeFunctions::_softmax_out(
       common_device, out, "xpu::_softmax_out", "out");
   c10::impl::check_and_update_common_device(
       common_device, self, "xpu::_softmax_out", "self");
-  torch_meta_func__softmax(out, self, dim, half_to_float);
+  meta_func__softmax(out, self, dim, half_to_float);
   Tensor out_cpu = out.to(Device(kCPU));
   Tensor self_cpu = self.to(Device(kCPU));
-  // std::cout << self_cpu.data() << std::endl;
   at::_softmax_out(out_cpu, self_cpu, dim, half_to_float);
-  // std::cout << out_cpu.data() << std::endl;
   out.copy_(out_cpu);
   return out;
 }
 
-void torch_meta_func__softmax_backward_data(
+Tensor XPUNativeFunctions::_softmax(
+    const Tensor& self,
+    int64_t dim,
+    bool half_to_float) {
+  std::optional<Device> common_device = std::nullopt;
+  c10::impl::check_and_update_common_device(
+      common_device, self, "xpu::_softmax_out", "self");
+  Tensor out;
+  meta_func__softmax(out, self, dim, half_to_float);
+  Tensor out_cpu = out.to(Device(kCPU));
+  Tensor self_cpu = self.to(Device(kCPU));
+  at::_softmax_out(out_cpu, self_cpu, dim, half_to_float);
+  out.copy_(out_cpu);
+  return out;
+}
+
+void meta_func__softmax_backward_data(
     const Tensor& grad_output,
     const Tensor& output,
     int64_t dim,
@@ -77,8 +92,13 @@ void torch_meta_func__softmax_backward_data(
   TORCH_CHECK(
       dim_ >= 0 && dim_ < grad_dim,
       "dim must be non-negative and less than input dimensions");
-
-  at::xpu::resize_out(grad_input, grad_output.sizes(), {}, grad_input_options);
+  if (grad_input.defined()) {
+    at::xpu::resize_out(
+        grad_input, grad_output.sizes(), {}, grad_input_options);
+  } else {
+    grad_input =
+        at::xpu::create_out(grad_output.sizes(), {}, grad_input_options);
+  }
 }
 
 Tensor& XPUNativeFunctions::_softmax_backward_data_out(
@@ -100,7 +120,32 @@ Tensor& XPUNativeFunctions::_softmax_backward_data_out(
       "grad_output");
   c10::impl::check_and_update_common_device(
       common_device, output, "xpu::_softmax_backward_data_out", "output");
-  torch_meta_func__softmax_backward_data(
+  meta_func__softmax_backward_data(
+      grad_output, output, dim, input_dtype, grad_input);
+  Tensor grad_output_cpu = grad_output.to(Device(kCPU));
+  Tensor output_cpu = output.to(Device(kCPU));
+  Tensor grad_input_cpu = grad_input.to(Device(kCPU));
+  at::_softmax_backward_data_out(
+      grad_input_cpu, grad_output_cpu, output_cpu, dim, input_dtype);
+  grad_input.copy_(grad_input_cpu);
+  return grad_input;
+}
+
+Tensor XPUNativeFunctions::_softmax_backward_data(
+    const Tensor& grad_output,
+    const Tensor& output,
+    int64_t dim,
+    ScalarType input_dtype) {
+  std::optional<Device> common_device = std::nullopt;
+  c10::impl::check_and_update_common_device(
+      common_device,
+      grad_output,
+      "xpu::_softmax_backward_data_out",
+      "grad_output");
+  c10::impl::check_and_update_common_device(
+      common_device, output, "xpu::_softmax_backward_data_out", "output");
+  Tensor grad_input;
+  meta_func__softmax_backward_data(
       grad_output, output, dim, input_dtype, grad_input);
   Tensor grad_output_cpu = grad_output.to(Device(kCPU));
   Tensor output_cpu = output.to(Device(kCPU));
--- src/aten/TriangularOps.cpp
+++ src/aten/TriangularOps.cpp
@@ -2,16 +2,13 @@
 #include <ATen/XPUNativeFunctions.h>
 #include <ATen/core/Tensor.h>
 #include <ATen/core/op_registration/adaption.h>
-// #include <c10/core/SymIntArrayRef.h>
 #include <comm/RegisterUtils.h>
-// #include <torch/library.h>
 
 namespace at {
 
-void torch_meta_func_tril(const Tensor& self, int64_t k, Tensor& out) {
+void meta_func_tril(const Tensor& self, int64_t k) {
   TORCH_CHECK(
-      self.dim() >= 2, "tril: input tensor must have at least 2 dimensions")
-  at::xpu::resize_out(out, self.sizes(), {}, self.options());
+      self.dim() >= 2, "tril: input tensor must have at least 2 dimensions");
 }
 
 Tensor& XPUNativeFunctions::tril_out(
@@ -23,11 +20,38 @@ Tensor& XPUNativeFunctions::tril_out(
       common_device, out, "xpu::tril_out", "out");
   c10::impl::check_and_update_common_device(
       common_device, self, "xpu::tril_out", "self");
-  torch_meta_func_tril(self, diagonal, out);
+  meta_func_tril(self, diagonal);
+  at::xpu::resize_out(out, self.sizes(), {}, self.options());
   Tensor out_cpu = out.to(Device(kCPU));
   Tensor self_cpu = self.to(Device(kCPU));
   at::tril_out(out_cpu, self_cpu, diagonal);
   out.copy_(out_cpu);
   return out;
+}
+
+Tensor XPUNativeFunctions::tril(const Tensor& self, int64_t diagonal) {
+  std::optional<Device> common_device = std::nullopt;
+  c10::impl::check_and_update_common_device(
+      common_device, self, "xpu::tril", "self");
+  Tensor out;
+  meta_func_tril(self, diagonal);
+  out = at::xpu::create_out(self.sizes(), {}, self.options());
+  Tensor out_cpu = out.to(Device(kCPU));
+  Tensor self_cpu = self.to(Device(kCPU));
+  at::tril_out(out_cpu, self_cpu, diagonal);
+  out.copy_(out_cpu);
+  return out;
+}
+
+Tensor& XPUNativeFunctions::tril_(Tensor& self, int64_t diagonal) {
+  std::optional<Device> common_device = std::nullopt;
+  c10::impl::check_and_update_common_device(
+      common_device, self, "xpu::tril", "self");
+  meta_func_tril(self, diagonal);
+  at::xpu::check_inplace(self, self.sizes(), self.options());
+  Tensor self_cpu = self.to(Device(kCPU));
+  at::tril_out(self_cpu, self_cpu, diagonal);
+  self.copy_(self_cpu);
+  return self;
 }
 } // namespace at
