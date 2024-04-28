34dc57bccd4a8545b232eceff65655655df2e322
Deng, Daisy <daisy.deng@intel.com>
2024-04-27 19:42:16 -0700

    Added fallback test for TestMathBitsi, TestRefsOpsInfo, TestFakeTensor, TestTags with skiplist enabled

--- test/xpu/_test_ops.py
+++ test/xpu/_test_ops.py
@@ -6,9 +6,14 @@ from torch.testing._internal.common_utils import run_tests
 from xpu_test_utils import XPUTestPatch, instantiate_device_type_tests
 
 with XPUTestPatch():
-    from test_ops import TestCommon
+    from test_ops import TestCommon, TestMathBits, TestRefsOpsInfo, TestFakeTensor, TestTags
+
 
 instantiate_device_type_tests(TestCommon, globals(), only_for="xpu")
+instantiate_device_type_tests(TestMathBits, globals(), only_for="xpu")
+instantiate_device_type_tests(TestRefsOpsInfo, globals(), only_for="cpu")
+instantiate_device_type_tests(TestFakeTensor, globals(), only_for="xpu")
+instantiate_device_type_tests(TestTags, globals(), only_for="xpu")
 
 
 if __name__ == "__main__":
