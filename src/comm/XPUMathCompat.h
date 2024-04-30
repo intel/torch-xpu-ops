267c9e9cd1328ea59dbb2ec912288b592c8d38d3
Yutao Xu <yutao.xu@intel.com>
2024-04-30 10:05:38 +0800

    Add aten::native_layer_norm and aten::native_layer_norm_backward (#136)
    e.g. native_layer_norm, native_layer_norm_backward
    
    ---------
    
    Co-authored-by: Feng Yuan <feng1.yuan@intel.com>

--- src/comm/XPUMathCompat.h
+++ src/comm/XPUMathCompat.h
@@ -2,6 +2,7 @@
 
 #include <c10/macros/Macros.h>
 #include <c10/util/Exception.h>
+#include <sycl/sycl.hpp>
 
 #define __MATH_FUNCTIONS_DECL__ static inline
 
@@ -19,6 +20,13 @@ __MATH_FUNCTIONS_DECL__ float tanh(float x) {
 }
 __MATH_FUNCTIONS_DECL__ double tanh(double x) {
   return ::tanh(x);
+}
+
+__MATH_FUNCTIONS_DECL__ float rsqrt(float x) {
+  return sycl::rsqrt(x);
+}
+__MATH_FUNCTIONS_DECL__ double rsqrt(double x) {
+  return sycl::rsqrt(x);
 }
 
 } // namespace c10::xpu::compat
