3332269ed54cecdc4d67277383a0e3f8255e13c5
Cheng Penghui <penghui.cheng@intel.com>
2024-05-13 03:32:51 +0000

    Add UT cases for reductions ops
    Signed-off-by: Cheng Penghui <penghui.cheng@intel.com>

---
+++ test/xpu/test_reductions_xpu.py
@@ -0,0 +1,19 @@
+# Owner(s): ["module: intel"]
+
+from torch.testing._internal.common_device_type import instantiate_device_type_tests
+from torch.testing._internal.common_utils import run_tests
+
+try:
+    from xpu_test_utils import XPUPatchForImport
+except Exception as e:
+    from .xpu_test_utils import XPUPatchForImport
+
+with XPUPatchForImport(False):
+    from test_reductions import TestReductions
+
+instantiate_device_type_tests(TestReductions, globals(), only_for="xpu")
+
+
+
+if __name__ == "__main__":
+    run_tests()
