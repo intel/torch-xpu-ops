c4277c49c9f5055662df6e1df0ef06c24eff174d
Feng Yuan <feng1.yuan@intel.com>
2024-05-18 03:21:44 +0800

    Remove fine gran cases

--- test/xpu/fin_grain/test_ops_xpu.py
+++ test/xpu/fin_grain/test_ops_xpu.py
@@ -127,7 +127,7 @@ _xpu_computation_op_list = [
     "scatter",
     "gather",
     "nn.functional.embedding",
-    "nn.functional.nll_loss",
+    # "nn.functional.nll_loss", # Lack of XPU implementation of aten::nll_loss2d_forward. Will retrieve the case, only if the op is implemented.
 ]
 
 _xpu_computation_ops = [
