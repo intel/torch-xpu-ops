fbe5fe20be0e0b7f1dd9e80837caa6cbbc65b988
Yutao Xu <yutao.xu@intel.com>
2024-05-17 14:01:31 +0800

    Add softmax operators (#174)
    e.g. _softmax, _softmax_backward_data, _log_softmax,
    _log_softmax_backward_data
    
    ---------
    
    Signed-off-by: Feng Yuan <feng1.yuan@intel.com>
    Co-authored-by: Feng Yuan <feng1.yuan@intel.com>

--- src/aten/SoftMax.cpp
+++ src/aten/SoftMax.cpp
@@ -2,15 +2,16 @@
 #include <ATen/XPUNativeFunctions.h>
 #include <ATen/core/Tensor.h>
 #include <ATen/core/op_registration/adaption.h>
+#include <aten/sycl/SoftMaxKernels.h>
 #include <comm/RegisterUtils.h>
 
 namespace at {
 
-void meta_func__softmax(
-    Tensor& out,
+Tensor& _softmax_meta(
     const Tensor& input,
     const int64_t dim,
-    const bool half_to_float) {
+    const bool half_to_float,
+    Tensor& out) {
   int64_t dim_ = maybe_wrap_dim(dim, input.dim());
 
   auto output_options =
@@ -24,11 +25,24 @@ void meta_func__softmax(
   TORCH_CHECK(
       dim_ >= 0 && dim_ < input_dim,
       "dim must be non-negative and less than input dimensions");
+
   if (out.defined()) {
-    at::xpu::resize_out(out, input.sizes(), {}, output_options);
+    xpu::resize_out(out, input.sizes(), {}, output_options);
   } else {
-    out = at::xpu::create_out(input.sizes(), {}, output_options);
+    out = xpu::create_out(input.sizes(), {}, output_options);
   }
+
+  return out;
+}
+
+Tensor XPUNativeFunctions::_softmax(
+    const Tensor& self,
+    int64_t dim,
+    bool half_to_float) {
+  Tensor out;
+  out = _softmax_meta(self, dim, half_to_float, out);
+  native::xpu::_softmax_kernel(self, dim, half_to_float, out);
+  return out;
 }
 
 Tensor& XPUNativeFunctions::_softmax_out(
@@ -38,122 +52,163 @@ Tensor& XPUNativeFunctions::_softmax_out(
     Tensor& out) {
   std::optional<Device> common_device = std::nullopt;
   c10::impl::check_and_update_common_device(
-      common_device, out, "xpu::_softmax_out", "out");
+      common_device, out, "xpu::_softmax_out_out", "out");
   c10::impl::check_and_update_common_device(
-      common_device, self, "xpu::_softmax_out", "self");
-  meta_func__softmax(out, self, dim, half_to_float);
-  Tensor out_cpu = out.to(Device(kCPU));
-  Tensor self_cpu = self.to(Device(kCPU));
-  at::_softmax_out(out_cpu, self_cpu, dim, half_to_float);
-  out.copy_(out_cpu);
-  return out;
+      common_device, self, "xpu::_softmax_out_out", "self");
+  out = _softmax_meta(self, dim, half_to_float, out);
+  return native::xpu::_softmax_kernel(self, dim, half_to_float, out);
 }
 
-Tensor XPUNativeFunctions::_softmax(
+Tensor XPUNativeFunctions::_log_softmax(
     const Tensor& self,
     int64_t dim,
     bool half_to_float) {
-  std::optional<Device> common_device = std::nullopt;
-  c10::impl::check_and_update_common_device(
-      common_device, self, "xpu::_softmax_out", "self");
   Tensor out;
-  meta_func__softmax(out, self, dim, half_to_float);
-  Tensor out_cpu = out.to(Device(kCPU));
-  Tensor self_cpu = self.to(Device(kCPU));
-  at::_softmax_out(out_cpu, self_cpu, dim, half_to_float);
-  out.copy_(out_cpu);
+  out = _softmax_meta(self, dim, half_to_float, out);
+  native::xpu::_log_softmax_kernel(self, dim, half_to_float, out);
   return out;
 }
 
-void meta_func__softmax_backward_data(
-    const Tensor& grad_output,
+Tensor& XPUNativeFunctions::_log_softmax_out(
+    const Tensor& self,
+    int64_t dim,
+    bool half_to_float,
+    Tensor& out) {
+  std::optional<Device> common_device = std::nullopt;
+  c10::impl::check_and_update_common_device(
+      common_device, out, "xpu::_log_softmax_out_out", "out");
+  c10::impl::check_and_update_common_device(
+      common_device, self, "xpu::_log_softmax_out_out", "self");
+  out = _softmax_meta(self, dim, half_to_float, out);
+  return native::xpu::_log_softmax_kernel(self, dim, half_to_float, out);
+}
+
+Tensor& _softmax_backward_data_meta(
+    const Tensor& grad,
     const Tensor& output,
     int64_t dim,
     ScalarType input_dtype,
     Tensor& grad_input) {
-  TensorArg grad_output_arg{grad_output, "grad_output", 1},
-      output_arg{output, "output", 2};
-  checkSameSize("softmax_backward", grad_output_arg, output_arg);
+  TensorArg grad_arg{grad, "grad", 1}, output_arg{output, "output", 2};
+  checkSameSize("softmax_backward", grad_arg, output_arg);
 
-  int64_t dim_ = maybe_wrap_dim(dim, grad_output.dim());
+  int64_t dim_ = maybe_wrap_dim(dim, grad.dim());
 
   auto grad_input_options =
-      grad_output.options().memory_format(LEGACY_CONTIGUOUS_MEMORY_FORMAT);
+      grad.options().memory_format(LEGACY_CONTIGUOUS_MEMORY_FORMAT);
 
-  bool half_to_float = grad_output.scalar_type() != input_dtype;
+  bool half_to_float = grad.scalar_type() != input_dtype;
   if (half_to_float) {
-    if (grad_output.scalar_type() == ScalarType::Float &&
+    if (grad.scalar_type() == ScalarType::Float &&
         input_dtype == ScalarType::Half) {
       grad_input_options = grad_input_options.dtype(ScalarType::Half);
     }
   }
 
-  int64_t grad_dim = grad_output.dim() > 0 ? grad_output.dim() : 1;
+  int64_t grad_dim = grad.dim() > 0 ? grad.dim() : 1;
   TORCH_CHECK(
       dim_ >= 0 && dim_ < grad_dim,
       "dim must be non-negative and less than input dimensions");
+
   if (grad_input.defined()) {
-    at::xpu::resize_out(
-        grad_input, grad_output.sizes(), {}, grad_input_options);
+    xpu::resize_out(grad_input, grad.sizes(), {}, grad_input_options);
   } else {
-    grad_input =
-        at::xpu::create_out(grad_output.sizes(), {}, grad_input_options);
+    grad_input = xpu::create_out(grad.sizes(), {}, grad_input_options);
   }
+
+  return grad_input;
+}
+
+Tensor XPUNativeFunctions::_softmax_backward_data(
+    const at::Tensor& grad_output,
+    const at::Tensor& output,
+    int64_t dim,
+    at::ScalarType input_dtype) {
+  std::optional<Device> common_device = std::nullopt;
+  c10::impl::check_and_update_common_device(
+      common_device, grad_output, "xpu::_softmax_backward_data", "grad_output");
+  c10::impl::check_and_update_common_device(
+      common_device, output, "xpu::_softmax_backward_data", "output");
+  Tensor grad_input;
+  grad_input = _softmax_backward_data_meta(
+      grad_output, output, dim, input_dtype, grad_input);
+  native::xpu::_softmax_backward_kernel(
+      grad_output, output, dim, false, grad_input);
+  return grad_input;
 }
 
 Tensor& XPUNativeFunctions::_softmax_backward_data_out(
-    const Tensor& grad_output,
-    const Tensor& output,
+    const at::Tensor& grad_output,
+    const at::Tensor& output,
     int64_t dim,
-    ScalarType input_dtype,
+    at::ScalarType input_dtype,
     Tensor& grad_input) {
   std::optional<Device> common_device = std::nullopt;
   c10::impl::check_and_update_common_device(
       common_device,
       grad_input,
-      "xpu::_softmax_backward_data_out",
+      "xpu::_softmax_backward_data_out_out",
       "grad_input");
   c10::impl::check_and_update_common_device(
       common_device,
       grad_output,
-      "xpu::_softmax_backward_data_out",
+      "xpu::_softmax_backward_data_out_out",
       "grad_output");
   c10::impl::check_and_update_common_device(
-      common_device, output, "xpu::_softmax_backward_data_out", "output");
-  meta_func__softmax_backward_data(
+      common_device, output, "xpu::_softmax_backward_data_out_out", "output");
+  grad_input = _softmax_backward_data_meta(
       grad_output, output, dim, input_dtype, grad_input);
-  Tensor grad_output_cpu = grad_output.to(Device(kCPU));
-  Tensor output_cpu = output.to(Device(kCPU));
-  Tensor grad_input_cpu = grad_input.to(Device(kCPU));
-  at::_softmax_backward_data_out(
-      grad_input_cpu, grad_output_cpu, output_cpu, dim, input_dtype);
-  grad_input.copy_(grad_input_cpu);
-  return grad_input;
+  return native::xpu::_softmax_backward_kernel(
+      grad_output, output, dim, false, grad_input);
 }
 
-Tensor XPUNativeFunctions::_softmax_backward_data(
-    const Tensor& grad_output,
-    const Tensor& output,
+Tensor XPUNativeFunctions::_log_softmax_backward_data(
+    const at::Tensor& grad_output,
+    const at::Tensor& output,
     int64_t dim,
-    ScalarType input_dtype) {
+    at::ScalarType input_dtype) {
   std::optional<Device> common_device = std::nullopt;
   c10::impl::check_and_update_common_device(
       common_device,
       grad_output,
-      "xpu::_softmax_backward_data_out",
+      "xpu::_log_softmax_backward_data",
       "grad_output");
   c10::impl::check_and_update_common_device(
-      common_device, output, "xpu::_softmax_backward_data_out", "output");
+      common_device, output, "xpu::_log_softmax_backward_data", "output");
   Tensor grad_input;
-  meta_func__softmax_backward_data(
+  grad_input = _softmax_backward_data_meta(
       grad_output, output, dim, input_dtype, grad_input);
-  Tensor grad_output_cpu = grad_output.to(Device(kCPU));
-  Tensor output_cpu = output.to(Device(kCPU));
-  Tensor grad_input_cpu = grad_input.to(Device(kCPU));
-  at::_softmax_backward_data_out(
-      grad_input_cpu, grad_output_cpu, output_cpu, dim, input_dtype);
-  grad_input.copy_(grad_input_cpu);
+  native::xpu::_log_softmax_backward_kernel(
+      grad_output, output, dim, false, grad_input);
   return grad_input;
+}
+
+Tensor& XPUNativeFunctions::_log_softmax_backward_data_out(
+    const at::Tensor& grad_output,
+    const at::Tensor& output,
+    int64_t dim,
+    at::ScalarType input_dtype,
+    Tensor& grad_input) {
+  std::optional<Device> common_device = std::nullopt;
+  c10::impl::check_and_update_common_device(
+      common_device,
+      grad_input,
+      "xpu::_log_softmax_backward_data_out_out",
+      "grad_input");
+  c10::impl::check_and_update_common_device(
+      common_device,
+      grad_output,
+      "xpu::_log_softmax_backward_data_out_out",
+      "grad_output");
+  c10::impl::check_and_update_common_device(
+      common_device,
+      output,
+      "xpu::_log_softmax_backward_data_out_out",
+      "output");
+  grad_input = _softmax_backward_data_meta(
+      grad_output, output, dim, input_dtype, grad_input);
+  return native::xpu::_log_softmax_backward_kernel(
+      grad_output, output, dim, false, grad_input);
 }
 
 } // namespace at
