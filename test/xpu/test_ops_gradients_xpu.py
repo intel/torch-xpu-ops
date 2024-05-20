4ce3dbe8ed32e0fc3324afb93a892e96cd242dc8
Deng, Daisy <daisy.deng@intel.com>
2024-05-19 22:28:51 -0700

    add test_ops_gradients_xpu.py

---
+++ test/xpu/test_ops_gradients_xpu.py
@@ -0,0 +1,19 @@
+# Owner(s): ["module: intel"]
+
+from torch.testing._internal.common_device_type import instantiate_device_type_tests
+from torch.testing._internal.common_utils import run_tests
+
+try:
+    from xpu_test_utils import XPUPatchForImport
+except Exception as e:
+    from .xpu_test_utils import XPUPatchForImport
+
+with XPUPatchForImport(False):
+    from test_ops_gradients import TestBwdGradients
+
+import pdb
+pdb.set_trace()
+instantiate_device_type_tests(TestBwdGradients, globals(), only_for="xpu")
+
+if __name__ == "__main__":
+    run_tests()
