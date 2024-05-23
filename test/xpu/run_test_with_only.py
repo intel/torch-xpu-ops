450d912b614ba66fcf3d267e0197376220da4201
Cheng Penghui <penghui.cheng@intel.com>
2024-05-23 06:34:10 +0000

    Add unit test cases for dynamic shapes
    Signed-off-by: Cheng Penghui <penghui.cheng@intel.com>

--- test/xpu/run_test_with_only.py
+++ test/xpu/run_test_with_only.py
@@ -47,5 +47,8 @@ res += launch_test("test_decomp_xpu.py", exe_list=execute_list)
 # test_comparison_utils
 res += launch_test("test_comparison_utils_xpu.py")
 
+# test_dynamic_shapes
+res += launch_test("test_dynamic_shapes_xpu.py")
+
 exit_code = os.WEXITSTATUS(res)
 sys.exit(exit_code)
