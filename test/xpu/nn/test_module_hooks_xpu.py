b339ab769fba441df094a91c8e8d758cced8d835
Cheng Penghui <penghui.cheng@intel.com>
2024-05-24 02:49:47 +0000

    Add unit test cases for module hooks
    Signed-off-by: Cheng Penghui <penghui.cheng@intel.com>

---
+++ test/xpu/nn/test_module_hooks_xpu.py
@@ -0,0 +1,24 @@
+# Owner(s): ["module: intel"]
+
+from torch.testing._internal.common_utils import instantiate_parametrized_tests, run_tests, TestCase
+
+try:
+    from .xpu_test_utils import XPUPatchForImport
+except Exception as e:
+    from ..xpu_test_utils import XPUPatchForImport
+
+with XPUPatchForImport(False):
+    from test_module_hooks import (
+        TestModuleHooks,
+        TestModuleHookNN,
+        TestModuleGlobalHooks,
+        TestStateDictHooks,
+    )
+
+
+instantiate_parametrized_tests(TestModuleHooks)
+
+
+if __name__ == "__main__":
+    TestCase._default_dtype_check_enabled = True
+    run_tests()
--- test/xpu/run_test_with_only.py
+++ test/xpu/run_test_with_only.py
@@ -47,5 +47,8 @@ res += launch_test("test_decomp_xpu.py", exe_list=execute_list)
 # test_comparison_utils
 res += launch_test("test_comparison_utils_xpu.py")
 
+# test_module_hooks
+res += launch_test("test_module_hooks_xpu.py")
+
 exit_code = os.WEXITSTATUS(res)
 sys.exit(exit_code)
