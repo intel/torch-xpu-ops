f89a933acae4e0ae83776a7f8a57e886c7d3dd0a
Deng, Daisy <daisy.deng@intel.com>
2024-05-27 23:22:50 -0700

    use patch for test_multihead_attention

--- test/xpu/run_test_with_skip.py
+++ test/xpu/run_test_with_skip.py
@@ -1617,6 +1617,8 @@ skip_list = (
     "test_multihead_attn_fast_path_small_test_xpu_float64",
     "test_multihead_attn_in_proj_bias_none_xpu_float64",
     "test_multihead_attn_in_proj_weight_none_xpu_float64",
+    # issue 342
+    "test_multihead_self_attn_two_masks_fast_path_mock_xpu",
 )
 
 res = launch_test("nn/test_multihead_attention_xpu.py", skip_list)
