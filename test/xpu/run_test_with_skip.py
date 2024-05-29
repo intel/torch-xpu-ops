0601582392194b1783ba8882f5ca4237e9b1c358
Yutao Xu <yutao.xu@intel.com>
2024-05-29 15:37:37 +0800

    Fix div ut failures caused by the ineffective type promotion (#307)
    Fix div ut failures caused by the ineffective type promotion.
    1. Align div with mul implementation by using opmath type.
    2. Fix div trunc failure when result of division between two same
    floating point values is not 1.f sometimes.
    3. Remove unnecessary kernels.
    
    ---------
    
    Signed-off-by: Feng Yuan <feng1.yuan@intel.com>
    Co-authored-by: Feng Yuan <feng1.yuan@intel.com>

--- test/xpu/run_test_with_skip.py
+++ test/xpu/run_test_with_skip.py
@@ -943,7 +943,7 @@ skip_list = (
     "jiterator", # Jiterator is only supported by CUDA
     "cuda", # Skip cuda hard-coded case
     "test_fmod_remainder_by_zero_integral_xpu_int64", # zero division is an undefined behavior: different handles on different backends
-    "test_div_rounding_numpy_xpu_float16", # CPU fail
+    "test_div_rounding_numpy_xpu_float16", # Calculation error. XPU implementation uses opmath type.
     "test_cpu_tensor_pow_cuda_scalar_tensor_xpu", # CUDA hard-coded
     "test_type_promotion_bitwise_and_xpu", # align CUDA dtype
     "test_type_promotion_bitwise_or_xpu", # align CUDA dtype
@@ -961,6 +961,8 @@ skip_list = (
     "test_logaddexp_xpu_complex64", # CPU fail
     "test_type_promotion_clamp_max_xpu", # align CUDA dtype, CUDA XFAIL
     "test_type_promotion_clamp_min_xpu", # align CUDA dtype, CUDA XFAIL
+    "test_div_rounding_nonfinite_xpu_bfloat16", # CPU result is not golden reference
+    "test_div_rounding_nonfinite_xpu_float16", # CPU result is not golden reference
 )
 res += launch_test("test_binary_ufuncs_xpu.py", skip_list)
 
