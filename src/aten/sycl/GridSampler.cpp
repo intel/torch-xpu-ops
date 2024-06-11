94828900f730c9a234cac67a250ccf1c9721d053
majing <Jing1.Ma@intel.com>
2024-06-11 00:50:21 -0700

    Change functor variables format
    Signed-off-by: majing <Jing1.Ma@intel.com>

--- src/aten/sycl/GridSampler.cpp
+++ src/aten/sycl/GridSampler.cpp
@@ -16,23 +16,23 @@ struct GridSampler2dKernelFunctor {
   using opmath_t = at::opmath_type<scalar_t>;
   void operator()(sycl::nd_item<1> item_id) const {
     auto index = item_id.get_global_linear_id();
-    if (index >= nthreads)
+    if (index >= nthreads_)
       return;
-    const index_t w = index % out_W;
-    const index_t h = (index / out_W) % out_H;
-    const index_t n = index / (out_H * out_W);
-    const index_t grid_offset = n * grid_sN + h * grid_sH + w * grid_sW;
+    const index_t w = index % out_W_;
+    const index_t h = (index / out_W_) % out_H_;
+    const index_t n = index / (out_H_ * out_W_);
+    const index_t grid_offset = n * grid_sN_ + h * grid_sH_ + w * grid_sW_;
 
     // get the corresponding input x, y co-ordinates from grid
-    opmath_t x = grid_data[grid_offset];
-    opmath_t y = grid_data[grid_offset + grid_sCoor];
+    opmath_t x = grid_.data[grid_offset];
+    opmath_t y = grid_.data[grid_offset + grid_sCoor_];
 
     opmath_t ix = grid_sampler_compute_source_index(
-        x, inp_W, padding_mode, align_corners);
+        x, inp_W_, padding_mode_, align_corners_);
     opmath_t iy = grid_sampler_compute_source_index(
-        y, inp_H, padding_mode, align_corners);
+        y, inp_H_, padding_mode_, align_corners_);
 
-    if (interpolation_mode == GridSamplerInterpolation::Bilinear) {
+    if (interpolation_mode_ == GridSamplerInterpolation::Bilinear) {
       // get NE, NW, SE, SW pixel values from (x, y)
       index_t ix_nw = static_cast<index_t>(std::floor(ix));
       index_t iy_nw = static_cast<index_t>(std::floor(iy));
@@ -50,43 +50,46 @@ struct GridSampler2dKernelFunctor {
       opmath_t se = (ix - ix_nw) * (iy - iy_nw);
 
       // calculate bilinear weighted pixel value and set output pixel
-      auto inp_ptr_NC = input_data + n * inp_sN;
-      auto out_ptr_NCHW = output_data + n * out_sN + h * out_sH + w * out_sW;
-      for (index_t c = 0; c < C;
-           ++c, inp_ptr_NC += inp_sC, out_ptr_NCHW += out_sC) {
+      auto inp_ptr_NC = input_.data + n * inp_sN_;
+      auto out_ptr_NCHW =
+          output_.data + n * out_sN_ + h * out_sH_ + w * out_sW_;
+      for (index_t c = 0; c < C_;
+           ++c, inp_ptr_NC += inp_sC_, out_ptr_NCHW += out_sC_) {
         opmath_t out_acc = 0;
-        if (within_bounds_2d(iy_nw, ix_nw, inp_H, inp_W)) {
-          out_acc += inp_ptr_NC[iy_nw * inp_sH + ix_nw * inp_sW] * nw;
+        if (within_bounds_2d(iy_nw, ix_nw, inp_H_, inp_W_)) {
+          out_acc += inp_ptr_NC[iy_nw * inp_sH_ + ix_nw * inp_sW_] * nw;
         }
-        if (within_bounds_2d(iy_ne, ix_ne, inp_H, inp_W)) {
-          out_acc += inp_ptr_NC[iy_ne * inp_sH + ix_ne * inp_sW] * ne;
+        if (within_bounds_2d(iy_ne, ix_ne, inp_H_, inp_W_)) {
+          out_acc += inp_ptr_NC[iy_ne * inp_sH_ + ix_ne * inp_sW_] * ne;
         }
-        if (within_bounds_2d(iy_sw, ix_sw, inp_H, inp_W)) {
-          out_acc += inp_ptr_NC[iy_sw * inp_sH + ix_sw * inp_sW] * sw;
+        if (within_bounds_2d(iy_sw, ix_sw, inp_H_, inp_W_)) {
+          out_acc += inp_ptr_NC[iy_sw * inp_sH_ + ix_sw * inp_sW_] * sw;
         }
-        if (within_bounds_2d(iy_se, ix_se, inp_H, inp_W)) {
-          out_acc += inp_ptr_NC[iy_se * inp_sH + ix_se * inp_sW] * se;
+        if (within_bounds_2d(iy_se, ix_se, inp_H_, inp_W_)) {
+          out_acc += inp_ptr_NC[iy_se * inp_sH_ + ix_se * inp_sW_] * se;
         }
         *out_ptr_NCHW = out_acc;
       }
-    } else if (interpolation_mode == GridSamplerInterpolation::Nearest) {
+    } else if (interpolation_mode_ == GridSamplerInterpolation::Nearest) {
       index_t ix_nearest = static_cast<index_t>(std::nearbyint(ix));
       index_t iy_nearest = static_cast<index_t>(std::nearbyint(iy));
 
       // assign nearest neighor pixel value to output pixel
-      auto inp_ptr_NC = input_data + n * inp_sN;
-      auto out_ptr_NCHW = output_data + n * out_sN + h * out_sH + w * out_sW;
-      for (index_t c = 0; c < C;
-           ++c, inp_ptr_NC += inp_sC, out_ptr_NCHW += out_sC) {
-        if (within_bounds_2d(iy_nearest, ix_nearest, inp_H, inp_W)) {
-          *out_ptr_NCHW = inp_ptr_NC[iy_nearest * inp_sH + ix_nearest * inp_sW];
+      auto inp_ptr_NC = input_.data + n * inp_sN_;
+      auto out_ptr_NCHW =
+          output_.data + n * out_sN_ + h * out_sH_ + w * out_sW_;
+      for (index_t c = 0; c < C_;
+           ++c, inp_ptr_NC += inp_sC_, out_ptr_NCHW += out_sC_) {
+        if (within_bounds_2d(iy_nearest, ix_nearest, inp_H_, inp_W_)) {
+          *out_ptr_NCHW =
+              inp_ptr_NC[iy_nearest * inp_sH_ + ix_nearest * inp_sW_];
         } else {
           *out_ptr_NCHW = static_cast<scalar_t>(0);
         }
       }
-    } else if (interpolation_mode == GridSamplerInterpolation::Bicubic) {
-      ix = grid_sampler_unnormalize(x, inp_W, align_corners);
-      iy = grid_sampler_unnormalize(y, inp_H, align_corners);
+    } else if (interpolation_mode_ == GridSamplerInterpolation::Bicubic) {
+      ix = grid_sampler_unnormalize(x, inp_W_, align_corners_);
+      iy = grid_sampler_unnormalize(y, inp_H_, align_corners_);
 
       opmath_t ix_nw = std::floor(ix);
       opmath_t iy_nw = std::floor(iy);
@@ -94,10 +97,11 @@ struct GridSampler2dKernelFunctor {
       const opmath_t tx = ix - ix_nw;
       const opmath_t ty = iy - iy_nw;
 
-      auto inp_ptr_NC = input.data + n * inp_sN;
-      auto out_ptr_NCHW = output.data + n * out_sN + h * out_sH + w * out_sW;
-      for (index_t c = 0; c < C;
-           ++c, inp_ptr_NC += inp_sC, out_ptr_NCHW += out_sC) {
+      auto inp_ptr_NC = input_.data + n * inp_sN_;
+      auto out_ptr_NCHW =
+          output_.data + n * out_sN_ + h * out_sH_ + w * out_sW_;
+      for (index_t c = 0; c < C_;
+           ++c, inp_ptr_NC += inp_sC_, out_ptr_NCHW += out_sC_) {
         opmath_t coefficients[4];
 
 #pragma unroll 4
@@ -107,42 +111,42 @@ struct GridSampler2dKernelFunctor {
                   inp_ptr_NC,
                   ix_nw - 1,
                   iy_nw - 1 + i,
-                  inp_W,
-                  inp_H,
-                  inp_sW,
-                  inp_sH,
-                  padding_mode,
-                  align_corners),
+                  inp_W_,
+                  inp_H_,
+                  inp_sW_,
+                  inp_sH_,
+                  padding_mode_,
+                  align_corners_),
               get_value_bounded<scalar_t>(
                   inp_ptr_NC,
                   ix_nw + 0,
                   iy_nw - 1 + i,
-                  inp_W,
-                  inp_H,
-                  inp_sW,
-                  inp_sH,
-                  padding_mode,
-                  align_corners),
+                  inp_W_,
+                  inp_H_,
+                  inp_sW_,
+                  inp_sH_,
+                  padding_mode_,
+                  align_corners_),
               get_value_bounded<scalar_t>(
                   inp_ptr_NC,
                   ix_nw + 1,
                   iy_nw - 1 + i,
-                  inp_W,
-                  inp_H,
-                  inp_sW,
-                  inp_sH,
-                  padding_mode,
-                  align_corners),
+                  inp_W_,
+                  inp_H_,
+                  inp_sW_,
+                  inp_sH_,
+                  padding_mode_,
+                  align_corners_),
               get_value_bounded<scalar_t>(
                   inp_ptr_NC,
                   ix_nw + 2,
                   iy_nw - 1 + i,
-                  inp_W,
-                  inp_H,
-                  inp_sW,
-                  inp_sH,
-                  padding_mode,
-                  align_corners),
+                  inp_W_,
+                  inp_H_,
+                  inp_sW_,
+                  inp_sH_,
+                  padding_mode_,
+                  align_corners_),
               tx);
         }
 
@@ -156,89 +160,80 @@ struct GridSampler2dKernelFunctor {
     }
   }
   GridSampler2dKernelFunctor(
-      const index_t nthreads_,
-      TensorInfo<scalar_t, index_t> input_,
-      TensorInfo<scalar_t, index_t> grid_,
-      TensorInfo<scalar_t, index_t> output_,
-      const GridSamplerInterpolation interpolation_mode_,
-      const GridSamplerPadding padding_mode_,
-      bool align_corners_,
-      index_t C_,
-      index_t inp_H_,
-      index_t inp_W_,
-      index_t out_H_,
-      index_t out_W_,
-      index_t inp_sN_,
-      index_t inp_sC_,
-      index_t inp_sH_,
-      index_t inp_sW_,
-      index_t grid_sN_,
-      index_t grid_sH_,
-      index_t grid_sW_,
-      index_t grid_sCoor_,
-      index_t out_sN_,
-      index_t out_sC_,
-      index_t out_sH_,
-      index_t out_sW_,
-      scalar_t* grid_data_,
-      scalar_t* input_data_,
-      scalar_t* output_data_)
-      : nthreads(nthreads_),
-        input(input_),
-        grid(grid_),
-        output(output_),
-        interpolation_mode(interpolation_mode_),
-        padding_mode(padding_mode_),
-        align_corners(align_corners_),
-        C(C_),
-        inp_H(inp_H_),
-        inp_W(inp_W_),
-        out_H(out_H_),
-        out_W(out_W_),
-        inp_sN(inp_sN_),
-        inp_sC(inp_sC_),
-        inp_sH(inp_sH_),
-        inp_sW(inp_sW_),
-        grid_sN(grid_sN_),
-        grid_sH(grid_sH_),
-        grid_sW(grid_sW_),
-        grid_sCoor(grid_sCoor_),
-        out_sN(out_sN_),
-        out_sC(out_sC_),
-        out_sH(out_sH_),
-        out_sW(out_sW_),
-        grid_data(grid_data_),
-        input_data(input_data_),
-        output_data(output_data_) {}
+      const index_t nthreads,
+      TensorInfo<scalar_t, index_t> input,
+      TensorInfo<scalar_t, index_t> grid,
+      TensorInfo<scalar_t, index_t> output,
+      const GridSamplerInterpolation interpolation_mode,
+      const GridSamplerPadding padding_mode,
+      bool align_corners,
+      index_t C,
+      index_t inp_H,
+      index_t inp_W,
+      index_t out_H,
+      index_t out_W,
+      index_t inp_sN,
+      index_t inp_sC,
+      index_t inp_sH,
+      index_t inp_sW,
+      index_t grid_sN,
+      index_t grid_sH,
+      index_t grid_sW,
+      index_t grid_sCoor,
+      index_t out_sN,
+      index_t out_sC,
+      index_t out_sH,
+      index_t out_sW)
+      : nthreads_(nthreads),
+        input_(input),
+        grid_(grid),
+        output_(output),
+        interpolation_mode_(interpolation_mode),
+        padding_mode_(padding_mode),
+        align_corners_(align_corners),
+        C_(C),
+        inp_H_(inp_H),
+        inp_W_(inp_W),
+        out_H_(out_H),
+        out_W_(out_W),
+        inp_sN_(inp_sN),
+        inp_sC_(inp_sC),
+        inp_sH_(inp_sH),
+        inp_sW_(inp_sW),
+        grid_sN_(grid_sN),
+        grid_sH_(grid_sH),
+        grid_sW_(grid_sW),
+        grid_sCoor_(grid_sCoor),
+        out_sN_(out_sN),
+        out_sC_(out_sC),
+        out_sH_(out_sH),
+        out_sW_(out_sW) {}
 
  private:
-  const index_t nthreads;
-  TensorInfo<scalar_t, index_t> input;
-  TensorInfo<scalar_t, index_t> grid;
-  TensorInfo<scalar_t, index_t> output;
-  const GridSamplerInterpolation interpolation_mode;
-  const GridSamplerPadding padding_mode;
-  bool align_corners;
-  index_t C;
-  index_t inp_H;
-  index_t inp_W;
-  index_t out_H;
-  index_t out_W;
-  index_t inp_sN;
-  index_t inp_sC;
-  index_t inp_sH;
-  index_t inp_sW;
-  index_t grid_sN;
-  index_t grid_sH;
-  index_t grid_sW;
-  index_t grid_sCoor;
-  index_t out_sN;
-  index_t out_sC;
-  index_t out_sH;
-  index_t out_sW;
-  scalar_t* grid_data;
-  scalar_t* input_data;
-  scalar_t* output_data;
+  const index_t nthreads_;
+  TensorInfo<scalar_t, index_t> input_;
+  TensorInfo<scalar_t, index_t> grid_;
+  TensorInfo<scalar_t, index_t> output_;
+  const GridSamplerInterpolation interpolation_mode_;
+  const GridSamplerPadding padding_mode_;
+  bool align_corners_;
+  index_t C_;
+  index_t inp_H_;
+  index_t inp_W_;
+  index_t out_H_;
+  index_t out_W_;
+  index_t inp_sN_;
+  index_t inp_sC_;
+  index_t inp_sH_;
+  index_t inp_sW_;
+  index_t grid_sN_;
+  index_t grid_sH_;
+  index_t grid_sW_;
+  index_t grid_sCoor_;
+  index_t out_sN_;
+  index_t out_sC_;
+  index_t out_sH_;
+  index_t out_sW_;
 };
 
 template <typename scalar_t, typename index_t>
@@ -272,10 +267,6 @@ void grid_sampler_2d_forward_template(
   index_t out_sH = output.strides[2];
   index_t out_sW = output.strides[3];
 
-  auto grid_data = grid.data;
-  auto input_data = input.data;
-  auto output_data = output.data;
-
   GridSampler2dKernelFunctor<scalar_t, index_t> kfn(
       nthreads,
       input,
@@ -300,10 +291,7 @@ void grid_sampler_2d_forward_template(
       out_sN,
       out_sC,
       out_sH,
-      out_sW,
-      grid_data,
-      input_data,
-      output_data);
+      out_sW);
   sycl_kernel_submit(
       sycl::range<1>(ngroups * wgroup_size),
       sycl::range<1>(wgroup_size),
