4e3cfbecb197a92476d258adfa1878271e163a91
hjhee <jianhang.he@intel.com>
2024-06-11 07:20:37 +0000

    remove redundant check_and_update_common_device in argmax, amax

--- src/aten/ReduceOps.cpp
+++ src/aten/ReduceOps.cpp
@@ -469,11 +469,6 @@ Tensor& XPUNativeFunctions::argmax_out(
     c10::optional<int64_t> dim,
     bool keepdim,
     Tensor& out) {
-  std::optional<Device> common_device = std::nullopt;
-  c10::impl::check_and_update_common_device(
-      common_device, out, "xpu::argmax_out", "out");
-  c10::impl::check_and_update_common_device(
-      common_device, self, "xpu::argmax_out", "self");
   out = argmax_meta(self, dim, keepdim, out);
   argmax_argmin_impl(self, dim, keepdim, out, native::xpu::argmax_kernel);
   return out;
@@ -484,11 +479,6 @@ Tensor XPUNativeFunctions::argmax(
     c10::optional<int64_t> dim,
     bool keepdim) {
   Tensor out;
-  std::optional<Device> common_device = std::nullopt;
-  c10::impl::check_and_update_common_device(
-      common_device, out, "xpu::argmax", "out");
-  c10::impl::check_and_update_common_device(
-      common_device, self, "xpu::argmax", "self");
   out = argmax_meta(self, dim, keepdim, out);
   argmax_argmin_impl(self, dim, keepdim, out, native::xpu::argmax_kernel);
   return out;
