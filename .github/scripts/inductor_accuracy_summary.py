e0a42f3c8e0a9d3a14c2c7fe35e39859f47ad08d
Zhong, Ruijie <ruijie.zhong@intel.com>
2024-06-21 01:46:58 +0000

    remove triton

--- .github/scripts/inductor_accuracy_summary.py
+++ .github/scripts/inductor_accuracy_summary.py
@@ -1,4 +1,3 @@
-## python inductor_accuracy_summary.py -t TRITON_v3.0 -r TRITON_v2.1 -p amp_bf16 amp_fp16 bfloat16 float16 float32 -s torchbench
 import argparse
 import pandas as pd
 from scipy.stats import gmean
