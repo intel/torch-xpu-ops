15c95df849c94076e0e5d749b7dc0b8387ee2075
Yan Zhiwei <zhiwei.yan@intel.com>
2024-06-21 05:35:56 +0000

    Modify file structure

--- src/aten/sycl/ScanKernels.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <cstdint>
-
-namespace at::native::xpu {
-
-void cumsum_kernel(const Tensor& result, const Tensor& self, int64_t dim);
-
-} // namespace at::native::xpu
--- src/aten/sycl/UnaryLogKernels.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <ATen/native/TensorIterator.h>
-
-namespace at::native::xpu {
-
-void log_kernel(TensorIteratorBase& iter);
-
-} // namespace at::native::xpu
