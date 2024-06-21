096281be139157bd89a7ed780e3c48bfa7d6beff
Huaiyu, Zheng <huaiyu.zheng@intel.com>
2024-06-21 02:57:54 +0000

    update test case list

--- test/xpu/fin_grain/run_fine_grain.py
+++ test/xpu/fin_grain/run_fine_grain.py
@@ -55,7 +55,6 @@ skip_list = (
     # AssertionError: False is not true : Keyword argument 'output grad 0' during backward call unexpectedly materializes. Either set `supports_cow_input_no_materialize_backward=False` in this operation's OpInfo, add the arg to the OpInfo's `allow_cow_input_materialize_backward` list, or change the implementation to avoid materialization.
     # https://github.com/intel/torch-xpu-ops/issues/281
     "test_cow_input",
-    "test_cow_input_addr_xpu_float32",
 
     # XPU implementation is correct.
     # std::exp{-inf, nan}, the result is (±0,±0) (signs are unspecified)
