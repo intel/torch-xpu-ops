079dbd62675d43829baa3f2745cd046f4634a4b0
Cheng Penghui <penghui.cheng@intel.com>
2024-06-24 10:23:08 +0000

    Fixed issue
    Signed-off-by: Cheng Penghui <penghui.cheng@intel.com>

--- test/xpu/run_test_with_skip.py
+++ test/xpu/run_test_with_skip.py
@@ -408,6 +408,7 @@ skip_list = (
 
     # https://github.com/intel/torch-xpu-ops/issues/157
     # Segfault:
+    "test_dtypes_nn_functional_linear_xpu", # https://github.com/intel/torch-xpu-ops/issues/157
     "test_dtypes_nn_functional_multi_head_attention_forward_xpu", # https://github.com/intel/torch-xpu-ops/issues/157
     "test_dtypes_svd_lowrank_xpu", # https://github.com/intel/torch-xpu-ops/issues/157
     "test_noncontiguous_samples_nn_functional_linear_xpu_int64", # https://github.com/intel/torch-xpu-ops/issues/157
