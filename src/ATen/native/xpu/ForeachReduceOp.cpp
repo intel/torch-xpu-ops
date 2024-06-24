a9d4dc7c971ae208fae34e55368eea80f52371b7
Feng Yuan <feng1.yuan@intel.com>
2024-06-24 18:58:24 +0800

    Remove cuda in sources

--- src/ATen/native/xpu/ForeachReduceOp.cpp
+++ src/ATen/native/xpu/ForeachReduceOp.cpp
@@ -50,7 +50,7 @@ std::vector<Tensor> XPUNativeFunctions::_foreach_norm(
       return ord.to<double>();
     } else {
       TORCH_CHECK(
-          false, "foreach_tensor_norm_cuda expects ord to be integer or float");
+          false, "foreach_norm_xpu expects ord to be integer or float");
     }
   }();
   at::native::check_foreach_api_restrictions(tensors);
