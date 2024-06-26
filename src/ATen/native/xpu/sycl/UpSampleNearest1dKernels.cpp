intel/torch-xpu-ops

commit d0d350e8f6a4c086b2bd7bf30808002f8a0b2c3c

Add aten::upsample_nearest series of ops (#373)
ops need to support in this pr
- [x] _upsample_nearest_exact1d
- [x] _upsample_nearest_exact1d_backward
- [x] _upsample_nearest_exact2d
- [x] _upsample_nearest_exact2d_backward
- [x] upsample_nearest1d
- [x] upsample_nearest1d_backward
- [x] upsample_nearest2d
- [x] upsample_nearest2d_backward

---------

Co-authored-by: Feng Yuan <feng1.yuan@intel.com>

src/ATen/native/xpu/sycl/UpSampleNearest1dKernels.cpp
@@ -1,0 +1,314 @@
+#include <ATen/AccumulateType.h>
+#include <ATen/core/Tensor.h>
+#include <ATen/native/xpu/sycl/UpSampleNearest1dKernels.h>
+#include <comm/Runtime.h>
+#include <comm/SYCLContext.h>
+#include <comm/SYCLHelpers.h>
+
+namespace at::native {
+namespace xpu {
+
+template <typename scalar_t, typename accscalar_t, typename index_bw_op_t>
+struct UpsampleNearest1dBackwardKernelFunctor {
+  void operator()(sycl::nd_item<1> item) const {
+    int dst_idx = item.get_global_linear_id();
+    if (dst_idx >= dim_c_ * dst_dim_w_)
+      return;
+    int c = (dst_idx / (dst_dim_w_)) % dim_c_;
+    int dst_x = dst_idx % dst_dim_w_;
+    // note that we do not want to clamp src_x to src_dim_w, since we might
+    // intentionally want to skip in case of scale_factor < 1.0
+    int src_x = index_bw_op_(scale_factor_, dst_x, src_dim_w_);
+    int src_x_up = index_bw_op_(scale_factor_, dst_x + 1, src_dim_w_);
+    for (int b = 0; b < dim_b_; b++) {
+      accscalar_t grad = 0;
+      int src_idx = b * dim_c_ * src_dim_w_ + c * src_dim_w_ + src_x;
+      for (int x = src_x; x < src_x_up; x++) {
+        grad += grad_o_[src_idx++];
+      }
+      grad_i_[dst_idx] = grad;
+      dst_idx += dim_c_ * dst_dim_w_;
+    }
+  }
+  UpsampleNearest1dBackwardKernelFunctor(
+      int n,
+      const scalar_t* grad_o,
+      size_t dim_b,
+      size_t dim_c,
+      size_t src_dim_w,
+      size_t dst_dim_w,
+      scalar_t* grad_i,
+      float scale_factor,
+      index_bw_op_t index_bw_op)
+      : n_(n),
+        grad_o_(grad_o),
+        dim_b_(dim_b),
+        dim_c_(dim_c),
+        src_dim_w_(src_dim_w),
+        dst_dim_w_(dst_dim_w),
+        grad_i_(grad_i),
+        scale_factor_(scale_factor),
+        index_bw_op_(index_bw_op) {}
+
+ private:
+  int n_;
+  const scalar_t* grad_o_;
+  size_t dim_b_;
+  size_t dim_c_;
+  size_t src_dim_w_;
+  size_t dst_dim_w_;
+  scalar_t* grad_i_;
+  float scale_factor_;
+  index_bw_op_t index_bw_op_;
+};
+template <typename scalar_t, typename accscalar_t, typename index_bw_op_t>
+void upsample_nearest1d_backward_frame(
+    int n,
+    const scalar_t* grad_o,
+    size_t dim_b,
+    size_t dim_c,
+    size_t src_dim_w,
+    size_t dst_dim_w,
+    scalar_t* grad_i,
+    float scale_factor,
+    index_bw_op_t index_bw_op) {
+  auto& queue = at::xpu::getCurrentSYCLQueue();
+  auto work_group_size = syclMaxWorkItemsPerEU();
+  int global_range =
+      (n + work_group_size - 1) / work_group_size * work_group_size;
+  auto caller = UpsampleNearest1dBackwardKernelFunctor<
+      scalar_t,
+      accscalar_t,
+      index_bw_op_t>(
+      n,
+      grad_o,
+      dim_b,
+      dim_c,
+      src_dim_w,
+      dst_dim_w,
+      grad_i,
+      scale_factor,
+      index_bw_op);
+  sycl_kernel_submit(global_range, work_group_size, queue, caller);
+}
+
+void upsample_nearest1d_backward_kernel(
+    Tensor& grad_input,
+    const Tensor& grad_output_,
+    IntArrayRef output_size,
+    IntArrayRef input_size,
+    std::optional<double> scales,
+    bool is_exact) {
+  TensorArg grad_input_arg{grad_input, "grad_input", 1};
+  TensorArg grad_output_arg{grad_output_, "grad_output_", 2};
+  checkAllSameGPU(__func__, {grad_input_arg, grad_output_arg});
+  int output_width = output_size[0];
+  int nbatch = input_size[0];
+  int channels = input_size[1];
+  int input_width = input_size[2];
+  Tensor grad_output = grad_output_.contiguous();
+  if (grad_input.numel() == 0) {
+    return;
+  }
+  // upsample_nearest1d meta call makes sure `nbatch != 0`
+  unsigned int n = grad_input.numel() / nbatch;
+  // safe check for int32 indexing; implicitly restrict launch config for kernel
+  TORCH_CHECK(grad_input.numel() <= std::numeric_limits<int32_t>::max());
+  Tensor grad_input_c = grad_input.contiguous();
+  AT_DISPATCH_FLOATING_TYPES_AND3(
+      ScalarType::BFloat16,
+      ScalarType::Half,
+      ScalarType::Byte,
+      grad_output.scalar_type(),
+      "upsample_nearest1d_backward_xpu",
+      [&] {
+        using accscalar_t = acc_type<scalar_t, true>;
+        auto idata = grad_input_c.data_ptr<scalar_t>();
+        auto odata = grad_output.data_ptr<scalar_t>();
+        const float scale_factor = compute_scales_value_backwards<float>(
+            scales, output_width, input_width);
+        if (is_exact) {
+          upsample_nearest1d_backward_frame<scalar_t, accscalar_t>(
+              n,
+              odata,
+              nbatch,
+              channels,
+              output_width,
+              input_width,
+              idata,
+              scale_factor,
+              NearestExactBwIndexOp());
+        } else {
+          upsample_nearest1d_backward_frame<scalar_t, accscalar_t>(
+              n,
+              odata,
+              nbatch,
+              channels,
+              output_width,
+              input_width,
+              idata,
+              scale_factor,
+              NearestBwIndexOp());
+        }
+      });
+  if (!grad_input.is_contiguous()) {
+    grad_input.copy_(grad_input_c);
+  }
+}
+
+template <typename scalar_t, typename index_op_t>
+struct UpsampleNearest1dKernelFunctor {
+  void operator()(sycl::nd_item<1> item) const {
+    int dst_idx = item.get_global_linear_id();
+    if (dst_idx >= dim_c_ * dst_dim_w_)
+      return;
+
+    int c = (dst_idx / dst_dim_w_) % dim_c_;
+
+    int dst_x = dst_idx % dst_dim_w_;
+    int src_x = index_op_(scale_factor_, dst_x, src_dim_w_);
+
+    int src_idx = c * src_dim_w_ + src_x;
+    int src_stride = dim_c_ * src_dim_w_;
+    int dst_stride = dim_c_ * dst_dim_w_;
+
+    for (int b = 0; b < dim_b_; b++) {
+      output_[dst_idx] = input_[src_idx];
+      src_idx += src_stride;
+      dst_idx += dst_stride;
+    }
+  }
+  UpsampleNearest1dKernelFunctor(
+      int n,
+      const scalar_t* input,
+      size_t dim_b,
+      size_t dim_c,
+      size_t src_dim_w,
+      size_t dst_dim_w,
+      scalar_t* output,
+      float scale_factor,
+      index_op_t index_op)
+      : n_(n),
+        input_(input),
+        dim_b_(dim_b),
+        dim_c_(dim_c),
+        src_dim_w_(src_dim_w),
+        dst_dim_w_(dst_dim_w),
+        output_(output),
+        scale_factor_(scale_factor),
+        index_op_(index_op) {}
+
+ private:
+  int n_;
+  const scalar_t* input_;
+  size_t dim_b_;
+  size_t dim_c_;
+  size_t src_dim_w_;
+  size_t dst_dim_w_;
+  scalar_t* output_;
+  float scale_factor_;
+  index_op_t index_op_;
+};
+
+template <typename scalar_t, typename index_op_t>
+void upsample_nearest1d_frame(
+    int n,
+    const scalar_t* input,
+    size_t dim_b,
+    size_t dim_c,
+    size_t src_dim_w,
+    size_t dst_dim_w,
+    scalar_t* output,
+    float scale_factor,
+    index_op_t index_op) {
+  auto& queue = at::xpu::getCurrentSYCLQueue();
+
+  auto work_group_size = syclMaxWorkItemsPerEU();
+  int global_range =
+      (n + work_group_size - 1) / work_group_size * work_group_size;
+
+  auto kfn = UpsampleNearest1dKernelFunctor<scalar_t, index_op_t>(
+      n,
+      input,
+      dim_b,
+      dim_c,
+      src_dim_w,
+      dst_dim_w,
+      output,
+      scale_factor,
+      index_op);
+
+  sycl_kernel_submit(global_range, work_group_size, queue, kfn);
+}
+
+void upsample_nearest1d_kernel(
+    Tensor& output,
+    const Tensor& input_,
+    IntArrayRef output_size,
+    c10::optional<double> scales,
+    bool is_exact) {
+  TensorArg input_arg{input_, "input_", 1};
+  TensorArg output_arg{output, "output", 2};
+  checkAllSameGPU(__func__, {input_arg, output_arg});
+  int output_width = output_size[0];
+
+  int nbatch = input_.size(0);
+  int channels = input_.size(1);
+  int input_width = input_.size(2);
+
+  Tensor input = input_.contiguous();
+
+  if (input.numel() == 0) {
+    return;
+  }
+  // upsample_nearest1d meta call makes sure `nbatch != 0`
+  unsigned int n = output.numel() / nbatch;
+  // safe check for int32 indexing; implicitly restrict launch config for
+  // kernel
+  TORCH_CHECK(output.numel() <= std::numeric_limits<int32_t>::max());
+  Tensor output_c = output.contiguous();
+
+  AT_DISPATCH_FLOATING_TYPES_AND3(
+      ScalarType::BFloat16,
+      ScalarType::Half,
+      ScalarType::Byte,
+      input.scalar_type(),
+      "upsample_nearest1d_xpu",
+      [&] {
+        using accscalar_t = acc_type<scalar_t, true>;
+
+        auto idata = input.data_ptr<scalar_t>();
+        auto odata = output_c.data_ptr<scalar_t>();
+
+        const float scale_factor =
+            compute_scales_value<float>(scales, input_width, output_width);
+        if (is_exact) {
+          upsample_nearest1d_frame<scalar_t>(
+              n,
+              idata,
+              nbatch,
+              channels,
+              input_width,
+              output_width,
+              odata,
+              scale_factor,
+              NearestExactIndexOp());
+        } else {
+          upsample_nearest1d_frame<scalar_t>(
+              n,
+              idata,
+              nbatch,
+              channels,
+              input_width,
+              output_width,
+              odata,
+              scale_factor,
+              NearestIndexOp());
+        }
+      });
+  if (!output.is_contiguous()) {
+    output.copy_(output_c);
+  }
+}
+} // namespace xpu
+} // namespace at::native

examples/test_upsample_nearest.py
@@ -1,0 +1,158 @@
+import torch
+from torch.testing._internal.common_utils import TestCase
+from torch.autograd import Variable
+
+device = torch.device("xpu")
+cpu_device = torch.device("cpu")
+
+class TestTorchMethod(TestCase):
+    def test_upsample_nearest(self):
+        test_dtypes = [torch.float, torch.bfloat16, torch.float16]
+
+        def test_upsample_nearest_exact(dtype):
+            # #### upsample nearest exact 1D #####
+            input_cpu = torch.randn((2, 3, 5), dtype=dtype, device=cpu_device)
+            input_xpu = input_cpu.to("xpu")
+            scales = [6]
+            input_cpu.requires_grad = True
+            input_xpu.requires_grad = True
+            rsf = False
+
+            output_cpu = torch.nn.functional.interpolate(
+                input_cpu,
+                scale_factor=scales,
+                mode="nearest-exact",
+                recompute_scale_factor=rsf,
+            )
+            output_xpu = torch.nn.functional.interpolate(
+                input_xpu,
+                scale_factor=scales,
+                mode="nearest-exact",
+                recompute_scale_factor=rsf,
+            )
+            self.assertEqual(output_cpu, output_xpu.cpu())
+
+            grad_out_cpu = torch.ones_like(output_cpu)
+            grad_out_xpu = grad_out_cpu.to("xpu")
+            grad_out_cpu = Variable(grad_out_cpu, requires_grad=True)
+            grad_out_xpu = Variable(grad_out_xpu, requires_grad=True)
+
+            output_cpu.backward(grad_out_cpu)
+            output_xpu.backward(grad_out_xpu)
+            grad_cpu = input_cpu.grad
+            grad_xpu = input_xpu.grad
+            self.assertEqual(grad_cpu, grad_xpu.cpu())
+
+            # #### upsample nearest exact 2D #####
+            input_cpu = torch.randn(
+                (2, 3, 5, 5), dtype=torch.float32, device=cpu_device
+            )
+            input_dpcpp = input_cpu.to("xpu")
+            scales = [6, 8]
+            input_cpu.requires_grad = True
+            input_dpcpp.requires_grad = True
+            alc = False
+
+            output_cpu = torch.nn.functional.interpolate(
+                input_cpu,
+                scale_factor=scales,
+                mode="nearest-exact",
+                recompute_scale_factor=rsf,
+            )
+            output_dpcpp = torch.nn.functional.interpolate(
+                input_dpcpp,
+                scale_factor=scales,
+                mode="nearest-exact",
+                recompute_scale_factor=rsf,
+            )
+            self.assertEqual(output_cpu, output_dpcpp.cpu())
+
+            grad_out_cpu = torch.randn(
+                (2, 3, 30, 40), dtype=torch.float32, device=cpu_device
+            )
+            grad_out_dpcpp = grad_out_cpu.to("xpu")
+            grad_out_cpu = Variable(grad_out_cpu, requires_grad=True)
+            grad_out_dpcpp = Variable(grad_out_dpcpp, requires_grad=True)
+
+            output_cpu.backward(grad_out_cpu)
+            output_dpcpp.backward(grad_out_dpcpp)
+            grad_cpu = input_cpu.grad
+            grad_dpcpp = input_dpcpp.grad
+            self.assertEqual(grad_cpu, grad_dpcpp.cpu())
+
+        def test_upsamle_nearest(dtype):
+            # #### upsample nearest 1D #####
+            input_cpu = torch.randn((2, 3, 5), dtype=torch.float32, device=cpu_device)
+            input_xpu = input_cpu.to("xpu")
+            scales = [6]
+            input_cpu.requires_grad = True
+            input_xpu.requires_grad = True
+            rsf = False
+
+            output_cpu = torch.nn.functional.interpolate(
+                input_cpu,
+                scale_factor=scales,
+                mode="nearest",
+                recompute_scale_factor=rsf,
+            )
+            output_xpu = torch.nn.functional.interpolate(
+                input_xpu,
+                scale_factor=scales,
+                mode="nearest",
+                recompute_scale_factor=rsf,
+            )
+            self.assertEqual(output_cpu, output_xpu.cpu())
+
+            grad_out_cpu = torch.randn(
+                (2, 3, 30), dtype=torch.float32, device=cpu_device
+            )
+            grad_out_xpu = grad_out_cpu.to("xpu")
+            grad_out_cpu = Variable(grad_out_cpu, requires_grad=True)
+            grad_out_xpu = Variable(grad_out_xpu, requires_grad=True)
+
+            output_cpu.backward(grad_out_cpu)
+            output_xpu.backward(grad_out_xpu)
+            grad_cpu = input_cpu.grad
+            grad_xpu = input_xpu.grad
+            self.assertEqual(grad_cpu, grad_xpu.cpu())
+
+            # #### upsample nearest 2D #####
+            input_cpu = torch.randn(
+                (2, 3, 5, 5), dtype=torch.float32, device=cpu_device
+            )
+            input_dpcpp = input_cpu.to("xpu")
+            scales = [6, 8]
+            input_cpu.requires_grad = True
+            input_dpcpp.requires_grad = True
+            alc = False
+
+            output_cpu = torch.nn.functional.interpolate(
+                input_cpu,
+                scale_factor=scales,
+                mode="nearest",
+                recompute_scale_factor=rsf,
+            )
+            output_dpcpp = torch.nn.functional.interpolate(
+                input_dpcpp,
+                scale_factor=scales,
+                mode="nearest",
+                recompute_scale_factor=rsf,
+            )
+            self.assertEqual(output_cpu, output_dpcpp.cpu())
+
+            grad_out_cpu = torch.randn(
+                (2, 3, 30, 40), dtype=torch.float32, device=cpu_device
+            )
+            grad_out_dpcpp = grad_out_cpu.to("xpu")
+            grad_out_cpu = Variable(grad_out_cpu, requires_grad=True)
+            grad_out_dpcpp = Variable(grad_out_dpcpp, requires_grad=True)
+
+            output_cpu.backward(grad_out_cpu)
+            output_dpcpp.backward(grad_out_dpcpp)
+            grad_cpu = input_cpu.grad
+            grad_dpcpp = input_dpcpp.grad
+            self.assertEqual(grad_cpu, grad_dpcpp.cpu())
+
+        for dtype in test_dtypes:
+            test_upsamle_nearest(dtype)
+            test_upsample_nearest_exact(dtype)

src/ATen/native/xpu/UpSampleNearest1d.cpp
@@ -1,0 +1,173 @@
+#include <ATen/ATen.h>
+#include <ATen/core/Tensor.h>
+#include <ATen/native/xpu/sycl/UpSampleNearest1dKernels.h>
+#include <ATen/xpu/XPUNativeFunctions.h>
+
+namespace at {
+
+Tensor& upsample_nearest1d_meta(
+    const Tensor& input,
+    Tensor& output,
+    IntArrayRef output_size) {
+  auto input_size = input.sizes();
+  TORCH_CHECK(
+      output_size.size() == 1,
+      "It is expected output_size equals to 1, but got size ",
+      output_size.size());
+
+  TORCH_CHECK(
+      input_size.size() == 3,
+      "It is expected input_size equals to 3, but got size ",
+      input_size.size());
+
+  int64_t output_width = output_size[0];
+
+  int64_t nbatch = input_size[0];
+  int64_t channels = input_size[1];
+  int64_t input_width = input_size[2];
+
+  TORCH_CHECK(
+      input_width > 0 && output_width > 0,
+      "Input and output sizes should be greater than 0, but got input (W: ",
+      input_width,
+      ") and output (W: ",
+      output_width,
+      ")");
+  TORCH_CHECK(
+      (input.size(1) != 0 && input.size(2) != 0) && input.dim() == 3,
+      "Non-empty 3D data tensor expected but got a tensor with sizes ",
+      input.sizes());
+
+  if (!output.defined())
+    output = at::empty({nbatch, channels, output_width}, input.options());
+  return output;
+}
+
+Tensor& upsample_nearest1d_backward_meta(
+    const Tensor& grad_output,
+    Tensor& grad_input,
+    IntArrayRef input_size,
+    IntArrayRef output_size) {
+  TORCH_CHECK(
+      output_size.size() == 1,
+      "It is expected output_size equals to 1, but got size ",
+      output_size.size());
+  TORCH_CHECK(
+      input_size.size() == 3,
+      "It is expected input_size equals to 3, but got size ",
+      input_size.size());
+  int64_t output_width = output_size[0];
+  int64_t nbatch = input_size[0];
+  int64_t channels = input_size[1];
+  int64_t input_width = input_size[2];
+  TORCH_CHECK(
+      input_width > 0 && output_width > 0,
+      "Input and output sizes should be greater than 0, but got input (W: ",
+      input_width,
+      ") and output (W: ",
+      output_width,
+      ")");
+  check_dim_size(grad_output, 3, 0, nbatch);
+  check_dim_size(grad_output, 3, 1, channels);
+  check_dim_size(grad_output, 3, 2, output_width);
+  if (!grad_input.defined())
+    grad_input = at::empty(input_size, grad_output.options());
+  return grad_input;
+}
+
+Tensor XPUNativeFunctions::_upsample_nearest_exact1d(
+    const Tensor& input,
+    IntArrayRef output_size,
+    c10::optional<double> scales) {
+  Tensor output;
+  output = upsample_nearest1d_meta(input, output, output_size);
+  at::native::xpu::upsample_nearest1d_kernel(
+      output, input, output_size, scales, true);
+  return output;
+}
+
+Tensor& XPUNativeFunctions::_upsample_nearest_exact1d_out(
+    const Tensor& input,
+    IntArrayRef output_size,
+    c10::optional<double> scales,
+    Tensor& output) {
+  upsample_nearest1d_meta(input, output, output_size);
+  at::native::xpu::upsample_nearest1d_kernel(
+      output, input, output_size, scales, true);
+  return output;
+}
+
+Tensor XPUNativeFunctions::upsample_nearest1d(
+    const Tensor& input,
+    IntArrayRef output_size,
+    c10::optional<double> scales) {
+  Tensor output;
+  output = upsample_nearest1d_meta(input, output, output_size);
+  at::native::xpu::upsample_nearest1d_kernel(
+      output, input, output_size, scales, false);
+  return output;
+}
+
+Tensor& XPUNativeFunctions::upsample_nearest1d_out(
+    const Tensor& input,
+    IntArrayRef output_size,
+    c10::optional<double> scales,
+    Tensor& output) {
+  upsample_nearest1d_meta(input, output, output_size);
+  at::native::xpu::upsample_nearest1d_kernel(
+      output, input, output_size, scales, false);
+  return output;
+}
+
+Tensor XPUNativeFunctions::_upsample_nearest_exact1d_backward(
+    const Tensor& grad_output,
+    IntArrayRef output_size,
+    IntArrayRef input_size,
+    std::optional<double> scales) {
+  Tensor grad_input;
+  grad_input = upsample_nearest1d_backward_meta(
+      grad_output, grad_input, input_size, output_size);
+  at::native::xpu::upsample_nearest1d_backward_kernel(
+      grad_input, grad_output, output_size, input_size, scales, true);
+  return grad_input;
+}
+Tensor& XPUNativeFunctions::_upsample_nearest_exact1d_backward_out(
+    const Tensor& grad_output,
+    IntArrayRef output_size,
+    IntArrayRef input_size,
+    std::optional<double> scales,
+    Tensor& grad_input) {
+  upsample_nearest1d_backward_meta(
+      grad_output, grad_input, input_size, output_size);
+  at::native::xpu::upsample_nearest1d_backward_kernel(
+      grad_input, grad_output, output_size, input_size, scales, true);
+  return grad_input;
+}
+
+Tensor XPUNativeFunctions::upsample_nearest1d_backward(
+    const Tensor& grad_output,
+    IntArrayRef output_size,
+    IntArrayRef input_size,
+    std::optional<double> scales) {
+  Tensor grad_input;
+  grad_input = upsample_nearest1d_backward_meta(
+      grad_output, grad_input, input_size, output_size);
+  at::native::xpu::upsample_nearest1d_backward_kernel(
+      grad_input, grad_output, output_size, input_size, scales, false);
+  return grad_input;
+}
+
+Tensor& XPUNativeFunctions::upsample_nearest1d_backward_out(
+    const Tensor& grad_output,
+    IntArrayRef output_size,
+    IntArrayRef input_size,
+    std::optional<double> scales,
+    Tensor& grad_input) {
+  upsample_nearest1d_backward_meta(
+      grad_output, grad_input, input_size, output_size);
+  at::native::xpu::upsample_nearest1d_backward_kernel(
+      grad_input, grad_output, output_size, input_size, scales, true);
+  return grad_input;
+}
+
+} // namespace at

commit 90352fe4ec065c2bd2275d36e2f00afc879c2ee0

build: fix warning in UpSampleNearest1dKernels.cpp
Fixes: #475

src/ATen/native/xpu/sycl/UpSampleNearest1dKernels.cpp:278:15: warning: typedef ‘using accscalar_t = at::acc_type<double, true>’ locally defined but not used [-Wunused-local-typedefs]
  278 |         using accscalar_t = acc_type<scalar_t, true>;

Fixes: d0d350e ("Add aten::upsample_nearest series of ops (#373)")
Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

src/ATen/native/xpu/sycl/UpSampleNearest1dKernels.cpp
@@ -275,8 +275,6 @@ void upsample_nearest1d_kernel(
       input.scalar_type(),
       "upsample_nearest1d_xpu",
       [&] {
-        using accscalar_t = acc_type<scalar_t, true>;
-
         auto idata = input.data_ptr<scalar_t>();
         auto odata = output_c.data_ptr<scalar_t>();
 