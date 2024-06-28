7f06e1a3d614e57553009d50524417d625d5c92e
Feng Yuan <feng1.yuan@intel.com>
2024-06-29 05:23:55 +0800

    Renaming variable type name

--- src/ATen/native/xpu/sycl/ActivationHardsigmoidKernels.cpp
+++ src/ATen/native/xpu/sycl/ActivationHardsigmoidKernels.cpp
@@ -6,25 +6,25 @@
 
 namespace at::native::xpu {
 
-template <typename scalar_t, typename accscalar_t>
+template <typename scalar_t, typename opmath_t>
 struct HardsigmoidFunctor {
   scalar_t operator()(scalar_t self_val) const {
-    accscalar_t x = static_cast<accscalar_t>(self_val);
+    opmath_t x = static_cast<opmath_t>(self_val);
     return std::min(std::max(x + three_, zero_), six_) * one_sixth_;
   }
 
   HardsigmoidFunctor(
-      const accscalar_t zero,
-      const accscalar_t one_sixth,
-      const accscalar_t three,
-      const accscalar_t six)
+      const opmath_t zero,
+      const opmath_t one_sixth,
+      const opmath_t three,
+      const opmath_t six)
       : zero_(zero), one_sixth_(one_sixth), three_(three), six_(six) {}
 
  private:
-  const accscalar_t zero_;
-  const accscalar_t one_sixth_;
-  const accscalar_t three_;
-  const accscalar_t six_;
+  const opmath_t zero_;
+  const opmath_t one_sixth_;
+  const opmath_t three_;
+  const opmath_t six_;
 };
 
 void hardsigmoid_kernel(TensorIteratorBase& iter) {
@@ -34,41 +34,41 @@ void hardsigmoid_kernel(TensorIteratorBase& iter) {
       iter.dtype(),
       "hardsigmoid_xpu",
       [&]() {
-        using accscalar_t = at::opmath_type<scalar_t>;
-        const accscalar_t zero(0.0f);
-        const accscalar_t one_sixth(1.0f / 6.0f);
-        const accscalar_t three(3.0f);
-        const accscalar_t six(6.0f);
-        HardsigmoidFunctor<scalar_t, accscalar_t> f(
+        using opmath_t = at::opmath_type<scalar_t>;
+        const opmath_t zero(0.0f);
+        const opmath_t one_sixth(1.0f / 6.0f);
+        const opmath_t three(3.0f);
+        const opmath_t six(6.0f);
+        HardsigmoidFunctor<scalar_t, opmath_t> f(
             zero, one_sixth, three, six);
         gpu_kernel(iter, f);
       });
 }
 
-template <typename scalar_t, typename accscalar_t>
+template <typename scalar_t, typename opmath_t>
 struct HardsigmoidBackwardFunctor {
   scalar_t operator()(scalar_t grad_val_, scalar_t self_val_) const {
-    accscalar_t grad_val = static_cast<accscalar_t>(grad_val_);
-    accscalar_t self_val = static_cast<accscalar_t>(self_val_);
+    opmath_t grad_val = static_cast<opmath_t>(grad_val_);
+    opmath_t self_val = static_cast<opmath_t>(self_val_);
     return (self_val > neg_three_ && self_val < three_) ? grad_val * one_sixth_
                                                         : zero_;
   }
 
   HardsigmoidBackwardFunctor(
-      const accscalar_t zero,
-      const accscalar_t three,
-      const accscalar_t neg_three,
-      const accscalar_t one_sixth)
+      const opmath_t zero,
+      const opmath_t three,
+      const opmath_t neg_three,
+      const opmath_t one_sixth)
       : zero_(zero),
         three_(three),
         neg_three_(neg_three),
         one_sixth_(one_sixth) {}
 
  private:
-  const accscalar_t zero_;
-  const accscalar_t three_;
-  const accscalar_t neg_three_;
-  const accscalar_t one_sixth_;
+  const opmath_t zero_;
+  const opmath_t three_;
+  const opmath_t neg_three_;
+  const opmath_t one_sixth_;
 };
 
 void hardsigmoid_backward_kernel(TensorIteratorBase& iter) {
@@ -78,12 +78,12 @@ void hardsigmoid_backward_kernel(TensorIteratorBase& iter) {
       iter.dtype(),
       "hardsigmoid_backward_xpu",
       [&]() {
-        using accscalar_t = at::opmath_type<scalar_t>;
-        const accscalar_t zero(0.0f);
-        const accscalar_t three(3.0f);
-        const accscalar_t neg_three(-3.0f);
-        const accscalar_t one_sixth(1.0f / 6.0f);
-        HardsigmoidBackwardFunctor<scalar_t, accscalar_t> f(
+        using opmath_t = at::opmath_type<scalar_t>;
+        const opmath_t zero(0.0f);
+        const opmath_t three(3.0f);
+        const opmath_t neg_three(-3.0f);
+        const opmath_t one_sixth(1.0f / 6.0f);
+        HardsigmoidBackwardFunctor<scalar_t, opmath_t> f(
             zero, three, neg_three, one_sixth);
         gpu_kernel(iter, f);
       });
