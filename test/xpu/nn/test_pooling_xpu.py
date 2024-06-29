afe8a684d2dab8933b88adb46db5313b5c725137
Feng Yuan <feng1.yuan@intel.com>
2024-06-30 05:37:55 +0800

    Remove unnecessary code

--- test/xpu/nn/test_pooling_xpu.py
+++ test/xpu/nn/test_pooling_xpu.py
@@ -1,6 +1,7 @@
 # Owner(s): ["module: intel"]
+
 import torch
-from torch.testing._internal.common_device_type import dtypes, instantiate_device_type_tests
+from torch.testing._internal.common_device_type import instantiate_device_type_tests
 from torch.testing._internal.common_utils import run_tests, instantiate_parametrized_tests
 
 try:
