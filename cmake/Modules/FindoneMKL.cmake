7ede913abe48a152c037a3a4cee76b1fc59a1783
Cui, Yifeng <yifeng.cui@intel.com>
2024-07-02 07:26:58 -0700

    Refine MKL cmake

--- cmake/Modules/FindoneMKL.cmake
+++ cmake/Modules/FindoneMKL.cmake
@@ -1,14 +1,24 @@
-set(mkl_root_hint $ENV{MKLROOT})
-
-set(CMAKE_INCLUDE_PATH ${CMAKE_INCLUDE_PATH}
-    "${mkl_root_hint}/include")
-set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH}
-    "${mkl_root_hint}/lib")
-
 set(ONEMKL_LIBRARIES)
 
-list(APPEND ONEMKL_LIBRARIES "${mkl_root_hint}/lib/libmkl_intel_lp64.so")
-list(APPEND ONEMKL_LIBRARIES "${mkl_root_hint}/lib/libmkl_gnu_thread.so")
-list(APPEND ONEMKL_LIBRARIES "${mkl_root_hint}/lib/libmkl_core.so")
-list(APPEND ONEMKL_LIBRARIES "${mkl_root_hint}/lib/libmkl_sycl_dft.so")
+if(DEFINED ENV{MKLROOT})
+  SET(INTEL_MKL_DIR $ENV{MKLROOT})
+else()
+  SET(INTEL_MKL_DIR "/opt/intel/oneapi/mkl/latest")
+endif()
+
+set(CMAKE_INCLUDE_PATH ${CMAKE_INCLUDE_PATH}
+    "${INTEL_MKL_DIR}/include")
+set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH}
+    "${INTEL_MKL_DIR}/lib")
+
+set(MKL_LIB_NAMES "mkl_intel_lp64" "mkl_gnu_thread" "mkl_core" "mkl_sycl_dft")
+
+foreach(LIB_NAME IN LISTS MKL_LIB_NAMES)
+  find_library(
+    ${LIB_NAME}_library
+    NAMES ${LIB_NAME}
+    HINTS ${INTEL_MKL_DIR}
+  )
+  list(APPEND ONEMKL_LIBRARIES ${${LIB_NAME}_library})
+endforeach()
 
