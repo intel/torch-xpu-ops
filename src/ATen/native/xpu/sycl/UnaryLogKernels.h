e8b9fbc2fc12cb354066114f4c642980aae07979
yucai <yu.cai@intel.com>
2024-07-03 03:05:58 +0000

    Revert "add lop10/log1p/log2"
    This reverts commit 8f27bad461fb1d5b093425d2e8397b4f49790b78.

--- src/ATen/native/xpu/sycl/UnaryLogKernels.h
+++ src/ATen/native/xpu/sycl/UnaryLogKernels.h
@@ -6,10 +6,4 @@ namespace at::native::xpu {
 
 void log_kernel(TensorIteratorBase& iter);
 
-void log10_kernel(TensorIteratorBase& iter);
-
-void log1p_kernel(TensorIteratorBase& iter);
-
-void log2_kernel(TensorIteratorBase& iter);
-
 } // namespace at::native::xpu
