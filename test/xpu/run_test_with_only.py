45697d7357efcca5d47ad5550f733cd869df65b3
Huaiyu, Zheng <huaiyu.zheng@intel.com>
2024-07-04 03:38:46 +0000

    rmv duplicated case

--- test/xpu/run_test_with_only.py
+++ test/xpu/run_test_with_only.py
@@ -46,7 +46,7 @@ execute_list = (
     "test_comprehensive_nn_functional_cross_entropy_xpu",
     "test_comprehensive_nn_functional_nll_loss_xpu_bfloat16",
     "test_comprehensive_nn_functional_nll_loss_xpu_float32",
-    "test_comprehensive_nn_functional_nll_loss_xpu_float64"
+    "test_comprehensive_nn_functional_nll_loss_xpu_float64",
     "bincount",
 )
 res += launch_test("test_decomp_xpu.py", exe_list=execute_list)
