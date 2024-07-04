6fa53f1a6fdd1cad2f658800717982f42eb9e3c2
Deng, Daisy <daisy.deng@intel.com>
2024-07-03 19:12:49 -0700

    fix the patch

--- test/xpu/test_torch_xpu.py
+++ test/xpu/test_torch_xpu.py
@@ -5664,9 +5664,9 @@ else:
 
         for mf, shape in formats_shapes:
             self._test_memory_format_transformations(
-                'cpu', get_generator(mf, shape), transformation_cuda_fn, mf, default_is_preserve=True)
+                'cpu', get_generator(mf, shape), transformation_xpu_fn, mf, default_is_preserve=True)
             self._test_memory_format_transformations(
-                'xpu', get_generator(mf, shape), transformation_xpu_fn, mf, default_is_preserve=True)
+                'xpu', get_generator(mf, shape), transformation_cpu_fn, mf, default_is_preserve=True)
 
     # FIXME: move to test_serialization
     @onlyNativeDeviceTypes
