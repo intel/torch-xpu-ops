92a43aae3130fe71d186798ab601ae3d0fb7c1d7
xytintel <yutao.xu@intel.com>
2024-07-10 08:48:48 +0000

    add huber loss

--- src/ATen/native/xpu/sycl/BinaryMiscOpsKernels.cpp
+++ src/ATen/native/xpu/sycl/BinaryMiscOpsKernels.cpp
@@ -23,4 +23,25 @@ void mse_kernel(TensorIteratorBase& iter) {
       [&]() { gpu_kernel(iter, MSEFunctor<scalar_t>()); });
 }
 
+template <typename scalar_t>
+struct HuberFunctor {
+  scalar_t operator()(scalar_t a, scalar_t b) const {
+    auto z = ::abs(a - b);
+    return z < delta_val_ ? scalar_t(0.5) * z * z
+                          : delta_val_ * (z - scalar_t(0.5) * delta_val_);
+  }
+  HuberFunctor(scalar_t delta_val) : delta_val_(delta_val) {}
+
+ private:
+  scalar_t delta_val_;
+};
+
+void huber_kernel(TensorIterator& iter, double delta) {
+  AT_DISPATCH_FLOATING_TYPES_AND2(
+      kBFloat16, kHalf, iter.dtype(), "huber_xpu", [&iter, delta] {
+        scalar_t delta_val(delta);
+        gpu_kernel(iter, HuberFunctor<scalar_t>(delta_val));
+      });
+}
+
 } // namespace at::native::xpu
