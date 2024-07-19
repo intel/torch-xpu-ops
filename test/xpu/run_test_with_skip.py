45edd38a239b7fb5d7b301569ca7b2844213a970
Feng Yuan <feng1.yuan@intel.com>
2024-07-20 04:38:32 +0800

    Skip case due to different implementation

--- test/xpu/run_test_with_skip.py
+++ test/xpu/run_test_with_skip.py
@@ -1527,6 +1527,26 @@ skip_list = (
     # Absolute difference: 3.063072442997111e-08 (up to 0.0 allowed)
     # Relative difference: 6.156719153309558e-06 (up to 1e-06 allowed)
     "test_log1p_complex_xpu_complex64",
+
+    # CPU MKL::erfinv vs XPU impl. At most 6.e-06
+    # Greatest absolute difference: 5.250126961175994e-06 at index (0,) (up to 1e-07 allowed)
+    # Greatest relative difference: 1.680894105274219e-06 at index (0,) (up to 1e-07 allowed)
+    "test_reference_numerics_large__refs_erfinv_xpu_float64"
+    # Greatest absolute difference: 5.250126961175994e-06 at index (0,) (up to 1e-07 allowed)
+    # Greatest relative difference: 1.680894105274219e-06 at index (0,) (up to 1e-07 allowed)
+    "test_reference_numerics_large_erfinv_xpu_float64",
+    # Greatest absolute difference: 4.829411781148707e-06 at index (690, 855) (up to 1e-07 allowed)
+    # Greatest relative difference: 1.5588752485769885e-06 at index (690, 855) (up to 1e-07 allowed)
+    "test_reference_numerics_normal__refs_erfinv_xpu_float64",
+    # Greatest absolute difference: 4.829411781148707e-06 at index (690, 855) (up to 1e-07 allowed)
+    # Greatest relative difference: 1.5588752485769885e-06 at index (690, 855) (up to 1e-07 allowed)
+    "test_reference_numerics_normal_erfinv_xpu_float64",
+    # Greatest absolute difference: 5.250126961175994e-06 at index (96,) (up to 1e-07 allowed)
+    # Greatest relative difference: 1.680894105274219e-06 at index (96,) (up to 1e-07 allowed)
+    "test_reference_numerics_small__refs_erfinv_xpu_float64",
+    # Greatest absolute difference: 5.250126961175994e-06 at index (96,) (up to 1e-07 allowed)
+    # Greatest relative difference: 1.680894105274219e-06 at index (96,) (up to 1e-07 allowed)
+    "test_reference_numerics_small_erfinv_xpu_float64",
 )
 res += launch_test("test_unary_ufuncs_xpu.py", skip_list)
 
