9c3784fbce3afb62b54449b59a96f27a05fb9287
Feng Yuan <feng1.yuan@intel.com>
2024-07-22 05:14:58 +0800

    Compilation issue

--- src/ATen/native/xpu/sycl/LossKernels.h
+++ src/ATen/native/xpu/sycl/LossKernels.h
@@ -3,32 +3,19 @@
 
 namespace at::native::xpu {
 
-Tensor& binary_cross_entropy_out_kernel(
+Tensor& binary_cross_entropy_kernel(
     const Tensor& input,
     const Tensor& target,
-    const std::optional<Tensor>& weight_opt,
+    const Tensor& weight,
     int64_t reduction,
     Tensor& loss);
 
-Tensor binary_cross_entropy_kernel(
-    const Tensor& input,
-    const Tensor& target,
-    const std::optional<Tensor>& weight_opt,
-    int64_t reduction);
-
-Tensor& binary_cross_entropy_backward_out_kernel(
+Tensor& binary_cross_entropy_backward_kernel(
     const Tensor& grad,
     const Tensor& input,
     const Tensor& target,
-    const std::optional<Tensor>& weight_opt,
+    const Tensor& weight,
     int64_t reduction,
     Tensor& grad_input);
-
-Tensor binary_cross_entropy_backward_kernel(
-    const Tensor& grad,
-    const Tensor& input,
-    const Tensor& target,
-    const std::optional<Tensor>& weight_opt,
-    int64_t reduction);
 
 } // namespace at::native::xpu
