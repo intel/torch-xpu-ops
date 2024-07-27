c2405f25a2a886b91f5a84432721511db977a5dd
Yutao Xu <yutao.xu@intel.com>
2024-07-27 16:44:35 +0800

    Add aten::nansum and aten::nansum.out (#567)
    Add aten::nansum and aten::nansum.out.
    
    ---------
    
    Co-authored-by: Feng Yuan <feng1.yuan@intel.com>

--- test/xpu/xpu_test_utils.py
+++ test/xpu/xpu_test_utils.py
@@ -136,6 +136,7 @@ _xpu_computation_op_list = [
     "atanh",
     "sqrt",
     "sum",
+    "nansum",
     "amin",
     "amax",
     "std",
@@ -218,6 +219,7 @@ _xpu_computation_op_list = [
     "copysign",
     "count_nonzero",
     "nan_to_num",
+    "nanmean",
 ]
 
 
