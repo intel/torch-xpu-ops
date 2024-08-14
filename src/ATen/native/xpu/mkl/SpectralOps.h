751832baa45febb4ee660d8a88f7b2b303748ef6
Cui, Yifeng <yifeng.cui@intel.com>
2024-08-13 22:38:40 -0700

    Init MKL for Pytorch XPU and enable fft_c2c
    Update FFT integration code
    
    Fix cow tensor
    
    Refine build configure for Linux/Windows
    
    Fix skip UT list
    
    Refine code style
    
    Enable MKL in CI
    
    Add ENV USE_ONEMKL to control whether to build with ONEMKL or not
    
    Small fix

---
+++ src/ATen/native/xpu/mkl/SpectralOps.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <ATen/core/Tensor.h>
+
+namespace at::native::xpu {
+
+TORCH_XPU_API Tensor _fft_c2c_mkl(
+    const Tensor& self,
+    IntArrayRef dim,
+    int64_t normalization,
+    bool forward);
+
+TORCH_XPU_API Tensor& _fft_c2c_mkl_out(
+    const Tensor& self,
+    IntArrayRef dim,
+    int64_t normalization,
+    bool forward,
+    Tensor& out);
+
+} // namespace at::native::xpu
