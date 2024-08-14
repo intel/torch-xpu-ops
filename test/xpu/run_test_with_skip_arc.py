1c8dd3c62d29942136b1aca3c08196287d241f4e
Cheng, Penghui <penghui.cheng@intel.com>
2024-08-14 14:30:26 +0800

    Separate skip cases list by platform name (#740)
    Separate skip cases list by platform name: "Arc", "MTL", "PVC".
    
    ---------
    
    Signed-off-by: Cheng Penghui <penghui.cheng@intel.com>

--- test/xpu/run_test_with_skip_arc.py
+++ test/xpu/run_test_with_skip_arc.py
@@ -1,16 +1,15 @@
 import os
 import sys
 from skip_list_common import skip_dict
+from skip_list_arc import skip_dict as skip_dict_specifical
 from xpu_test_utils import launch_test
 
 
 res = 0
 
 for key in skip_dict:
-    skip_list = skip_dict[key]
+    skip_list = skip_dict[key] if key not in skip_dict_specifical else skip_dict[key] + skip_dict_specifical[key]
     res += launch_test(key, skip_list)
-
-res += launch_test("test_segment_reductions_xpu.py")
 
 exit_code = os.WEXITSTATUS(res)
 sys.exit(exit_code)
