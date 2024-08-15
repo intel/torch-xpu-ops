e0b1846be4140eb203dd9117294f48ef010d785d
yuchengliu1 <yucheng.liu@intel.com>
2024-08-16 03:27:58 +0800

    add fail test hint

--- test/xpu/run_test_with_skip.py
+++ test/xpu/run_test_with_skip.py
@@ -5,10 +5,16 @@ from xpu_test_utils import launch_test
 
 
 res = 0
+fail_test = []
 
 for key in skip_dict:
     skip_list = skip_dict[key]
-    res += launch_test(key, skip_list)
+    fail = launch_test(key, skip_list)
+    res += fail
+    if fail:
+        fail_test.append(key)
+
+print(",".join(fail_test)+" have failures")
 
 exit_code = os.WEXITSTATUS(res)
 sys.exit(exit_code)
