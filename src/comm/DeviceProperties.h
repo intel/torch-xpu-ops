86c65e760f835b7521817eea42de89e410d36523
Feng Yuan <feng1.yuan@intel.com>
2024-08-16 22:12:16 +0800

    SYCL: Walk around runtime error of launching kernel on xpu:1 when querying SYCL kernel bundle ahead
    Signed-off-by: Feng Yuan <feng1.yuan@intel.com>

--- src/comm/DeviceProperties.h
+++ src/comm/DeviceProperties.h
@@ -16,8 +16,8 @@ static int64_t syclMaxWorkGroupSize(
   auto dev = q.get_device();
 
   auto kid = ::sycl::get_kernel_id<KernelClass>();
-  auto kbundle =
-      ::sycl::get_kernel_bundle<::sycl::bundle_state::executable>(ctx, {kid});
+  auto kbundle = ::sycl::get_kernel_bundle<::sycl::bundle_state::executable>(
+      ctx, {dev} {kid});
 
   ::sycl::kernel k = kbundle.get_kernel(kid);
   return k.get_info<::sycl::info::kernel_device_specific::work_group_size>(dev);
