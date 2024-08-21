8738bd7c8af5c4d9538bd5206882b70b7113a612
sandeep.polisetty@intel.com <sandeep.polisetty@intel.com>
2024-08-21 15:07:15 -0700

    minor

--- test/xpu/extended/run_test_with_skip.py
+++ test/xpu/extended/run_test_with_skip.py
@@ -189,8 +189,8 @@ for skip_case in skip_list[1:]:
     skip_options += skip_option
 skip_options += "\""
 
-os.environ["PYTORCH_ENABLE_XPU_FALLBACK"]= 1
-os.environ["PYTORCH_TEST_WITH_SLOW"]=1
+os.environ["PYTORCH_ENABLE_XPU_FALLBACK"]="1"
+os.environ["PYTORCH_TEST_WITH_SLOW"]="1"
 
 test_command = "pytest -v test_ops_xpu.py"
 test_command += skip_options
