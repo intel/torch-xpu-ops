efb9c10b4ad202d2eb7467f07854470f044c6598
cfgfung <cfgfung@gmail.com>
2024-08-26 15:52:51 -0700

    Added a skip list for ARC display card on windows platform.

--- test/xpu/run_test_with_skip_arc.py
+++ test/xpu/run_test_with_skip_arc.py
@@ -3,6 +3,7 @@ import sys
 from skip_list_common import skip_dict
 from skip_list_arc import skip_dict as skip_dict_specifical
 from skip_list_win import skip_dict as skip_dict_win
+from skip_list_win_arc import skip_dict as skip_dict_win_arc
 from xpu_test_utils import launch_test
 
 
@@ -15,6 +16,8 @@ for key in skip_dict:
         skip_list += skip_dict_specifical[key]
     if IS_WINDOWS and key in skip_dict_win:
         skip_list += skip_dict_win[key]
+    if IS_WINDOWS and key in skip_dict_win_arc:
+        skip_list += skip_dict_win_arc[key]
     res += launch_test(key, skip_list)
 
 exit_code = os.WEXITSTATUS(res)
