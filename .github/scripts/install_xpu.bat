310467aa1306072e616b9d6073dc6ec599af8a60
Zhong, Ruijie <ruijie.zhong@intel.com>
2024-08-27 05:47:39 +0000

    update pti install

--- .github/scripts/install_xpu.bat
+++ .github/scripts/install_xpu.bat
@@ -1,56 +1,68 @@
 REM Description: Install Intel Support Packages on Windows
 REM BKM reference: https://www.intel.com/content/www/us/en/developer/articles/tool/pytorch-prerequisites-for-intel-gpu/2-5.html
-REM To-do: Add driver installation in this file.
 
-set ONEAPI_PARENT_DIR=C:\Program Files (x86)\Intel
-set INTEL_ONEAPI_PYTORCH_BUNDLE_URL=https://registrationcenter-download.intel.com/akdlm/IRC_NAS/9d1a91e2-e8b8-40a5-8c7f-5db768a6a60c/w_intel-for-pytorch-gpu-dev_p_0.5.3.37_offline.exe
-set ONEAPI_VERSION="0.5.3+37"
-set INTEL_ONEAPI_PYTORCH_BUNDLE_DISPLAY_NAME=intel.oneapi.win.intel-for-pytorch-gpu-dev.product
-set INSTALL_FRESH_ONEAPI=0
+:xpu_bundle_install_start
 
-REM Check if oneAPI is already installed
-if not exist "C:\Program Files (x86)\Intel\oneAPI\pytorch-gpu-dev-0.5\oneapi-vars.bat" (
-    set INSTALL_FRESH_ONEAPI=1
-)
+set XPU_BUNDLE_PARENT_DIR=C:\Program Files (x86)\Intel\oneAPI
+set XPU_BUNDLE_URL=https://registrationcenter-download.intel.com/akdlm/IRC_NAS/9d1a91e2-e8b8-40a5-8c7f-5db768a6a60c/w_intel-for-pytorch-gpu-dev_p_0.5.3.37_offline.exe
+set XPU_PTI_URL=https://registrationcenter-download.intel.com/akdlm/IRC_NAS/9d1a91e2-e8b8-40a5-8c7f-5db768a6a60c/w_intel-pti-dev_p_0.9.0.37_offline.exe
+set XPU_BUNDLE_VERSION=0.5.3+31
+set XPU_PTI_VERSION=0.9.0+36
+set XPU_BUNDLE_PRODUCT_NAME=intel.oneapi.win.intel-for-pytorch-gpu-dev.product
+set XPU_PTI_PRODUCT_NAME=intel.oneapi.win.intel-pti-dev.product
+set XPU_BUNDLE_INSTALLED=0
+set XPU_PTI_INSTALLED=0
+set XPU_BUNDLE_UNINSTALL=0
+set XPU_PTI_UNINSTALL=0
 
-REM Check if oneAPI is latest version
-"%ONEAPI_PARENT_DIR%\oneAPI\Installer\installer.exe" --list-products > tmp_oneapi_uninstall_version.log
-for /f "tokens=1,2" %%a in (tmp_oneapi_uninstall_version.log) do (
-    if "%%a"=="%INTEL_ONEAPI_PYTORCH_BUNDLE_DISPLAY_NAME%" (
-        set "CURRENT_VERSION=%%b"
+:: Check if XPU bundle is target version or already installed
+if exist "%XPU_BUNDLE_PARENT_DIR%\Installer\installer.exe" goto xpu_bundle_ver_check
+goto xpu_bundle_install
+
+:xpu_bundle_ver_check
+
+"%XPU_BUNDLE_PARENT_DIR%\Installer\installer.exe" --list-products > xpu_bundle_installed_ver.log
+
+for /f "tokens=1,2" %%a in (xpu_bundle_installed_ver.log) do (
+    if "%%a"=="%XPU_BUNDLE_PRODUCT_NAME%" (
+        echo %%a Installed Version: %%b
+	    set XPU_BUNDLE_INSTALLED=1
+        if not "%XPU_BUNDLE_VERSION%"=="%%b" (
+            start /wait "Installer Title" "%XPU_BUNDLE_PARENT_DIR%\Installer\installer.exe" --action=remove --eula=accept --silent --product-id %XPU_BUNDLE_PRODUCT_NAME% --product-ver %%b --log-dir uninstall_bundle
+            set XPU_BUNDLE_UNINSTALL=1
+        )
+    )
+    if "%%a"=="%XPU_PTI_PRODUCT_NAME%" (
+        echo %%a Installed Version: %%b
+	    set XPU_PTI_INSTALLED=1
+        if not "%XPU_PTI_VERSION%"=="%%b" (
+            start /wait "Installer Title" "%XPU_BUNDLE_PARENT_DIR%\Installer\installer.exe" --action=remove --eula=accept --silent --product-id %XPU_PTI_PRODUCT_NAME% --product-ver %%b --log-dir uninstall_bundle
+            set XPU_PTI_UNINSTALL=1
+        )
     )
 )
+if errorlevel 1 exit /b 1
+if exist xpu_bundle_installed_ver.log del xpu_bundle_installed_ver.log
+if "%XPU_BUNDLE_INSTALLED%"=="0" goto xpu_bundle_install
+if "%XPU_BUNDLE_UNINSTALL%"=="1" goto xpu_bundle_install
+if "%XPU_PTI_INSTALLED%"=="0" goto xpu_pti_install
+if "%XPU_PTI_UNINSTALL%"=="1" goto xpu_pti_install
+goto xpu_install_end
 
-if not "%CURRENT_VERSION%"=="%ONEAPI_VERSION%" (
-    set INSTALL_FRESH_ONEAPI=1
-)
+:xpu_bundle_install
 
+curl -o xpu_bundle.exe --retry 3 --retry-all-errors -k %XPU_BUNDLE_URL%
+echo "XPU Bundle installing..."
+start /wait "Intel Pytorch Bundle Installer" "xpu_bundle.exe" --action=install --eula=accept --silent --log-dir install_bundle
+if errorlevel 1 exit /b 1
+del xpu_bundle.exe
 
-if "%INSTALL_FRESH_ONEAPI%"=="1" (
-    IF EXIST "%ONEAPI_PARENT_DIR%\oneAPI\Installer\installer.exe" (
-        "%ONEAPI_PARENT_DIR%\oneAPI\Installer\installer.exe" --list-products > oneapi_products_before_uninstall.log
-        IF EXIST tmp_oneapi_uninstall_version.log (
-            del tmp_oneapi_uninstall_version.log
-        )
-    
-        "%ONEAPI_PARENT_DIR%\oneAPI\Installer\installer.exe" --list-products > tmp_oneapi_uninstall_version.log
-        for /f "tokens=1,2" %%a in (tmp_oneapi_uninstall_version.log) do (
-            if "%%a"=="%INTEL_ONEAPI_PYTORCH_BUNDLE_DISPLAY_NAME%" (
-                echo Version: %%b
-                start /wait "Installer Title" "%ONEAPI_PARENT_DIR%\oneAPI\Installer\installer.exe" --action=remove --eula=accept --silent --product-id %INTEL_ONEAPI_PYTORCH_BUNDLE_DISPLAY_NAME% --product-ver %%b --log-dir uninstall_bundle
-            )
-        )
-    
-        IF EXIST tmp_oneapi_uninstall_version.log (
-            del tmp_oneapi_uninstall_version.log
-        )
-        if errorlevel 1 exit /b
-        if not errorlevel 0 exit /b
-    )
+:xpu_pti_install
 
-    curl -o oneapi_bundle.exe --retry 3 --retry-all-errors -k %INTEL_ONEAPI_PYTORCH_BUNDLE_URL%
-    start /wait "Intel Pytorch Bundle Installer" "oneapi_bundle.exe" --action=install --eula=accept --silent --log-dir install_bundle
-    if errorlevel 1 exit /b
-    if not errorlevel 0 exit /b
-    rm oneapi_bundle.exe
-)
+curl -o xpu_pti.exe --retry 3 --retry-all-errors -k %XPU_PTI_URL%
+echo "XPU PTI installing..."
+start /wait "Intel PTI Installer" "xpu_pti.exe" --action=install --eula=accept --silent --log-dir install_bundle
+if errorlevel 1 exit /b 1
+del xpu_pti.exe
+
+:xpu_install_end
