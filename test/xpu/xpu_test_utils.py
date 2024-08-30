f8a395e3f8ba5c162301baa0769c4c5de39c0ff5
Daisy Deng <daisy.deng@intel.com>
2024-08-30 14:11:38 +0800

    Added hooks to avoid fp64 input on ARC (#843)
    Add hooks to avoid the sample inputs on ARC.
    
    ---------
    
    Co-authored-by: Zhong, Ruijie <ruijie.zhong@intel.com>

--- test/xpu/xpu_test_utils.py
+++ test/xpu/xpu_test_utils.py
@@ -570,6 +570,98 @@ def CriterionTest_test_xpu(self, test_case, dtype, extra_args=None):
 
 CriterionTest.test_cuda = CriterionTest_test_xpu
 
+from torch.testing._internal.common_methods_invocations import sample_inputs_cat_concat, S, M
+from torch.testing._internal.common_methods_invocations import make_tensor
+from functools import partial
+from torch.testing._internal.opinfo.core import SampleInput
+
+def reference_inputs_cat_nofp64(op, device, dtype, requires_grad, **kwargs):
+    yield from sample_inputs_cat_concat(op, device, dtype, requires_grad, **kwargs)
+
+    make_arg = partial(make_tensor, device=device, dtype=dtype, requires_grad=requires_grad)
+
+    # Noncontiguous type promoting tensors
+    a = make_arg((3, 4, 2))
+    #b = make_arg((3, 2, 2), noncontiguous=True, dtype=torch.double)
+    # for platform without fp64 support
+    b = make_arg((3, 2, 2), noncontiguous=True, dtype=torch.float)
+    c = make_arg((3, 3, 2), dtype=torch.float16).permute(1, 0, 2)
+
+    yield SampleInput((a, b, c), kwargs={'dim': 1})
+
+    # Special 1D tensor with dim length of 0 case
+    a = make_arg((0,))
+    b = make_arg((3, 2, 2))
+
+    yield SampleInput((a, b, a))
+    yield SampleInput((a, a, a))
+
+
+def index_variable_nofp64(shape, max_indices, device=torch.device('cpu')):
+    if not isinstance(shape, tuple):
+        shape = (shape,)
+    #index = torch.rand(*shape, dtype=torch.double, device=device).mul_(max_indices).floor_().long()
+    # for platform without fp64 support
+    index = torch.rand(*shape, dtype=torch.float32, device=device).mul_(max_indices).floor_().long()
+    return index
+
+
+def sample_inputs_softmax_variant_nofp64(
+    op_info,
+    device,
+    dtype,
+    requires_grad,
+    with_dtype=False,
+    use_zero_dimensions=True,
+    **kwargs,
+):
+    make_arg = partial(
+        make_tensor, device=device, dtype=dtype, requires_grad=requires_grad
+    )
+    cases = [
+        ((S,), (0,)),
+        ((S, S), (0,)),
+        ((S, S), (1,)),
+        ((S, S), (-1,)),
+        ((S, M, S), (2,)),
+        *([((S, 0, 0), (-1,))] if use_zero_dimensions else []),
+    ]
+    #kwargs = dict(dtype=torch.float64) if with_dtype else None
+    # for platform without fp64 support
+    kwargs = dict(dtype=torch.float32) if with_dtype else None
+
+    # PyTorch on XLA throws an error when passed with dim argument for 0d tensor.
+    # See https://github.com/pytorch/xla/issues/3061 for more details.
+    if torch.device(device).type != "xla":
+        cases.append(((), (0,)))
+
+    return (
+        SampleInput(make_arg(shape), args=dim, kwargs=kwargs) for shape, dim in cases
+    )
+
+def sample_inputs_like_fns_nofp64(self, device, dtype, requires_grad, **kwargs):
+
+    inputs = [
+        ((), {}),
+        ((S, S), {}),
+        ((0, S, 0), {}),
+        ((S,), {'dtype': dtype, 'device': device}),
+        # Hard-code some dtypes/devices. We want to test cases where the
+        # (dtype, device) is different from the input's (dtype, device)
+        # disabled for ARC
+        # ((S,), {'dtype': torch.double}),
+        ((S,), {'device': 'cpu'}),
+        # disabled for ARC
+        #((S,), {'dtype': torch.double, 'device': 'cpu'}),
+    ]
+    if torch.cuda.is_available():
+        inputs.append(((S,), {'device': 'cuda'}))
+
+    for shape, kwargs in inputs:
+        t = make_tensor(shape, dtype=dtype, device=device,
+                        low=None, high=None,
+                        requires_grad=requires_grad)
+        yield SampleInput(t, **kwargs)
 
 class XPUPatchForImport:
     def __init__(self, patch_test_case=True) -> None:
@@ -603,6 +695,10 @@ class XPUPatchForImport:
         self.TEST_CUDNN = common_cuda.TEST_CUDNN
         self.cuda_is_available = cuda.is_available
         self.cuda_is_bf16_supported = cuda.is_bf16_supported
+
+        if "has_fp64=0" in str(torch.xpu.get_device_properties(0)):
+            self.index_variable = common_methods_invocations.index_variable
+            self.reference_inputs_cat = common_methods_invocations.reference_inputs_cat
 
     def align_db_decorators(self, db):
         def gen_xpu_wrappers(op_name, wrappers):
@@ -661,10 +757,36 @@ class XPUPatchForImport:
                 backward_dtypes.add(bfloat16)
                 opinfo.backward_dtypes = tuple(backward_dtypes)
 
+            if "has_fp64=0" in str(torch.xpu.get_device_properties(0)):
+                fp64_dtypes = [ torch.float64, torch.complex128, torch.double, ]
+                opinfo.dtypesIfXPU = set(filter(lambda x: (x not in fp64_dtypes), list(opinfo.dtypesIfXPU)))
+                opinfo.backward_dtypes = tuple(filter(lambda x: (x not in fp64_dtypes), list(opinfo.backward_dtypes)))
+
+    def filter_fp64_sample_input(self, db):
+        # Only for platform without fp64 support
+        if "has_fp64=0" in str(torch.xpu.get_device_properties(0)):
+            for opinfo in db:
+                if opinfo.name in _xpu_computation_op_list:
+                    if opinfo.variant_test_name == "with_dtype" and \
+                        opinfo.name in ["log_softmax", "softmax", "nn.functional.softmin", ] and \
+                        get_wrapped_fn(opinfo.sample_inputs_func) != opinfo.sample_inputs_func and \
+                        get_wrapped_fn(opinfo.sample_inputs_func).func.__name__ == common_methods_invocations.sample_inputs_softmax_variant.__name__:
+                            opinfo.sample_inputs_func = torch.no_grad()(partial(sample_inputs_softmax_variant_nofp64, with_dtype=True))
+                    elif opinfo.sample_inputs_func.__name__ == common_methods_invocations.sample_inputs_softmax_variant.__name__:
+                        opinfo.sample_inputs_func = sample_inputs_softmax_variant_nofp64
+                    elif opinfo.sample_inputs_func.__name__ == common_methods_invocations.sample_inputs_like_fns.__name__:
+                        opinfo.sample_inputs_func = sample_inputs_like_fns_nofp64
+
+
+
     def __enter__(self):
         # Monkey patch until we have a fancy way
 
         common_device_type.onlyCUDA = common_device_type.onlyXPU
+
+        if "has_fp64=0" in str(torch.xpu.get_device_properties(0)):
+            common_methods_invocations.index_variable = index_variable_nofp64
+            common_methods_invocations.reference_inputs_cat = reference_inputs_cat_nofp64
 
         class dtypesIfXPU(common_device_type.dtypes):
             def __init__(self, *args):
@@ -692,6 +814,7 @@ class XPUPatchForImport:
         ]:
             self.align_supported_dtypes(db)
             self.align_db_decorators(db)
+            self.filter_fp64_sample_input(db)
         self.align_db_decorators(module_db)
         common_methods_invocations.python_ref_db = [
             op
@@ -765,6 +888,7 @@ class XPUPatchForImport:
         cuda.is_bf16_supported = lambda: True
 
         sys.path.extend(self.test_package)
+
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
@@ -786,6 +910,10 @@ class XPUPatchForImport:
         common_cuda.TEST_CUDNN = self.TEST_CUDNN
         cuda.is_available = self.cuda_is_available
         cuda.is_bf16_supported = self.cuda_is_bf16_supported
+
+        if "has_fp64=0" in str(torch.xpu.get_device_properties(0)):
+            common_methods_invocations.index_variable = self.index_variable
+            common_methods_invocations.reference_inputs_cat = self.reference_inputs_cat
 
 
 # Copy the test cases from generic_base_class to generic_test_class.
