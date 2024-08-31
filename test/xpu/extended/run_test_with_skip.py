9cb31bc1c73bba1e5a02bc7a831b20e47e471d37
poliset1 <sandeep.polisetty@intel.com>
2024-08-30 18:59:40 -0700

    minor changes to support testing on windows (#790)
    The following changes are made to support testing on windows. 
    1.  Changing single quote to double quite for system call 
    2.  separating setting environment variables in windows and linux
    
    ---------
    
    Co-authored-by: min-jean-cho <min.jean.cho@intel.com>
    Co-authored-by: Cheng, Penghui <penghui.cheng@intel.com>

--- test/xpu/extended/run_test_with_skip.py
+++ test/xpu/extended/run_test_with_skip.py
@@ -4,15 +4,15 @@ from skip_list_common import skip_dict
 
 skip_list = skip_dict["test_ops_xpu.py"]
 
-skip_options = " -k 'not " + skip_list[0]
+skip_options = " -k \"not " + skip_list[0]
 for skip_case in skip_list[1:]:
     skip_option = " and not " + skip_case
     skip_options += skip_option
-skip_options += "'"
+skip_options += "\""
 
-test_command = "PYTORCH_TEST_WITH_SLOW=1 pytest -v test_ops_xpu.py"
+os.environ["PYTORCH_TEST_WITH_SLOW"]="1"
+
+test_command = "pytest -v test_ops_xpu.py"
 test_command += skip_options
-
 res = os.system(test_command)
-exit_code = os.WEXITSTATUS(res)
-sys.exit(exit_code)
+sys.exit(res)
--- test/xpu/run_test_with_only.py
+++ test/xpu/run_test_with_only.py
@@ -6,37 +6,27 @@ import sys
 
 
 def launch_test(test_case, skip_list=None, exe_list=None):
+    os.environ["PYTORCH_ENABLE_XPU_FALLBACK"]="1"
+    os.environ["PYTORCH_TEST_WITH_SLOW"]="1"
     if skip_list != None:
-        skip_options = " -k 'not " + skip_list[0]
+        skip_options = " -k \"not " + skip_list[0]
         for skip_case in skip_list[1:]:
             skip_option = " and not " + skip_case
             skip_options += skip_option
-        skip_options += "'"
-        test_command = (
-            "PYTORCH_ENABLE_XPU_FALLBACK=1 PYTORCH_TEST_WITH_SLOW=1 pytest -v "
-            + test_case
-        )
-        test_command += skip_options
+        skip_options += "\""
+        test_command   = "pytest -v "  + test_case  + skip_options
         return os.system(test_command)
     elif exe_list != None:
-        exe_options = " -k '" + exe_list[0]
+        exe_options = " -k \"" + exe_list[0]
         for exe_case in exe_list[1:]:
             exe_option = " or " + exe_case
             exe_options += exe_option
-        exe_options += "'"
-        test_command = (
-            "PYTORCH_ENABLE_XPU_FALLBACK=1 PYTORCH_TEST_WITH_SLOW=1 pytest -v "
-            + test_case
-        )
-        test_command += exe_options
+        exe_options += "\""
+        test_command   = "pytest -v "  + test_case  + exe_options
         return os.system(test_command)
     else:
-        test_command = (
-            "PYTORCH_ENABLE_XPU_FALLBACK=1 PYTORCH_TEST_WITH_SLOW=1 pytest -v "
-            + test_case
-        )
-        return os.system(test_command)
-
+        test_command   = "pytest -v " + test_case  
+        return os.system(test_command)     
 
 res = 0
 
@@ -51,5 +41,8 @@ execute_list = (
 )
 res += launch_test("test_decomp_xpu.py", exe_list=execute_list)
 
-exit_code = os.WEXITSTATUS(res)
-sys.exit(exit_code)
+if os.name == "nt":
+    sys.exit(res)
+else:    
+    exit_code = os.WEXITSTATUS(res)
+    sys.exit(exit_code)
