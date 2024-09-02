9a78276402b13c48ebb07e4a81a60d7a60be931b
min-jean-cho <min.jean.cho@intel.com>
2024-09-02 04:12:06 -0700

    [Windows] Enable Extended UTs

--- test/xpu/skip_list_win_arc.py
+++ test/xpu/skip_list_win_arc.py
@@ -1,10 +1,4 @@
 skip_dict = {
-    # SYCL Compiler on Windows removed the following operations when '-cl-poison-unsupported-fp64-kernels' is on
-    # Hence, skip the following windows specific errors
-    "test_ops_xpu.py": (
-        "test_compare_cpu_sqrt_xpu_complex64",
-        "test_backward_nn_functional_adaptive_avg_pool2d_xpu_float32",
-    ),
     "test_binary_ufuncs_xpu": (
         "test_batch_vs_slicing___rpow___xpu_complex64",
         "test_batch_vs_slicing__refs_pow_xpu_complex64",
