c847e5137bc5615d00aead52bd032e95f09144cc
cfgfung <cfgfung@gmail.com>
2024-09-03 09:39:43 +0800

    Cherry pick for release/2.5. Details of the commit can be checked in https://github.com/intel/torch-xpu-ops/pull/828

--- test/xpu/run_test_with_skip_arc.py
+++ test/xpu/run_test_with_skip_arc.py
@@ -4,7 +4,7 @@ from skip_list_common import skip_dict
 from skip_list_arc import skip_dict as skip_dict_specifical
 from skip_list_win import skip_dict as skip_dict_win
 from xpu_test_utils import launch_test
-
+from skip_list_win_arc import skip_dict as skip_dict_win_arc
 
 res = 0
 IS_WINDOWS = sys.platform == "win32"
@@ -15,6 +15,8 @@ for key in skip_dict:
         skip_list += skip_dict_specifical[key]
     if IS_WINDOWS and key in skip_dict_win:
         skip_list += skip_dict_win[key]
+    if IS_WINDOWS and key in skip_dict_win_arc:
+        skip_list += skip_dict_win_arc[key]
     res += launch_test(key, skip_list)
 
 if os.name == "nt":
