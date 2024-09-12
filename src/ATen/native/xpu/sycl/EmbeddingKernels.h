d3192f67bd0f9d26016816d9a16b86231fc904fe
chunhuanMeng <105194461+chunhuanMeng@users.noreply.github.com>
2024-09-12 14:15:44 +0800

    add TORCH_XPU_API

--- src/ATen/native/xpu/sycl/EmbeddingKernels.h
+++ src/ATen/native/xpu/sycl/EmbeddingKernels.h
@@ -4,14 +4,14 @@
 
 namespace at::native::xpu {
 
-Tensor embedding_dense_backward_kernel(
+TORCH_XPU_API Tensor embedding_dense_backward_kernel(
     const Tensor& grad_,
     const Tensor& indices_,
     int64_t num_weights,
     int64_t padding_idx,
     bool scale_grad_by_freq);
 
-Tensor& embedding_renorm_kernel(
+TORCH_XPU_API Tensor& embedding_renorm_kernel(
     Tensor& self,
     const Tensor& indices,
     double max_norm,
