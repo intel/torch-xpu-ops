51ca10e94028d588325e5265a6c529badd3afac4
chunhuanMeng <105194461+chunhuanMeng@users.noreply.github.com>
2024-09-14 17:18:40 +0800

    Update MultiLabelMarginLossKernels.h

--- src/ATen/native/xpu/sycl/MultiLabelMarginLossKernels.h
+++ src/ATen/native/xpu/sycl/MultiLabelMarginLossKernels.h
@@ -3,14 +3,14 @@
 
 namespace at::native::xpu {
 
-TORCH_XPU_API void multilabel_margin_loss_kernel(
+void multilabel_margin_loss_kernel(
     const Tensor& input,
     const Tensor& target,
     int64_t reduction,
     Tensor& output,
     Tensor& is_target);
 
-TORCH_XPU_API void multilabel_margin_loss_backward_kernel(
+void multilabel_margin_loss_backward_kernel(
     const Tensor& grad_output,
     const Tensor& input,
     const Tensor& target,
