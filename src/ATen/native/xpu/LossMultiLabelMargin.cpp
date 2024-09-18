49b8892c5f98ac75db074655d81c380be1e80853
chunhuanMeng <105194461+chunhuanMeng@users.noreply.github.com>
2024-09-18 15:54:00 +0800

    Update LossMultiLabelMargin.cpp

--- src/ATen/native/xpu/LossMultiLabelMargin.cpp
+++ src/ATen/native/xpu/LossMultiLabelMargin.cpp
@@ -10,7 +10,6 @@ std::tuple<Tensor&, Tensor&> XPUNativeFunctions::
         int64_t reduction,
         Tensor& output,
         Tensor& is_target) {
-  printf("multilabel_margin_loss_forward\n");
   at::native::xpu::multilabel_margin_loss_kernel(
       self, target, reduction, output, is_target);
   return std::tuple<Tensor&, Tensor&>(output, is_target);
@@ -20,13 +19,10 @@ std::tuple<Tensor, Tensor> XPUNativeFunctions::multilabel_margin_loss_forward(
     const Tensor& self,
     const Tensor& target,
     int64_t reduction) {
-  printf("multilabel_margin_loss_forward3\n");
   auto output = at::empty({0}, self.options());
   auto is_target = at::empty({0}, self.options());
-  printf("multilabel_margin_loss_forward4\n");
   at::native::xpu::multilabel_margin_loss_kernel(
       self, target, reduction, output, is_target);
-  printf("multilabel_margin_loss_forward5\n");
   return std::tuple<Tensor&, Tensor&>(output, is_target);
 }
 
@@ -37,10 +33,8 @@ Tensor& XPUNativeFunctions::multilabel_margin_loss_backward_out(
     int64_t reduction,
     const Tensor& is_target,
     Tensor& grad_input) {
-  printf("multilabel_margin_loss_backward_out\n");
   at::native::xpu::multilabel_margin_loss_backward_kernel(
       grad_output, self, target, reduction, is_target, grad_input);
-  printf("multilabel_margin_loss_backward_out done\n");
   return grad_input;
 }
 
@@ -50,11 +44,9 @@ Tensor XPUNativeFunctions::multilabel_margin_loss_backward(
     const Tensor& target,
     int64_t reduction,
     const Tensor& is_target) {
-  printf("multilabel_margin_loss_backward_out2\n");
   auto grad_input = zeros_like(self, LEGACY_CONTIGUOUS_MEMORY_FORMAT);
   at::native::xpu::multilabel_margin_loss_backward_kernel(
       grad_output, self, target, reduction, is_target, grad_input);
-  printf("multilabel_margin_loss_backward_out2 done\n");
   return grad_input;
 }
 
