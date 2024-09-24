5e40acb08da31738f99598dca669fa3a14aed154
Yutao Xu <yutao.xu@intel.com>
2024-09-24 11:22:35 +0800

    Update LossCTCKernels.h

--- src/ATen/native/xpu/sycl/LossCTCKernels.h
+++ src/ATen/native/xpu/sycl/LossCTCKernels.h
@@ -11,4 +11,15 @@ TORCH_XPU_API std::tuple<Tensor, Tensor> ctc_loss_kernel(
     int64_t BLANK,
     bool zero_infinity);
 
+TORCH_XPU_API Tensor ctc_loss_backward_kernel(
+    const Tensor& grad,
+    const Tensor& log_probs,
+    const Tensor& targets,
+    IntArrayRef input_lengths,
+    IntArrayRef target_lengths,
+    const Tensor& neg_log_likelihood,
+    const Tensor& log_alpha,
+    int64_t BLANK,
+    bool zero_infinity);
+
 } // namespace at::native::xpu
