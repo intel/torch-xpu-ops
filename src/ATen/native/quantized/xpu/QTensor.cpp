cd8e90a2123d03531a3fed5d42262c219b4f30fd
hjhee <jianhang.he@intel.com>
2024-09-25 05:16:44 +0000

    add quantize_per_channel, quantize_per_tensor, quantize_per_tensor_dynamic, quantize_per_tensor.tensor_qparams

---
+++ src/ATen/native/quantized/xpu/QTensor.cpp
@@ -0,0 +1,98 @@
+#include <ATen/core/ScalarType.h>
+#include <ATen/core/Tensor.h>
+#include <ATen/native/quantized/cpu/QuantUtils.h>
+#include <ATen/xpu/XPUNativeFunctions.h>
+
+#include <ATen/native/quantized/xpu/MakePerTensorQuantizedTensor.h>
+
+namespace at {
+
+Tensor XPUNativeFunctions::quantize_per_channel(
+    const Tensor& self,
+    const Tensor& scales,
+    const Tensor& zero_points,
+    int64_t axis,
+    ScalarType dtype) {
+  if (self.is_quantized()) {
+    return self;
+  }
+  auto quantizer = native::make_per_channel_affine_quantizer_xpu(
+      scales, zero_points, axis, dtype);
+  return quantizer->quantize(self);
+}
+
+Tensor XPUNativeFunctions::quantize_per_tensor(
+    const Tensor& self,
+    double scale,
+    int64_t zero_point,
+    ScalarType dtype) {
+  if (self.is_quantized()) {
+    return self;
+  }
+  auto quantizer =
+      native::make_per_tensor_affine_quantizer_xpu(scale, zero_point, dtype);
+  return quantizer->quantize(self);
+}
+
+Tensor XPUNativeFunctions::quantize_per_tensor(
+    const Tensor& self,
+    const Tensor& scale,
+    const Tensor& zero_point,
+    ScalarType dtype) {
+  if (self.is_quantized()) {
+    return self;
+  }
+  auto quantizer = native::make_per_tensor_affine_quantizer_xpu(
+      scale.item().toDouble(), zero_point.item().toLong(), dtype);
+  return quantizer->quantize(self);
+}
+
+Tensor XPUNativeFunctions::quantize_per_tensor_dynamic(
+    const Tensor& self,
+    ScalarType dtype,
+    bool reduce_range) {
+  TORCH_CHECK(
+      (dtype == ScalarType::QInt8 || dtype == ScalarType::QUInt8 ||
+       dtype == ScalarType::Half),
+      "dtype ",
+      dtype,
+      "not supported");
+  auto input_contig = self.contiguous();
+  if (dtype == ScalarType::Half) {
+    return input_contig.to(ScalarType::Half);
+  }
+  float x_min = input_contig.min().item<float>();
+  float x_max = input_contig.max().item<float>();
+
+  if (reduce_range && at::globalContext().qEngine() == at::QEngine::QNNPACK) {
+    reduce_range = false;
+  }
+
+  int qmin = 0;
+  int qmax = 0;
+
+  if (dtype == ScalarType::QInt8) {
+    qmin = -128;
+    qmax = 127;
+  } else {
+    // for now, this branch executes for dtype == ScalarType::QUInt8
+    // additional cases will be added when quantization support for other dtypes
+    // becomes available
+    qmin = 0;
+    qmax = 255;
+  }
+
+  auto q_params = quant_utils::ChooseQuantizationParams(
+      /*min=*/x_min,
+      /*max=*/x_max,
+      /*qmin=*/qmin,
+      /*qmax=*/qmax,
+      /*preserve_sparsity=*/false,
+      /*force_scale_power_of_two=*/false,
+      /*reduce_range=*/reduce_range);
+
+  return XPUNativeFunctions::quantize_per_tensor(
+      self, q_params.scale, q_params.zero_point, dtype);
+}
+
+} // namespace at
