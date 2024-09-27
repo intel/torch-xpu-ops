1cf0c6e9f8636d1cab7d26002a436393dc33af6e
xytintel <yutao.xu@intel.com>
2024-09-27 07:13:08 +0000

    using new codegen

--- src/ATen/native/xpu/LossCTC.cpp
+++ src/ATen/native/xpu/LossCTC.cpp
@@ -1,13 +1,12 @@
-#include <ATen/ATen.h>
 #include <ATen/core/Reduction.h>
 #include <ATen/core/Tensor.h>
 #include <ATen/native/xpu/sycl/LossCTCKernels.h>
-#include <ATen/xpu/XPUNativeFunctions.h>
 #include <comm/RegisterUtils.h>
 
 namespace at {
+namespace native {
 
-std::tuple<Tensor, Tensor> XPUNativeFunctions::_ctc_loss(
+std::tuple<Tensor, Tensor> ctc_loss_xpu(
     const Tensor& log_probs,
     const Tensor& targets,
     IntArrayRef input_lengths,
@@ -18,18 +17,7 @@ std::tuple<Tensor, Tensor> XPUNativeFunctions::_ctc_loss(
       log_probs, targets, input_lengths, target_lengths, blank, zero_infinity);
 }
 
-std::tuple<Tensor, Tensor> XPUNativeFunctions::_ctc_loss(
-    const Tensor& log_probs,
-    const Tensor& targets,
-    const Tensor& input_lengths,
-    const Tensor& target_lengths,
-    int64_t blank,
-    bool zero_infinity) {
-  return at::native::ctc_loss_tensor(
-      log_probs, targets, input_lengths, target_lengths, blank, zero_infinity);
-}
-
-Tensor XPUNativeFunctions::_ctc_loss_backward(
+Tensor ctc_loss_backward_xpu(
     const Tensor& grad,
     const Tensor& log_probs,
     const Tensor& targets,
@@ -51,26 +39,5 @@ Tensor XPUNativeFunctions::_ctc_loss_backward(
       zero_infinity);
 }
 
-Tensor XPUNativeFunctions::_ctc_loss_backward(
-    const Tensor& grad,
-    const Tensor& log_probs,
-    const Tensor& targets,
-    const Tensor& input_lengths,
-    const Tensor& target_lengths,
-    const Tensor& neg_log_likelihood,
-    const Tensor& log_alpha,
-    int64_t blank,
-    bool zero_infinity) {
-  return at::native::ctc_loss_backward_tensor(
-      grad,
-      log_probs,
-      targets,
-      input_lengths,
-      target_lengths,
-      neg_log_likelihood,
-      log_alpha,
-      blank,
-      zero_infinity);
-}
-
+} // namespace native
 } // namespace at
