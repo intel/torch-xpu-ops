intel/torch-xpu-ops

commit 1f7270da00ac92d045ad246ea50d4532e94cc926

Add aten::mode (#770)
- mode
- mode.values

---------

Co-authored-by: Yutao Xu <yutao.xu@intel.com>

src/ATen/native/xpu/LayerNorm.cpp
@@ -69,7 +69,8 @@ namespace native {
   for (const auto idx : c10::irange(axis)) {
     stat_shape.push_back(input_shape[idx]);
   }
-  for (const auto C10_UNUSED idx : c10::irange(axis, input.dim())) {
+  for (const auto idx : c10::irange(axis, input.dim())) {
+    (void)idx;
     stat_shape.push_back(1);
   }
 

src/ATen/native/xpu/UpSample.h
@@ -12,7 +12,7 @@
 
 namespace at::native::xpu {
 
-inline C10_UNUSED std::array<int64_t, 4> upsample_2d_common_check(
+inline std::array<int64_t, 4> upsample_2d_common_check(
     IntArrayRef input_size,
     IntArrayRef output_size) {
   TORCH_CHECK(
@@ -228,7 +228,7 @@ static scalar_t upsample_get_value_bounded(
   return data[batch][channel][access_y][access_x];
 }
 
-static C10_UNUSED std::array<int64_t, 3> upsample_1d_common_check(
+inline std::array<int64_t, 3> upsample_1d_common_check(
     IntArrayRef input_size,
     IntArrayRef output_size) {
   TORCH_CHECK(

src/ATen/native/xpu/TensorCompare.cpp
@@ -8,6 +8,7 @@
 #include <ATen/native/xpu/sycl/ReduceMaxValuesKernels.h>
 #include <ATen/native/xpu/sycl/ReduceMinValuesKernels.h>
 #include <ATen/native/xpu/sycl/TensorCompareKernels.h>
+#include <ATen/native/xpu/sycl/TensorModeKernel.h>
 #include <comm/ReduceOpsUtils.h>
 
 #include <ATen/ops/result_type_native.h>
@@ -59,6 +60,7 @@ void minmax_out_impl(
 } // namespace xpu
 
 REGISTER_XPU_DISPATCH(where_kernel, &xpu::where_kernel);
+REGISTER_XPU_DISPATCH(mode_stub, &xpu::mode_kernel);
 REGISTER_XPU_DISPATCH(clamp_min_scalar_stub, &xpu::clamp_min_scalar_kernel);
 REGISTER_XPU_DISPATCH(clamp_max_scalar_stub, &xpu::clamp_max_scalar_kernel);
 REGISTER_XPU_DISPATCH(clamp_scalar_stub, &xpu::clamp_scalar_kernel);

src/ATen/native/xpu/XPUFallback.template
@@ -214,7 +214,6 @@ TORCH_LIBRARY_IMPL(aten, XPU, m) {
     "max_pool3d_with_indices_backward",
     "max_unpool2d",
     "max_unpool3d",
-    "mode",
     "multilabel_margin_loss_backward",
     "multilabel_margin_loss_forward",
     "multi_margin_loss",

src/ATen/native/xpu/sycl/TensorModeKernel.h
@@ -1,0 +1,14 @@
+#pragma once
+
+#include <ATen/ATen.h>
+
+namespace at::native::xpu {
+
+TORCH_XPU_API void mode_kernel(
+    Tensor& values,
+    Tensor& indices,
+    const Tensor& self,
+    int64_t dim,
+    bool keepdim);
+
+} // namespace at::native::xpu

test/xpu/extended/skip_list_common.py
@@ -171,6 +171,11 @@ skip_dict = {
     "test_compare_cpu_isin_xpu_bfloat16",
     "test_compare_cpu_unique_consecutive_xpu_bfloat16",
 
+    # returned index is dependent on input data and implementation detail, and no
+    # specification is given to uniquely identify the correct index 
+    # (e.g. index with maximal / minimal value)
+    "test_compare_cpu_mode",
+
     # nextafter: Numeric error due to `std::nextafter` difference between CPU (GCC) and XPU (SYCL)
     # https://github.com/intel/torch-xpu-ops/issues/623
     "test_compare_cpu_nextafter_xpu_float16",

test/xpu/skip_list_common.py
@@ -67,7 +67,6 @@ skip_dict = {
         # RuntimeError: mode only supports CPU AND CUDA device type, got: xpu
         # Issue https://github.com/intel/torch-xpu-ops/issues/327
         "test_numpy_ref_linalg_tensorinv_xpu_float64",
-        "test_out_mode_xpu_float32",
 
         # RuntimeError: false INTERNAL ASSERT FAILED at "/home/gta/daisyden/pytorch4/aten/src/ATen/native/DispatchStub.cpp":220, please report a bug to PyTorch. DispatchStub: missing kernel for xpu
         "test_out_nanmean_xpu_float32",

commit e51311c83cd2b9d72d9f936eff1c4909ee977807

Use [[maybe_unused]] to suppress unsued warning (#989)
# Motivation
Accroding to https://github.com/pytorch/pytorch/pull/138364 to use
`[[maybe_unused]]` to suppress the unused warning.

Maybe it could fix the
[issue](https://github.com/intel/torch-xpu-ops/issues/987) introduced by
https://github.com/intel/torch-xpu-ops/pull/770/files#diff-ae810fb2aff5cf37ab1b21244fd1e172a660a8e4f4b9292f0661af64c2f92b21L15.

src/ATen/native/xpu/LayerNorm.cpp
@@ -69,8 +69,7 @@ namespace native {
   for (const auto idx : c10::irange(axis)) {
     stat_shape.push_back(input_shape[idx]);
   }
-  for (const auto idx : c10::irange(axis, input.dim())) {
-    (void)idx;
+  for ([[maybe_unused]] const auto _ : c10::irange(axis, input.dim())) {
     stat_shape.push_back(1);
   }
 

src/ATen/native/xpu/UpSample.h
@@ -12,7 +12,7 @@
 
 namespace at::native::xpu {
 
-inline std::array<int64_t, 4> upsample_2d_common_check(
+[[maybe_unused]] inline std::array<int64_t, 4> upsample_2d_common_check(
     IntArrayRef input_size,
     IntArrayRef output_size) {
   TORCH_CHECK(
@@ -228,7 +228,7 @@ static scalar_t upsample_get_value_bounded(
   return data[batch][channel][access_y][access_x];
 }
 
-inline std::array<int64_t, 3> upsample_1d_common_check(
+[[maybe_unused]] inline std::array<int64_t, 3> upsample_1d_common_check(
     IntArrayRef input_size,
     IntArrayRef output_size) {
   TORCH_CHECK(