e2a8c224ce17070d5939d06bcde0127afc1cdd74
Kanya-Mo <167922169+Kanya-Mo@users.noreply.github.com>
2024-10-24 09:44:32 -0700

    Add 4 modified Bessel and 1 spherical Bessel operators (#1010)
    - [x] special_modified_bessel_i0.out
    - [x] special_modified_bessel_i0
    - [x] special_modified_bessel_i1.out
    - [x] special_modified_bessel_i1
    - [x] special_modified_bessel_k0.out
    - [x] special_modified_bessel_k0
    - [x] special_modified_bessel_k1.out
    - [x] special_modified_bessel_k1
    - [x] special_spherical_bessel_j0.out
    - [x] special_spherical_bessel_j0

--- src/ATen/native/xpu/Bessel.cpp
+++ src/ATen/native/xpu/Bessel.cpp
@@ -6,6 +6,11 @@
 #include <ATen/native/xpu/sycl/BesselJ1Kernel.h>
 #include <ATen/native/xpu/sycl/BesselY0Kernel.h>
 #include <ATen/native/xpu/sycl/BesselY1Kernel.h>
+#include <ATen/native/xpu/sycl/ModifiedBesselI0Kernel.h>
+#include <ATen/native/xpu/sycl/ModifiedBesselI1Kernel.h>
+#include <ATen/native/xpu/sycl/ModifiedBesselK0Kernel.h>
+#include <ATen/native/xpu/sycl/ModifiedBesselK1Kernel.h>
+#include <ATen/native/xpu/sycl/SphericalBesselJ0Kernel.h>
 
 namespace at {
 namespace native {
@@ -13,5 +18,10 @@ REGISTER_XPU_DISPATCH(special_bessel_j0_stub, &xpu::bessel_j0_kernel);
 REGISTER_XPU_DISPATCH(special_bessel_j1_stub, &xpu::bessel_j1_kernel);
 REGISTER_XPU_DISPATCH(special_bessel_y0_stub, &xpu::bessel_y0_kernel);
 REGISTER_XPU_DISPATCH(special_bessel_y1_stub, &xpu::bessel_y1_kernel);
+REGISTER_XPU_DISPATCH(special_modified_bessel_i0_stub, &xpu::modified_bessel_i0_kernel);
+REGISTER_XPU_DISPATCH(special_modified_bessel_i1_stub, &xpu::modified_bessel_i1_kernel);
+REGISTER_XPU_DISPATCH(special_modified_bessel_k0_stub, &xpu::modified_bessel_k0_kernel);
+REGISTER_XPU_DISPATCH(special_modified_bessel_k1_stub, &xpu::modified_bessel_k1_kernel);
+REGISTER_XPU_DISPATCH(special_spherical_bessel_j0_stub, &xpu::spherical_bessel_j0_kernel);
 } // namespace native
 } // namespace at
---
+++ src/ATen/native/xpu/sycl/ModifiedBesselK1Kernel.cpp
@@ -0,0 +1,24 @@
+#include <ATen/Dispatch.h>
+#include <ATen/native/Math.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/native/xpu/sycl/Loops.h>
+#include <c10/core/Scalar.h>
+
+#include <ATen/native/xpu/sycl/ModifiedBesselK1Kernel.h>
+
+namespace at::native::xpu {
+
+template <typename scalar_t>
+struct ModifiedBesselK1Functor {
+  scalar_t operator()(scalar_t a) const {
+    return modified_bessel_k1_forward(a);
+  }
+};
+
+void modified_bessel_k1_kernel(TensorIteratorBase& iter) {
+  AT_DISPATCH_FLOATING_TYPES(iter.common_dtype(), "modified_bessel_k1_xpu", [&]() {
+    gpu_kernel(iter, ModifiedBesselK1Functor<scalar_t>());
+  });
+}
+
+}
