2b671959aaec56ad5aa2a79bc6947ebd675aad8a
sdp <min.jean.cho@intel.com>
2024-11-02 04:01:44 -0700

    Add aten::special_airy_ai.out

--- src/ATen/native/xpu/AiryAi.cpp
+++ src/ATen/native/xpu/AiryAi.cpp
@@ -1,9 +1,12 @@
+#include <ATen/native/UnaryOps.h>
 #include <ATen/native/DispatchStub.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/core/Tensor.h>
 #include <ATen/native/xpu/sycl/AiryAiKernel.h>
 
 namespace at {
 namespace native {
-REGISTER_XPU_DISPATCH(airy_ai_stub, &xpu::airy_ai_kernel);
+REGISTER_XPU_DISPATCH(special_airy_ai_stub, &xpu::airy_ai_kernel);
 
 } // namespace native
 } // namespace at
