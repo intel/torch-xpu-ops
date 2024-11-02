f0e5947cad603ed6e5c68da6b5e7b4aafbe293bc
sdp <min.jean.cho@intel.com>
2024-11-02 02:01:49 -0700

    Add aten::special_airy_ai.out

---
+++ src/ATen/native/xpu/AiryAi.cpp
@@ -0,0 +1,8 @@
+#include <ATen/native/xpu/sycl/AiryAiKernel.h>
+
+namespace at {
+namespace native {
+REGISTER_XPU_DISPATCH(airy_ai_stub, &xpu::airy_ai_kernel);
+
+} // namespace native
+} // namespace at
