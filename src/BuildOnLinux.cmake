ca5fbf2b0a290c1e633a16659286150149a873aa
Yutao Xu <yutao.xu@intel.com>
2024-11-09 13:54:39 +0800

    Update BuildOnLinux.cmake

--- src/BuildOnLinux.cmake
+++ src/BuildOnLinux.cmake
@@ -28,13 +28,17 @@ else()
   # Split SYCL kernels into 4 libraries as categories 1) Unary+Binary 2) Reduce 3) Foreach 4) Others.
   set(ATen_XPU_SYCL_UNARY_BINARY_SRCS)
   set(ATen_XPU_SYCL_REDUCE_SRCS)
+  set(ATen_XPU_SYCL_ACTIVATION_SRCS)
   set(ATen_XPU_SYCL_FOREACH_SRCS)
   set(ATen_XPU_SYCL_OTHERS_SRCS)
 
   foreach(sycl_src ${ATen_XPU_SYCL_SRCS})
     string(REGEX MATCH "Binary" IS_BINARY ${sycl_src})
     string(REGEX MATCH "Unary" IS_UNARY ${sycl_src})
+    string(REGEX MATCH "Pow" IS_POW ${sycl_src})
+    string(REGEX MATCH "Copy" IS_COPY ${sycl_src})
     string(REGEX MATCH "Reduce" IS_REDUCE ${sycl_src})
+    string(REGEX MATCH "Activation" IS_ACTIVATION ${sycl_src})
     string(REGEX MATCH "Foreach" IS_FOREACH ${sycl_src})
 
     if(NOT IS_FOREACH STREQUAL "")
@@ -43,6 +47,10 @@ else()
       list(APPEND ATen_XPU_SYCL_REDUCE_SRCS ${sycl_src})
     elseif(NOT IS_UNARY STREQUAL "" OR NOT IS_BINARY STREQUAL "")
       list(APPEND ATen_XPU_SYCL_UNARY_BINARY_SRCS ${sycl_src})
+    elseif(NOT IS_COPY STREQUAL "" OR NOT IS_POW STREQUAL "")
+      list(APPEND ATen_XPU_SYCL_UNARY_BINARY_SRCS ${sycl_src})
+    elseif(NOT IS_ACTIVATION STREQUAL "")
+      list(APPEND ATen_XPU_SYCL_ACTIVATION_SRCS ${sycl_src})
     else()
       list(APPEND ATen_XPU_SYCL_OTHERS_SRCS ${sycl_src})
     endif()
@@ -71,6 +79,18 @@ else()
 
   # Decouple with PyTorch cmake definition.
   install(TARGETS ${sycl_reduce_lib} DESTINATION "${TORCH_INSTALL_LIB_DIR}")
+
+  # Activation kernel lib
+  set(sycl_activation_lib torch_xpu_ops_sycl_activation_kernels)
+  sycl_add_library(
+    ${sycl_activation_lib}
+    SHARED
+    SYCL_SOURCES ${ATen_XPU_SYCL_ACTIVATION_SRCS})
+  target_link_libraries(torch_xpu_ops PUBLIC ${sycl_activation_lib})
+  list(APPEND TORCH_XPU_OPS_LIBRARIES ${sycl_activation_lib})
+
+  # Decouple with PyTorch cmake definition.
+  install(TARGETS ${sycl_activation_lib} DESTINATION "${TORCH_INSTALL_LIB_DIR}")
 
   # Foreach kernel lib
   set(sycl_foreach_lib torch_xpu_ops_sycl_foreach_kernels)
