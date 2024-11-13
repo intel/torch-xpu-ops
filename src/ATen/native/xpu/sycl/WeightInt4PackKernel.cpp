fb947c4d069ab0fff3af1d5bec77815e0ede397b
Yutao Xu <yutao.xu@intel.com>
2024-11-13 15:41:35 +0800

    Update WeightInt4PackKernel.cpp

--- src/ATen/native/xpu/sycl/WeightInt4PackKernel.cpp
+++ src/ATen/native/xpu/sycl/WeightInt4PackKernel.cpp
@@ -9,39 +9,31 @@ namespace at::native::xpu {
 struct WeightToInt4PackKernelFunctor {
   void operator()(sycl::item<1> item) const {
     auto idx = item.get_linear_id();
-    int i = idx / K_div_2_;
-    int k = idx % K_div_2_;
-    int nb_size = std::min(BLOCK_N_, N_ - i * BLOCK_N_);
-    uint8_t* src = weight_ + i * BLOCK_N_ * K_div_2_;
-    uint8_t* dst = weight_packed_ + i * K_ * BLOCK_N_ / 2;
-    for (int n = 0; n < nb_size; n += 2) {
-      uint8_t val0 = src[n * K_div_2_ + k];
-      uint8_t val1 = src[n * K_div_2_ + K_div_2_ + k];
-      uint8_t packed_0 = ((val1 & 0xF0)) | ((val0 & 0xF0) >> 4);
-      uint8_t packed_1 = ((val1 & 0xF) << 4) | (val0 & 0xF);
-      dst[k * 2 * nb_size / 2 + n / 2] = packed_0;
-      dst[(k * 2 + 1) * nb_size / 2 + n / 2] = packed_1;
-    }
+    int out_y = idx / N_;
+    int out_x = idx % N_;
+    int in_y = out_x;
+    int in_x = out_y * 4;
+    int K_div_2 = K_ / 2;
+    uint32_t res =
+        *reinterpret_cast<uint32_t*>(&weight_[in_y * K_div_2 + in_x]);
+    weight_packed_[out_y * N_ + out_x] = res;
   }
   WeightToInt4PackKernelFunctor(
-      uint8_t* weight_packed,
+      uint32_t* weight_packed,
       uint8_t* weight,
-      int K_div_2,
-      int BLOCK_N,
+      int K_div_8,
       int N,
       int K)
       : weight_packed_(weight_packed),
         weight_(weight),
-        K_div_2_(K_div_2),
-        BLOCK_N_(BLOCK_N),
+        K_div_8_(K_div_8),
         N_(N),
         K_(K) {}
 
  private:
-  uint8_t* weight_packed_;
+  uint32_t* weight_packed_;
   uint8_t* weight_;
-  int K_div_2_;
-  int BLOCK_N_;
+  int K_div_8_;
   int N_;
   int K_;
 };
@@ -52,14 +44,12 @@ void weight_to_int4pack_kernel(
     int N,
     int K) {
   auto weight_packed_data =
-      reinterpret_cast<uint8_t*>(weight_packed.data_ptr());
+      reinterpret_cast<uint32_t*>(weight_packed.data_ptr());
   const auto weight_data = weight.data_ptr<uint8_t>();
-  constexpr int BLOCK_N = sizeof(float) * 4; // 16
-  const int NB = (N + BLOCK_N - 1) / BLOCK_N;
-  int K_div_2 = K / 2;
-  size_t global_range = NB * K_div_2;
+  int K_div_8 = K / 8;
+  size_t global_range = N * K_div_8;
   auto fn = WeightToInt4PackKernelFunctor(
-      weight_packed_data, weight_data, K_div_2, BLOCK_N, N, K);
+      weight_packed_data, weight_data, K_div_8, N, K);
   sycl_kernel_submit(sycl::range<1>(global_range), getCurrentSYCLQueue(), fn);
 }
 
