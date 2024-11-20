4120fabe82b136f6f6b1f40c3e29890a4e281fa2
Mo, Kanya <kanya.mo@intel.com>
2024-11-19 23:01:08 -0800

    Add backward.

--- src/ATen/native/xpu/UpSampleBilinear2d.cpp
+++ src/ATen/native/xpu/UpSampleBilinear2d.cpp
@@ -47,8 +47,22 @@ TORCH_IMPL_FUNC(_upsample_bilinear2d_aa_out_xpu) (
   std::optional<double> scales_h,
   std::optional<double> scales_w,
   const Tensor& output) {
-    xpu::upsample_bilinear2d_aa_out_kernel(
+    xpu::_upsample_bilinear2d_aa_out_kernel(
       output, input, output_size, align_corners, scales_h, scales_w);
+}
+
+TORCH_IMPL_FUNC(_upsample_bilinear2d_aa_backward_out_xpu) (
+    const Tensor& grad_output,
+    IntArrayRef output_size,
+    IntArrayRef input_size,
+    bool align_corners,
+    std::optional<double> scales_h,
+    std::optional<double> scales_w,
+    const Tensor& grad_input) {
+  // Nondeterministic because of atomicAdd usage
+  globalContext().alertNotDeterministic("upsample_bilinear2d_aa_backward_out_xpu");
+  xpu::_upsample_bilinear2d_aa_backward_out_kernel(
+      grad_input, grad_output, output_size, input_size, align_corners, scales_h, scales_w);
 }
 
 // We define bicubic anti-alias function implementations in this file instead of
@@ -60,8 +74,22 @@ TORCH_IMPL_FUNC(_upsample_bicubic2d_aa_out_xpu) (
     std::optional<double> scales_h,
     std::optional<double> scales_w,
     const Tensor& output) {
-      xpu::upsample_bicubic2d_aa_out_kernel(
+      xpu::_upsample_bicubic2d_aa_out_kernel(
       output, input, output_size, align_corners, scales_h, scales_w);
+}
+
+TORCH_IMPL_FUNC(_upsample_bicubic2d_aa_backward_out_xpu) (
+    const Tensor& grad_output,
+    IntArrayRef output_size,
+    IntArrayRef input_size,
+    bool align_corners,
+    std::optional<double> scales_h,
+    std::optional<double> scales_w,
+    const Tensor& grad_input) {
+  // Nondeterministic because of atomicAdd usage
+  globalContext().alertNotDeterministic("upsample_bicubic2d_aa_backward_out_xpu");
+  xpu::_upsample_bicubic2d_aa_backward_out_kernel(
+      grad_input, grad_output, output_size, input_size, align_corners, scales_h, scales_w);
 }
 
 } // namespace native
