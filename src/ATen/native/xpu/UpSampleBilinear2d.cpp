43957e2523e9530adb2019fa730b1b43b988bf5a
Kanya-Mo <167922169+Kanya-Mo@users.noreply.github.com>
2024-11-27 22:37:46 -0800

    Add upsample_aa op series. (#1106)
    - [x] _upsample_bicubic2d_aa
    - [x] _upsample_bicubic2d_aa.out
    - [x] _upsample_bicubic2d_aa_backward
    - [x] _upsample_bicubic2d_aa_backward.grad_input
    - [x] _upsample_bilinear2d_aa
    - [x] _upsample_bilinear2d_aa.out
    - [x] _upsample_bilinear2d_aa_backward
    - [x] _upsample_bilinear2d_aa_backward.grad_input
    
    ---------
    
    Co-authored-by: Yutao Xu <yutao.xu@intel.com>

--- src/ATen/native/xpu/UpSampleBilinear2d.cpp
+++ src/ATen/native/xpu/UpSampleBilinear2d.cpp
@@ -6,6 +6,8 @@
 
 #include <xpu/ATen/ops/upsample_bilinear2d_backward_native.h>
 #include <xpu/ATen/ops/upsample_bilinear2d_native.h>
+#include <xpu/ATen/ops/_upsample_bilinear2d_aa_backward_native.h>
+#include <xpu/ATen/ops/_upsample_bilinear2d_aa_native.h>
 
 namespace at {
 namespace native {
@@ -36,6 +38,31 @@ TORCH_IMPL_FUNC(upsample_bilinear2d_backward_out_xpu)
       align_corners,
       scales_h,
       scales_w);
+}
+
+TORCH_IMPL_FUNC(_upsample_bilinear2d_aa_out_xpu)
+(const Tensor& input,
+ IntArrayRef output_size,
+ bool align_corners,
+ std::optional<double> scales_h,
+ std::optional<double> scales_w,
+ const Tensor& output) {
+  xpu::_upsample_bilinear2d_aa_out_kernel(
+    output, input, output_size, align_corners, scales_h, scales_w);
+}
+
+TORCH_IMPL_FUNC(_upsample_bilinear2d_aa_backward_out_xpu)
+(const Tensor& grad_output,
+ IntArrayRef output_size,
+ IntArrayRef input_size,
+ bool align_corners,
+ std::optional<double> scales_h,
+ std::optional<double> scales_w,
+ const Tensor& grad_input) {
+  // Nondeterministic because of atomicAdd usage
+  globalContext().alertNotDeterministic("upsample_bilinear2d_aa_backward_out_xpu");
+  xpu::_upsample_bilinear2d_aa_backward_out_kernel(
+      grad_input, grad_output, output_size, input_size, align_corners, scales_h, scales_w);
 }
 
 } // namespace native
