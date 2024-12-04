2f7392e5d60ad6d694b2cb82aff1606b22937819
mengfeil <mengfei.li@intel.com>
2024-12-04 13:36:31 +0800

    2.6-rolling

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-source /opt/intel/oneapi/compiler/latest/env/vars.sh
-source /opt/intel/oneapi/umf/latest/env/vars.sh
-source /opt/intel/oneapi/pti/latest/env/vars.sh
+echo "No need source DL-Essential for runtime"
