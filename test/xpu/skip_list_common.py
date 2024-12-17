9616f6deb0398839cc315210e20e6b17aa0b3270
Cheng <penghui.cheng@intel.com>
2024-12-17 04:04:54 +0000

    skip UT cases
    Signed-off-by: Cheng <penghui.cheng@intel.com>

--- test/xpu/skip_list_common.py
+++ test/xpu/skip_list_common.py
@@ -1549,6 +1549,7 @@ skip_dict = {
         "test_bmm_tunableop_rocm_xpu_float32",
         "test_numeric_check_leak_tunableop_rocm_xpu_float32",
         "test_dump_results_on_exit_tunableop_xpu_float32",
+        "test_rotating_buffer_tunableop_xpu_float32",
         # CUDA bias cases added in latest PyTorch
         # AttributeError: module 'torch._C' has no attribute '_cuda_tunableop_enable'
         "test_matmul_check_entries_tunableop_xpu_float16",
