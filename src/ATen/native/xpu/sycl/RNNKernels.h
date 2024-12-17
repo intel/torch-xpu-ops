c530864fdef61e771b033ef22ff981781ab8ef80
Yutao Xu <yutao.xu@intel.com>
2024-12-17 11:43:36 +0800

    Update RNNKernels.h

--- src/ATen/native/xpu/sycl/RNNKernels.h
+++ src/ATen/native/xpu/sycl/RNNKernels.h
@@ -24,8 +24,8 @@ TORCH_XPU_API std::tuple<Tensor, Tensor> _thnn_fused_gru_cell_kernel(
     const Tensor& input_gates,
     const Tensor& hidden_gates,
     const Tensor& hx,
-    const c10::optional<Tensor>& input_bias_opt,
-    const c10::optional<Tensor>& hidden_bias_opt);
+    const std::optional<Tensor>& input_bias_opt,
+    const std::optional<Tensor>& hidden_bias_opt);
 
 TORCH_XPU_API std::tuple<Tensor, Tensor, Tensor, Tensor, Tensor>
 _thnn_fused_gru_cell_backward_kernel(
