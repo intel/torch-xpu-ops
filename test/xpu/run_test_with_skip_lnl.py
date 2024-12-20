fb4a91ea8bd9cc1997a3da70c07a4d6b3e7ce3c4
Cheng, Penghui <penghui.cheng@intel.com>
2024-12-20 04:50:32 -0800

    Add skip list for LNL and BGM device
    Signed-off-by: Cheng, Penghui <penghui.cheng@intel.com>

---
+++ test/xpu/run_test_with_skip_lnl.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from skip_list_common import skip_dict
+from skip_list_win import skip_dict as skip_dict_win
+from skip_list_win_lnl import skip_dict as skip_dict_win_lnl
+from xpu_test_utils import launch_test
+
+
+res = 0
+IS_WINDOWS = sys.platform == "win32"
+
+for key in skip_dict:
+    skip_list = skip_dict[key]
+    if IS_WINDOWS and key in skip_dict_win:
+        skip_list += skip_dict_win[key]
+    if IS_WINDOWS and key in skip_dict_win_lnl:
+        skip_list += skip_dict_win_lnl[key]
+    res += launch_test(key, skip_list)
+
+if os.name == "nt":
+    sys.exit(res)
+else:    
+    exit_code = os.WEXITSTATUS(res)
+    sys.exit(exit_code)
