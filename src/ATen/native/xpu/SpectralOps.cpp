6e8f7cc1ed1a03f55e7c397652218b2025555d27
Cui, Yifeng <yifeng.cui@intel.com>
2024-12-24 00:55:26 -0800

    Enable fft_c2r

--- src/ATen/native/xpu/SpectralOps.cpp
+++ src/ATen/native/xpu/SpectralOps.cpp
@@ -42,4 +42,25 @@ Tensor& _fft_c2c_xpu_out(
 #endif // USE_ONEMKL
 }
 
+Tensor _fft_c2r_xpu(
+    const Tensor& self,
+    IntArrayRef dim,
+    int64_t normalization,
+    int64_t last_dim_size) {
+  TORCH_CHECK(self.is_complex());
+
+  return native::xpu::_fft_c2r_mkl(self, dim, normalization, last_dim_size);
+}
+
+Tensor _fft_c2r_xpu_out(
+    const Tensor& self,
+    IntArrayRef dim,
+    int64_t normalization,
+    int64_t last_dim_size,
+    Tensor& out) {
+  TORCH_CHECK(self.is_complex());
+
+  return native::xpu::_fft_c2r_mkl_out(self, dim, normalization, last_dim_size, out);
+}
+
 } // namespace at::native
