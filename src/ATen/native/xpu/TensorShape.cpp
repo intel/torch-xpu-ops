18bcd9aaa3353b8a686bde8910c0b818a38cdd79
yucai-intel <108388355+yucai-intel@users.noreply.github.com>
2025-01-03 17:32:06 +0800

    Add aten::split_with_sizes_copy and _chunk_cat relative ops (#1089)
    - [x] split_with_sizes_copy
    - [x] split_with_sizes_copy.out
    - [x] _chunk_cat
    - [x] _chunk_cat.out
    
    ---------
    
    Co-authored-by: Yutao Xu <yutao.xu@intel.com>

--- src/ATen/native/xpu/TensorShape.cpp
+++ src/ATen/native/xpu/TensorShape.cpp
@@ -7,6 +7,7 @@
 #include <ATen/native/TensorShape.h>
 #include <ATen/native/TypeProperties.h>
 #include <ATen/native/xpu/sycl/ShapeKernels.h>
+#include <ATen/native/xpu/sycl/TensorShapeKernels.h>
 #include <comm/RegisterUtils.h>
 #include <xpu/ATen/ops/as_strided_copy_native.h>
 #include <xpu/ATen/ops/as_strided_native.h>
@@ -56,6 +57,26 @@ TORCH_IMPL_FUNC(cat_out_xpu)
       all_same_sizes_and_stride,
       memory_format,
       result);
+}
+
+void split_with_sizes_copy_out_xpu(
+    const Tensor& self,
+    IntArrayRef split_sizes,
+    int64_t dim,
+    TensorList out) {
+  xpu::split_with_sizes_copy_out_xpu_kernel(self, split_sizes, dim, out);
+}
+
+Tensor _chunk_cat_xpu(TensorList tensors, int64_t dim, int64_t num_chunks) {
+  return xpu::_chunk_cat_xpu_kernel(tensors, dim, num_chunks);
+}
+
+Tensor& _chunk_cat_out_xpu(
+    TensorList tensors,
+    int64_t dim,
+    int64_t num_chunks,
+    Tensor& out) {
+  return xpu::_chunk_cat_out_xpu_kernel(tensors, dim, num_chunks, out);
 }
 
 } // namespace native
