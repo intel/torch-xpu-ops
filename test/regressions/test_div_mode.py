c96f676d8e1f856f361d70c0b8c00e82175e5dce
Deng, Daisy <daisy.deng@intel.com>
2025-01-08 13:53:30 +0000

    test/regressions pass lint check

--- test/regressions/test_div_mode.py
+++ test/regressions/test_div_mode.py
@@ -1,13 +1,15 @@
+# Owner(s): ["module: intel"]
 import torch
-from torch.testing._internal.common_utils import TestCase
 from torch.testing._internal.common_dtype import get_all_dtypes
+from torch.testing._internal.common_utils import TestCase
+
 
 class TestDivMode(TestCase):
     def test_div_true_dtype(self):
         claimed_dtypes = get_all_dtypes()
         for dtype in claimed_dtypes:
             a_cpu = torch.randint(1, 100, [8, 8]).to(dtype)
-            a_xpu = a_cpu.to('xpu')
+            a_xpu = a_cpu.to("xpu")
             ref = torch.ops.aten.div(a_cpu * 2, a_cpu, rounding_mode=None)
             res = torch.ops.aten.div(a_xpu * 2, a_xpu, rounding_mode=None)
-            self.assertEqual(ref, res.to('cpu'))
+            self.assertEqual(ref, res.to("cpu"))
--- test/regressions/test_foreach_scalar.py
+++ test/regressions/test_foreach_scalar.py
@@ -1,8 +1,9 @@
-import torch
+# Owner(s): ["module: intel"]
 import random
-from torch.testing._internal.common_utils import TestCase
-from torch.testing._internal.common_dtype import get_all_dtypes
 
+import torch
+from torch.testing._internal.common_dtype import get_all_dtypes
+from torch.testing._internal.common_utils import TestCase
 
 
 class ForeachTest:
@@ -36,22 +37,29 @@ class TestForeachScalarMethod(TestCase):
             torch.float16,
             torch.bfloat16,
             torch.cfloat,
-            torch.cdouble]
+            torch.cdouble,
+        ]
         int_types = [
             torch.uint8,
             torch.int8,
             torch.int16,
             torch.int32,
             torch.int64,
-            torch.bool]
+            torch.bool,
+        ]
         if dtype in float_types:
-            x1 = [torch.randint(1, 100, [5, 8]).to(torch.float).div(1000.0).to(dtype) for _ in range(250)]
+            x1 = [
+                torch.randint(1, 100, [5, 8]).to(torch.float).div(1000.0).to(dtype)
+                for _ in range(250)
+            ]
             scalar = random.uniform(-5, 5)
         elif dtype in int_types:
             x1 = [torch.randint(1, 100, [5, 8]).to(dtype) for _ in range(250)]
             scalar = torch.randint(1, 10, [1]).to(dtype).item()
         else:
-            assert False, "TestForeachScalarMethod::create_sample unsupported dtype"
+            AssertionError(
+                False, "TestForeachScalarMethod::create_sample unsupported dtype"
+            )
         return x1, scalar
 
     def test_foreach_add(self, dtype=torch.float):
