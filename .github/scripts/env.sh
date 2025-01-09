6b4bad9963bf6106be676114f8cab379359b9fb4
Cui, Yifeng <yifeng.cui@intel.com>
2025-01-09 00:50:48 -0800

    Source ONEMKL in CI

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -6,6 +6,7 @@ if [ "$1" != "nightly_wheel" ];then
     source /opt/intel/oneapi/pti/latest/env/vars.sh
     source /opt/intel/oneapi/ccl/latest/env/vars.sh
     source /opt/intel/oneapi/mpi/latest/env/vars.sh
+    source /opt/intel/oneapi/mkl/latest/env/vars.sh
 else
     echo "Don't need to source DL-Essential for nightly wheel"
 fi
