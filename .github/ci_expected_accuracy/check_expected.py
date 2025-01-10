670807fa01a13bb53e469bf2c9932beaec0de829
mengfeil <mengfei.li@intel.com>
2025-01-10 16:39:37 +0800

    Skip known issues

--- .github/ci_expected_accuracy/check_expected.py
+++ .github/ci_expected_accuracy/check_expected.py
@@ -67,7 +67,8 @@ for model_name in model_names:
     else:
         if refer_accuracy == "N/A":
             new_models.append([model_name, test_accuracy])
-            real_failed_models.append([model_name, test_accuracy])
+            # Not failed for new models
+            expected_failed_models.append([model_name, test_accuracy])
             refer_data.loc[len(refer_data),:] = "N/A"
             refer_data.at[len(refer_data) - 1, "name"] = model_name
             refer_data.at[len(refer_data) - 1, args.dtype] = test_accuracy
