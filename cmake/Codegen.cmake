d98cf1dc299029440f98e32221256f6e257f1528
Yutao Xu <yutao.xu@intel.com>
2025-01-13 14:59:39 +0800

    Relocate generated files to adapt to the latest design (#1281)
    Resolve https://github.com/intel/torch-xpu-ops/issues/1279

--- cmake/Codegen.cmake
+++ cmake/Codegen.cmake
@@ -6,8 +6,8 @@ set(Codegen_GPU_cmake_included true)
 set(BUILD_TORCH_XPU_ATEN_GENERATED "${CMAKE_BINARY_DIR}/xpu/ATen/")
 file(MAKE_DIRECTORY ${BUILD_TORCH_XPU_ATEN_GENERATED})
 
-set(RegisterXPU_PATH ${BUILD_TORCH_XPU_ATEN_GENERATED}/RegisterXPU.cpp)
-set(RegisterSparseXPU_PATH ${BUILD_TORCH_XPU_ATEN_GENERATED}/RegisterSparseXPU.cpp)
+set(RegisterXPU_PATH ${BUILD_TORCH_XPU_ATEN_GENERATED}/RegisterXPU_0.cpp)
+set(RegisterSparseXPU_PATH ${BUILD_TORCH_XPU_ATEN_GENERATED}/RegisterSparseXPU_0.cpp)
 set(XPUFallback_PATH ${TORCH_XPU_OPS_ROOT}/src/ATen/native/xpu/XPUFallback.template)
 
 if(WIN32)
@@ -45,8 +45,8 @@ function(GEN_BACKEND file_yaml)
 endfunction(GEN_BACKEND)
 
 
-set(RegisterXPU_PATH ${BUILD_TORCH_XPU_ATEN_GENERATED}/RegisterXPU.cpp)
-set(RegisterSparseXPU_PATH ${BUILD_TORCH_XPU_ATEN_GENERATED}/RegisterSparseXPU.cpp)
+set(RegisterXPU_PATH ${BUILD_TORCH_XPU_ATEN_GENERATED}/RegisterXPU_0.cpp)
+set(RegisterSparseXPU_PATH ${BUILD_TORCH_XPU_ATEN_GENERATED}/RegisterSparseXPU_0.cpp)
 set(XPUFallback_PATH ${TORCH_XPU_OPS_ROOT}/src/ATen/native/xpu/XPUFallback.template)
 set(XPU_AOTI_INSTALL_DIR ${TORCH_ROOT}/torch/csrc/inductor/aoti_torch/generated/extend)
 function(GEN_XPU file_yaml)
@@ -78,7 +78,7 @@ function(GEN_XPU file_yaml)
     --per-operator-headers
     --static-dispatch-backend
     --backend-whitelist XPU SparseXPU
-    # --xpu: generate in-tree RegisterXPU.cpp for in-tree OPs
+    # --xpu: generate in-tree RegisterXPU_0.cpp for in-tree OPs
     --xpu
     # --update-aoti-c-shim: generate extend/c_shim_xpu.h
     --update-aoti-c-shim
@@ -115,13 +115,13 @@ endfunction(GEN_XPU)
 # GEN_BACKEND(
 #   xpu_functions.yaml
 #   XPUNativeFunctions.h
-#   RegisterXPU.cpp)
+#   RegisterXPU_0.cpp)
 
 GEN_XPU(
   native_functions.yaml
   ${BUILD_TORCH_XPU_ATEN_GENERATED}/XPUFunctions.h
-  ${BUILD_TORCH_XPU_ATEN_GENERATED}/RegisterXPU.cpp
-  ${BUILD_TORCH_XPU_ATEN_GENERATED}/RegisterSparseXPU.cpp
+  ${BUILD_TORCH_XPU_ATEN_GENERATED}/RegisterXPU_0.cpp
+  ${BUILD_TORCH_XPU_ATEN_GENERATED}/RegisterSparseXPU_0.cpp
   ${XPU_AOTI_INSTALL_DIR}/c_shim_xpu.h
   ${XPU_AOTI_INSTALL_DIR}/c_shim_xpu.cpp
 )
