f74acae834e28503043560ca8be85e46182c4ae5
Sun, Jiwei1 <jiwei1.sun@intel.com>
2025-01-15 07:14:21 +0000

    add M > 1

--- src/ATen/native/xpu/sycl/Dequant_int4.cpp
+++ src/ATen/native/xpu/sycl/Dequant_int4.cpp
@@ -1,5 +1,6 @@
 #include <ATen/native/xpu/sycl/Dequant_int4.h>
 #include <comm/SYCLContext.h>
+<<<<<<< HEAD
 
 namespace at::native::xpu {
 void dequant_int4_kernel(
@@ -10,18 +11,10 @@ void dequant_int4_kernel(
   int constexpr SgSize = 16;
   int constexpr TileK = 16;
   int constexpr TileN = 16;
+=======
+>>>>>>> 454bce9 (add M > 1)
 
-  int constexpr GroupN = SgSize * TileN;
-  int constexpr GroupK = TileK;
-  static_assert(TileN % 2 == 0);
-  assert(qGroupSize % TileK == 0);
-  int n = weight.size(0);
-  int k = weight.size(1);
-  int nsg_k = k / GroupK;
-  int nsg_n = n / GroupN;
-  sycl::range<1> global_range{static_cast<size_t>(nsg_n) * nsg_k * SgSize};
-  sycl::range<1> local_range{SgSize};
-}
+namespace at::native::xpu {
 
 template <
     typename scalar_t = sycl::half,
@@ -35,15 +28,15 @@ struct DequantInt4KernelFunctor : public __SYCL_KER_CONFIG_CONVENTION__ {
       int k,
       const uint8_t* weight_int4,
       const scalar_t* ScaleAndZeros,
-      scalar_t* weight_dequant) {
-    this->n = n;
-    this->k = k;
-    this->weight_int4 = weight_int4;
-    this->ScaleAndZeros = ScaleAndZeros;
-    this->weight_dequant = weight_dequant;
-  }
+      scalar_t* weight_dequant)
+      : n(n),
+        k(k),
+        weight_int4(weight_int4),
+        ScaleAndZeros(ScaleAndZeros),
+        weight_dequant(weight_dequant) {}
+
   void sycl_ker_config_convention(sycl::handler& cgh) {}
-  [[intel::reqd_sub_group_size(16)]] void operator()(
+  [[intel::reqd_sub_group_size(SgSize)]] void operator()(
       sycl::nd_item<1> it) const {
     int constexpr GroupN = SgSize * TileN;
     int constexpr GroupK = TileK;
@@ -91,5 +84,36 @@ struct DequantInt4KernelFunctor : public __SYCL_KER_CONFIG_CONVENTION__ {
   const scalar_t* ScaleAndZeros;
   scalar_t* weight_dequant;
 };
+
+void dequant_int4_kernel(
+    const Tensor& weight_int4,
+    Tensor& weight,
+    int qGroupSize,
+    const Tensor& qScaleAndZeros) {
+  auto& sycl_queue = at::xpu::getCurrentSYCLQueue();
+
+  int constexpr SgSize = 16;
+  int constexpr TileK = 16;
+  int constexpr TileN = 16;
+  using scalar_t = sycl::half;
+  int constexpr GroupN = SgSize * TileN;
+  int constexpr GroupK = TileK;
+  static_assert(TileN % 2 == 0);
+  assert(qGroupSize % TileK == 0);
+  int n = weight.size(0);
+  int k = weight.size(1);
+  int nsg_k = k / GroupK;
+  int nsg_n = n / GroupN;
+  sycl::range<1> global_range{static_cast<size_t>(nsg_n) * nsg_k * SgSize};
+  sycl::range<1> local_range{SgSize};
+  DequantInt4KernelFunctor<scalar_t, 32, TileK, TileN, SgSize> kfn =
+      DequantInt4KernelFunctor<scalar_t, 32, TileK, TileN, SgSize>(
+          n,
+          k,
+          reinterpret_cast<const uint8_t*>(weight_int4.data_ptr()),
+          reinterpret_cast<const scalar_t*>(qScaleAndZeros.data_ptr()),
+          reinterpret_cast<scalar_t*>(weight.data_ptr()));
+  sycl_kernel_submit(global_range, local_range, sycl_queue, kfn);
+}
 
 } // namespace at::native::xpu
