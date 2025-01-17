2a2e3cd71d30565de6d03a55f0b2b2a5d00db92e
Yutao Xu <yutao.xu@intel.com>
2025-01-17 12:42:14 +0800

    Update NestedTensorTransformerFunctionKernels.h

--- src/ATen/native/nested/xpu/sycl/NestedTensorTransformerFunctionKernels.h
+++ src/ATen/native/nested/xpu/sycl/NestedTensorTransformerFunctionKernels.h
@@ -40,4 +40,15 @@ TORCH_XPU_API void remove_padding_transform0213_kernel_half(
     int64_t output_dim,
     const int64_t batch_size);
 
+TORCH_XPU_API void add_padding_kernel(
+    at::Tensor input,
+    at::Tensor output,
+    double padding,
+    const at::Tensor offsets,
+    const at::Tensor nt_sizes,
+    int input_dim,
+    const std::vector<int64_t>& new_size,
+    const int batch_size,
+    const int output_batch_size);
+
 } // namespace at::native::xpu
