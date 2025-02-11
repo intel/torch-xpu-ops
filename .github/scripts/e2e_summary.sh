8132ccc1c437f0ab8c5c3475d3488fe3b8bd8c3b
mengfeil <mengfei.li@intel.com>
2025-02-11 09:51:55 +0800

    modify

--- .github/scripts/e2e_summary.sh
+++ .github/scripts/e2e_summary.sh
@@ -3,14 +3,14 @@
 results_dir="$1"
 
 # Accuracy
-accuracy=$(find ${results_dir} -name "*.csv" |grep -E "_xpu_accuracy.csv" |wc -l)
-if [ ${accuracy} -gt 0 ];then
+accuracy=$(find "${results_dir}" -name "*.csv" |grep -E "_xpu_accuracy.csv" -c)
+if [ "${accuracy}" -gt 0 ];then
     echo "### Accuracy"
     echo "| Category | Passed | Total | Pass Rate |"
     echo "| --- | --- | --- | --- |"
-    for csv in $(find ${results_dir} -name "*.csv" |grep -E "_xpu_accuracy.csv" |sort)
+    for csv in $(find "${results_dir}" -name "*.csv" |grep -E "_xpu_accuracy.csv" |sort)
     do
-        category="$(echo ${csv} |sed 's/.*inductor_//;s/_xpu_accuracy.*//')"
+        category="$(echo "${csv}" |sed 's/.*inductor_//;s/_xpu_accuracy.*//')"
         test_result="$(awk -F ',' 'BEGIN{
             total = 0;
             pass = 0;
@@ -26,20 +26,20 @@ if [ ${accuracy} -gt 0 ];then
             }
         }END{
             printf("%d | %d | %.2f%\n", total, pass, pass/total*100)
-        }' ${csv})"
+        }' "${csv}")"
         echo "| ${category} | ${test_result} |"
     done
 fi
 
 # Performance
-performance=$(find ${results_dir} -name "*.csv" |grep -E "_xpu_performance.csv" |wc -l)
-if [ ${performance} -gt 0 ];then
+performance=$(find "${results_dir}" -name "*.csv" |grep -E "_xpu_performance.csv" -c)
+if [ "${performance}" -gt 0 ];then
     echo "### Performance"
     echo "| Category | Passed | Total | Pass Rate | Speedup |"
     echo "| --- | --- | --- | --- | --- |"
-    for csv in $(find ${results_dir} -name "*.csv" |grep -E "_xpu_performance.csv" |sort)
+    for csv in $(find "${results_dir}" -name "*.csv" |grep -E "_xpu_performance.csv" |sort)
     do
-        category="$(echo ${csv} |sed 's/.*inductor_//;s/_xpu_performance.*//')"
+        category="$(echo "${csv}" |sed 's/.*inductor_//;s/_xpu_performance.*//')"
         test_result="$(awk -M -v PREC=1024 -F ',' 'BEGIN{
             total = 0;
             pass = 0;
@@ -57,7 +57,7 @@ if [ ${performance} -gt 0 ];then
             }
         }END{
             printf("%d | %d | %.2f% | %.3f\n", total, pass, pass/total*100, speedup^(1/pass))
-        }' ${csv})"
+        }' "${csv}")"
         echo "| ${category} | ${test_result} |"
     done
 fi
