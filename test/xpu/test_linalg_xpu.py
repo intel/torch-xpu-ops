f6f859ff1b85bbec3b2c99477f9f1d2c62aac90f
Sun, Jiwei1 <jiwei1.sun@intel.com>
2025-02-11 10:17:00 +0000

    add transpose

--- test/xpu/test_linalg_xpu.py
+++ test/xpu/test_linalg_xpu.py
@@ -275,9 +275,7 @@ def _int4_mm(self, device, m, k, n):
             ],
             2,
         )
-
-        if out.device.type != "xpu":
-            scales_and_zeros = scales_and_zeros.transpose(0, 1).contiguous()
+        scales_and_zeros = scales_and_zeros.transpose(0, 1).contiguous()
         return out, scales_and_zeros
 
     def convert_weight_to_int4pack(b):
