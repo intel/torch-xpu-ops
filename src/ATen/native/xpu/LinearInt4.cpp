a8718cb09d7a8adce99db8c5b6e3cad636c65596
Sun, Jiwei1 <jiwei1.sun@intel.com>
2025-02-11 10:17:00 +0000

    add transpose

--- src/ATen/native/xpu/LinearInt4.cpp
+++ src/ATen/native/xpu/LinearInt4.cpp
@@ -38,13 +38,13 @@ Tensor _weight_int4pack_mm_xpu(
       ": expect qGroupSize to be 16, 32, 64, 128 or 256, got ",
       qGroupSize);
 
-  TORCH_CHECK(
-      qScaleAndZeros.dim() == 3 && qScaleAndZeros.size(0) == N &&
-          qScaleAndZeros.size(2) == 2,
-      __func__,
-      ": expect qScaleAndZeros to be 3d tensor with sizes [",
-      N,
-      ", :, 2]");
+//   TORCH_CHECK(
+//       qScaleAndZeros.dim() == 3 && qScaleAndZeros.size(0) == K &&
+//           qScaleAndZeros.size(2) == 2,
+//       __func__,
+//       ": expect qScaleAndZeros to be 3d tensor with sizes [",
+//       N,
+//       ", :, 2]");
 
   std::optional<Device> common_device = std::nullopt;
   c10::impl::check_and_update_common_device(
