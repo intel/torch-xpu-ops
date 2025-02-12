a1acd278b0e183d98a1be31ce453a7db877ac93b
Zhong, Ruijie <ruijie.zhong@intel.com>
2025-02-12 06:13:08 +0000

    align the lint check

--- test/xpu/xpu_test_utils.py
+++ test/xpu/xpu_test_utils.py
@@ -1155,7 +1155,7 @@ def launch_test(test_case, skip_list=None, exe_list=None):
             skip_options += skip_option
         skip_options += '"'
         test_command = (
-            f"pytest -v --junit-xml=./op_ut_with_skip_{test_case}.xml " 
+            f"pytest -v --junit-xml=./op_ut_with_skip_{test_case}.xml "
             + test_case
         )
         test_command += skip_options
@@ -1166,13 +1166,13 @@ def launch_test(test_case, skip_list=None, exe_list=None):
             exe_options += exe_option
         exe_options += '"'
         test_command = (
-            f"pytest -v --junit-xml=./op_ut_with_skip_{test_case}.xml " 
+            f"pytest -v --junit-xml=./op_ut_with_skip_{test_case}.xml "
             + test_case
         )
         test_command += exe_options
     else:
         test_command = (
-            f"pytest -v --junit-xml=./op_ut_with_skip_{test_case}.xml " 
+            f"pytest -v --junit-xml=./op_ut_with_skip_{test_case}.xml "
             + test_case
         )
     return os.system(test_command)
