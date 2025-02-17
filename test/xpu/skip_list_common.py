f1c09337fd4dc2e6395cbf739b5d3f5045676a8a
Deng, Daisy <daisy.deng@intel.com>
2025-02-17 02:35:45 +0000

    add unexpected success cases in skip list

--- test/xpu/skip_list_common.py
+++ test/xpu/skip_list_common.py
@@ -1002,6 +1002,11 @@ skip_dict = {
         "test_to_nn_TransformerEncoder_eval_mode_swap_True_set_grad_True_xpu_float32",
         "test_to_nn_TransformerEncoder_train_mode_swap_True_set_grad_True_xpu_float32",
         "test_to_nn_Transformer_swap_True_set_grad_True_xpu_float32",
+        # Unexpected succuss under investigation after PR https://github.com/intel/torch-xpu-ops/pull/1365"
+        "test_memory_format_nn_Conv2d_xpu_float64",
+        "test_memory_format_nn_ConvTranspose2d_xpu_float64",
+        "test_memory_format_nn_LazyConv2d_xpu_float64",
+        "test_memory_format_nn_LazyConvTranspose2d_xpu_float64",
     ),
 
     "test_nn_xpu.py": (
