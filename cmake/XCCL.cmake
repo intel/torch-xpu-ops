2535e873d86d6bd06c01a8fd36265beeb7f55302
Yu, Guangye <guangye.yu@intel.com>
2025-02-18 15:38:22 +0000

    Update USE_XCCL back to PyTorch

--- cmake/XCCL.cmake
+++ cmake/XCCL.cmake
@@ -4,18 +4,17 @@ if(NOT __XCCL_INCLUDED)
   # XCCL_ROOT, XCCL_LIBRARY_DIR, XCCL_INCLUDE_DIR are handled by FindXCCL.cmake.
   find_package(XCCL REQUIRED)
   if(NOT XCCL_FOUND)
-    message("${XCCL_NOT_FOUND_MESSAGE}")
+    set(PYTORCH_FOUND_XCCL FALSE)
+    message(WARNING "${XCCL_NOT_FOUND_MESSAGE}")
     return()
   endif()
-  if(XCCL_FOUND)
-    add_library(torch::xccl INTERFACE IMPORTED)
-    set_property(
-      TARGET torch::xccl PROPERTY INTERFACE_INCLUDE_DIRECTORIES
-      ${XCCL_INCLUDE_DIR})
-    set_property(
-      TARGET torch::xccl PROPERTY INTERFACE_LINK_LIBRARIES
-      ${XCCL_LIBRARY})
-    set(USE_C10D_XCCL ON)
-    set(USE_C10D_XCCL ${USE_C10D_XCCL} PARENT_SCOPE)
-  endif()
+
+  set(PYTORCH_FOUND_XCCL TRUE)
+  add_library(torch::xccl INTERFACE IMPORTED)
+  set_property(
+    TARGET torch::xccl PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+    ${XCCL_INCLUDE_DIR})
+  set_property(
+    TARGET torch::xccl PROPERTY INTERFACE_LINK_LIBRARIES
+    ${XCCL_LIBRARY})
 endif()
