98eb85d7bcb6a6e436d07800b062095a55ca3dc3
yucai <yu.cai@intel.com>
2025-02-24 02:39:39 +0000

    del annotation

--- src/ATen/native/xpu/sycl/ForeachCopyKernels.cpp
+++ src/ATen/native/xpu/sycl/ForeachCopyKernels.cpp
@@ -100,24 +100,6 @@ void foreach_copy_list_kernel_(TensorList self, TensorList src) {
                   Identity<opmath_t>());
             });
       });
-
-  // AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(
-  //     at::ScalarType::Half,
-  //     at::ScalarType::BFloat16,
-  //     at::ScalarType::Bool,
-  //     self[0].scalar_type(),
-  //     "foreach_tensor_copy",
-  //     [&]() {
-  //       using opmath_t = at::opmath_type<scalar_t>;
-  //       multi_tensor_apply<2>(
-  //           tensor_lists,
-  //           UnaryOpFunctor<
-  //               scalar_t,
-  //               /* depth */ 2,
-  //               /* r_args_depth */ 1,
-  //               /* res_arg_index */ 1>(),
-  //           Identity<opmath_t>());
-  //     });
 }
 
 #undef AT_DISPATCH_SOURCE_TYPES
