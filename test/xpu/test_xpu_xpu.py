dd8ff95b2f193cc9b2375fd0395a3995abfa79e0
Zhong, Ruijie <ruijie.zhong@intel.com>
2025-02-26 07:39:33 +0000

    fix instantiate issue

--- test/xpu/test_xpu_xpu.py
+++ test/xpu/test_xpu_xpu.py
@@ -1,6 +1,5 @@
 # Owner(s): ["module: intel"]
 
-from torch.testing._internal.autocast_test_lists import TestAutocast
 from torch.testing._internal.common_device_type import instantiate_device_type_tests
 from torch.testing._internal.common_utils import run_tests, TestCase
 
@@ -13,13 +12,7 @@ with XPUPatchForImport(False):
     from test_xpu import TestXpu, TestXpuAutocast, TestXpuTrace
 
 instantiate_device_type_tests(TestXpu, globals(), only_for="xpu", allow_xpu=True)
-instantiate_device_type_tests(
-    TestXpuAutocast, globals(), only_for="xpu", allow_xpu=True
-)
-instantiate_device_type_tests(TestXpuTrace, globals(), only_for="xpu", allow_xpu=True)
-
 
 if __name__ == "__main__":
     TestCase._default_dtype_check_enabled = True
-    TestAutocast._default_dtype_check_enabled = True
     run_tests()
