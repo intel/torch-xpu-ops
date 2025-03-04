896793ba18f4d4d11fb3f0a3de32b2c46ad2b33f
mengfeil <mengfei.li@intel.com>
2025-03-04 13:34:21 +0800

    oneDNN v3.7.1

--- .github/scripts/apply_torch_pr.py
+++ .github/scripts/apply_torch_pr.py
@@ -15,6 +15,8 @@ parser.add_argument('--pr-list', '-n', nargs='+',
         "https://github.com/pytorch/pytorch/pull/143739",
         # Allow XPU device for validating the arguments to sparse compressed tensor factory functions
         "https://github.com/pytorch/pytorch/pull/147306",
+        # oneDNN v3.7.1
+        "https://github.com/pytorch/pytorch/pull/148403",
     ]
 )
 parser.add_argument('--extra-pr-list', '-e', nargs='+',default=[])
