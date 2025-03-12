510f2fcfe6cb7b1ebcc383eff5253623f75fa40f
mengfeil <mengfei.li@intel.com>
2025-03-12 15:06:58 +0800

    modify build

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$1" != "nightly_wheel" ];then
+if [ "$1" == "build" ];then
     source /opt/intel/oneapi/compiler/latest/env/vars.sh
     source /opt/intel/oneapi/umf/latest/env/vars.sh
     source /opt/intel/oneapi/pti/latest/env/vars.sh
@@ -8,5 +8,6 @@ if [ "$1" != "nightly_wheel" ];then
     source /opt/intel/oneapi/mpi/latest/env/vars.sh
     source /opt/intel/oneapi/mkl/latest/env/vars.sh
 else
+    pip install intel-cmplr-lib-rt intel-cmplr-lib-ur intel-cmplr-lic-rt intel-sycl-rt tcmlib umf intel-pti
     echo "Don't need to source DL-Essential for nightly wheel"
 fi
