ce61cc91aeb6c19c4b14aa31cd6c794423dd5aab
Zhong, Ruijie <ruijie.zhong@intel.com>
2025-03-13 01:17:54 +0000

    update cmake set

--- .github/scripts/copy.bat
+++
@@ -1,9 +0,0 @@
-copy "%CONDA_PREFIX%\Library\bin\libiomp*5md.dll" .\torch\lib
-:: Should be set in build_pytorch.bat
-copy "%CONDA_PREFIX%\Library\bin\uv.dll" .\torch\lib
-
-if defined CMAKE_PREFIX_PATH (
-    set CMAKE_PREFIX_PATH=%CONDA_PREFIX%\Library;%CMAKE_PREFIX_PATH%
-) else (
-    set CMAKE_PREFIX_PATH=%CONDA_PREFIX%\Library
-)
