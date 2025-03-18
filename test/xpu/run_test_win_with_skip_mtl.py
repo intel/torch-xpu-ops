a58bd4e12830ab7593db58076775b2bae099d665
Zhong, Ruijie <ruijie.zhong@intel.com>
2025-03-18 01:28:45 +0000

    update skip function

--- test/xpu/run_test_win_with_skip_mtl.py
+++ test/xpu/run_test_win_with_skip_mtl.py
@@ -5,13 +5,13 @@ from skip_list_win_mtl import skip_dict
 
 IS_WINDOWS = sys.platform == "win32"
 
-skip_list = skip_dict["test_xpu_xpu.py"]
+skip_list = skip_dict["test_xpu.py"]
 
 skip_options = "not " + skip_list[0]
 for skip_case in skip_list[1:]:
     skip_option = " and not " + skip_case
     skip_options += skip_option
 
-test_command = ["-k", skip_options, "test_xpu_xpu.py", "-v"]
+test_command = ["-k", skip_options, "../../../../test/test_xpu.py", "-v"]
 res = pytest.main(test_command)
 sys.exit(res)
