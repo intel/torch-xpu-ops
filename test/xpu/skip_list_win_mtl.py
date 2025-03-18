1b8e95b8a410c0289a0a2a901ef3f278b58363ce
Zhong, Ruijie <ruijie.zhong@intel.com>
2025-03-18 01:28:45 +0000

    update skip function

--- test/xpu/skip_list_win_mtl.py
+++ test/xpu/skip_list_win_mtl.py
@@ -1,6 +1,6 @@
 skip_dict = {
     # failed on MTL windows, skip first for Preci
-    "test_xpu_xpu.py": (
+    "test_xpu.py": (
         "test_lazy_init_xpu",
         "test_mem_get_info_xpu",
         "test_wrong_xpu_fork_xpu",
