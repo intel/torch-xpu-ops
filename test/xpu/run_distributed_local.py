28a259e59448bb70958a818d3f50fee62f2ebfa2
Cheng, Penghui <penghui.cheng@intel.com>
2025-04-03 02:01:55 -0700

    Fixed pylint error
    Signed-off-by: Cheng, Penghui <penghui.cheng@intel.com>

--- test/xpu/run_distributed_local.py
+++ test/xpu/run_distributed_local.py
@@ -17,29 +17,29 @@ os.environ["PYTHONPATH"] = "$PYTHONPATH:../../../../test/distributed/pipelining"
 ret = os.system("xpu-smi topology -m 2>&1|tee topology.log")
 if ret == 0:
     gpu_dict = {}
-    with open("topology.log", "r") as file:
+    with open("topology.log") as file:
         lines = file.readlines()
         for line in lines:
-           if "CPU Affinity" in line:
-              continue
-           line = line.strip()
-           if line.startswith("GPU "):
-               items = line.split(' ')
-               items = [x for x in items if x]
-               gpu_id = items[1]
-               i = gpu_id.split('/')[0]
-               affinity = ""
-               for j, item in enumerate(items):
-                   if "SYS" not in item and ( "XL" in item or "S" in item ):
-                      if len(affinity) == 0:
-                          affinity = str(j-2)
-                      else:
-                          affinity = affinity + ',' + str(j-2)
-               gpu_dict[i] = affinity
+            if "CPU Affinity" in line:
+                continue
+            line = line.strip()
+            if line.startswith("GPU "):
+                items = line.split(" ")
+                items = [x for x in items if x]
+                gpu_id = items[1]
+                i = gpu_id.split("/")[0]
+                affinity = ""
+                for j, item in enumerate(items):
+                    if "SYS" not in item and ("XL" in item or "S" in item):
+                        if len(affinity) == 0:
+                            affinity = str(j - 2)
+                        else:
+                            affinity = affinity + "," + str(j - 2)
+                gpu_dict[i] = affinity
 
     max_affinity = ""
     for key, value in gpu_dict.items():
-        if  len(value) > len(max_affinity):
+        if len(value) > len(max_affinity):
             max_affinity = value
 
     os.environ["ZE_AFFINITY_MASK"] = str(max_affinity)
@@ -49,12 +49,14 @@ else:
     print("xpu-smi topology failed")
     sys.exit(255)
 
+
 # run python test
 def run(test_command):
     result = subprocess.run(test_command, capture_output=True, text=True)
     print(result.stdout)
     print(result.stderr)
     return result
+
 
 for key in skip_dict_python:
     skip_list = skip_dict_python[key]
