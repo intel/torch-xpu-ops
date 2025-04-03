cbd34cd308e4cd601561c3ce64e44c408b94f730
Cheng, Penghui <penghui.cheng@intel.com>
2025-04-03 01:27:44 -0700

    Update distributed UT cases in DDP and PP
    Signed-off-by: Cheng, Penghui <penghui.cheng@intel.com>

--- test/xpu/run_distributed_local.py
+++ test/xpu/run_distributed_local.py
@@ -2,15 +2,17 @@ import os
 import subprocess
 import sys
 
-from skip_list_dist_local import skip_dict
+from skip_list_dist_local import skip_dict, skip_dict_python
 from xpu_test_utils import launch_test
 
 res = 0
+res2 = 0
 fail_test = []
 
 os.environ["CCL_ATL_TRANSPORT"] = "ofi"
 os.environ["CCL_SEND"] = "direct"
 os.environ["CCL_RECV"] = "direct" 
+os.environ["PYTHONPATH"] = "$PYTHONPATH:../../../../test/distributed/pipelining"
 # Get the xelink group card affinity
 ret = os.system("xpu-smi topology -m 2>&1|tee topology.log")
 if ret == 0:
@@ -48,6 +50,29 @@ else:
     print("xpu-smi topology failed")
     sys.exit(255)
 
+# run python test
+def run(test_command):
+    result = subprocess.run(test_command, capture_output=True, text=True)
+    print(result.stdout)
+    print(result.stderr)
+    return result
+
+for key in skip_dict_python:
+    skip_list = skip_dict_python[key]
+    test_command = ["python", key]
+    fail = run(test_command)
+    if fail.returncode:
+        for line in fail.stderr.split("\n"):
+            if "FAIL: " in line:
+                is_error = True
+                for skip_case in skip_list:
+                    if skip_case in line:
+                        print("Skiped error: ", key + " " + skip_case)
+                        is_error = False
+                if is_error:
+                    res2 += fail.returncode
+                    fail_test.append("".join(key + " " + line))
+
 # run pytest with skiplist
 for key in skip_dict:
     skip_list = skip_dict[key]
@@ -61,6 +86,6 @@ if fail_test:
 
 exit_code = os.WEXITSTATUS(res)
 if exit_code == 0:
-    sys.exit(res)
+    sys.exit(res2)
 else:
     sys.exit(exit_code)
