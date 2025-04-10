0fe9e6a390c0fa66a05a344de8103f32ef0cec14
Zhong, Ruijie <ruijie.zhong@intel.com>
2025-04-09 18:39:21 -0700

    verify the new fix

--- test/xpu/skip_list_win_mtl.py
+++ test/xpu/skip_list_win_mtl.py
@@ -1,8 +1,6 @@
 skip_dict = {
     # failed on MTL windows, skip first for Preci
     "test_xpu.py": (
-        "test_lazy_init_xpu",
         "test_mem_get_info_xpu",
-        "test_wrong_xpu_fork_xpu",
     ),
 }
