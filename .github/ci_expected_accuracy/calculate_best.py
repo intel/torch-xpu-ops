f0288b805e35547113e6eeae252209f1d4dfef62
mengfeil <mengfei.li@intel.com>
2025-04-15 14:39:34 +0800

    lint

--- .github/ci_expected_accuracy/calculate_best.py
+++ .github/ci_expected_accuracy/calculate_best.py
@@ -6,7 +6,8 @@ import argparse
 import pandas as pd
 from datetime import date
 
-parser = argparse.ArgumentParser(description="Get Best Performance", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser = argparse.ArgumentParser(description="Get Best Performance",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--best", default=None, required=True, help="Saved best performance file")
 parser.add_argument("--new", default=None, required=True, help="New round launch")
 parser.add_argument("--device", default=None, type=str, help="Device")
@@ -51,22 +52,42 @@ for new_file in new_files:
     for index, row in new_data.iterrows():
         new_eager = row["abs_latency"] * row["speedup"]
         # eager
-        eager_row = next(([i, line] for i, line in best_data.iterrows() if (line["Category"] == category and line["Model"] == row["name"] and line["Value Type"] == "eager")), "N/A")
-        best_eager_value = best_data.loc[(best_data["Category"] == category) & (best_data["Model"] == row["name"]) & (best_data["Value Type"] == "eager")]
+        eager_row = next(([i, line] for i, line in best_data.iterrows()
+                          if (line["Category"] == category
+                              and line["Model"] == row["name"]
+                              and line["Value Type"] == "eager")), "N/A")
+        best_eager_value = best_data.loc[
+            (best_data["Category"] == category) &
+            (best_data["Model"] == row["name"]) &
+            (best_data["Value Type"] == "eager")]
         if eager_row != "N/A":
             if new_eager < best_eager_value["Value"].values:
-                best_data.loc[eager_row[0]] = category,row["name"],"eager",new_eager,args.device,args.os,args.driver,args.oneapi,args.gcc,args.python,args.pytorch,args.torch_xpu_ops,date.today().strftime('%F')
+                best_data.loc[eager_row[0]] = category, row["name"], "eager", new_eager, \
+                    args.device, args.os, args.driver, args.oneapi, args.gcc, args.python, \
+                    args.pytorch, args.torch_xpu_ops, date.today().strftime('%F')
         else:
             best_data.loc[len(best_data), :] = None
-            best_data.loc[len(best_data) - 1] = category,row["name"],"eager",new_eager,args.device,args.os,args.driver,args.oneapi,args.gcc,args.python,args.pytorch,args.torch_xpu_ops,date.today().strftime('%F')
+            best_data.loc[len(best_data) - 1] = category, row["name"], "eager", new_eager, \
+                args.device, args.os, args.driver, args.oneapi, args.gcc, args.python, \
+                args.pytorch, args.torch_xpu_ops, date.today().strftime('%F')
         # inductor
-        inductor_row = next(([i, line] for i, line in best_data.iterrows() if line["Category"] == category and line["Model"] == row["name"] and line["Value Type"] == "inductor"), "N/A")
-        best_inductor_value = best_data.loc[(best_data["Category"] == category) & (best_data["Model"] == row["name"]) & (best_data["Value Type"] == "inductor")]
+        inductor_row = next(([i, line] for i, line in best_data.iterrows()
+                             if (line["Category"] == category
+                                 and line["Model"] == row["name"]
+                                 and line["Value Type"] == "inductor")), "N/A")
+        best_inductor_value = best_data.loc[
+            (best_data["Category"] == category) &
+            (best_data["Model"] == row["name"]) &
+            (best_data["Value Type"] == "inductor")]
         if inductor_row != "N/A":
             if row["abs_latency"] < best_inductor_value["Value"].values:
-                best_data.at[inductor_row[0]] = category,row["name"],"inductor",row["abs_latency"],args.device,args.os,args.driver,args.oneapi,args.gcc,args.python,args.pytorch,args.torch_xpu_ops,date.today().strftime('%F')
+                best_data.at[inductor_row[0]] = category, row["name"], "inductor", row["abs_latency"], \
+                    args.device, args.os, args.driver, args.oneapi, args.gcc, args.python, \
+                    args.pytorch, args.torch_xpu_ops, date.today().strftime('%F')
         else:
             best_data.loc[len(best_data), :] = None
-            best_data.loc[len(best_data) - 1] = category,row["name"],"inductor",row["abs_latency"],args.device,args.os,args.driver,args.oneapi,args.gcc,args.python,args.pytorch,args.torch_xpu_ops,date.today().strftime('%F')
+            best_data.loc[len(best_data) - 1] = category, row["name"], "inductor", row["abs_latency"], \
+                args.device, args.os, args.driver, args.oneapi, args.gcc, args.python, \
+                args.pytorch, args.torch_xpu_ops, date.today().strftime('%F')
 
 best_data.to_csv(args.best, sep=',', encoding='utf-8', index=False)
