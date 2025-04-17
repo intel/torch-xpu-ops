38df6636d20f94a63103b2992713e3baa15a985c
mengfeil <mengfei.li@intel.com>
2025-04-17 15:35:30 +0800

    remove platform in env.sh

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -8,14 +8,14 @@ if [ "$1" != "nightly_wheel" ];then
     source /opt/intel/oneapi/mpi/latest/env/vars.sh
     source /opt/intel/oneapi/mkl/latest/env/vars.sh
     export PYTORCH_EXTRA_INSTALL_REQUIREMENTS="\
-        intel-cmplr-lib-rt==2025.0.5; platform_system == 'Linux' |\
-        intel-cmplr-lib-ur==2025.0.5; platform_system == 'Linux' |\
-        intel-cmplr-lic-rt==2025.0.5; platform_system == 'Linux' |\
-        intel-sycl-rt==2025.0.5; platform_system == 'Linux' |\
-        impi-devel==2021.14.2; platform_system == 'Linux' |\
-        oneccl-devel==2021.14.1; platform_system == 'Linux' |\
-        mkl-devel==2025.0.1; platform_system == 'Linux' |\
-        onemkl-sycl-dft==2025.0.1; platform_system == 'Linux' |\
+        intel-cmplr-lib-rt==2025.0.5 |\
+        intel-cmplr-lib-ur==2025.0.5 |\
+        intel-cmplr-lic-rt==2025.0.5 |\
+        intel-sycl-rt==2025.0.5 |\
+        impi-devel==2021.14.2 |\
+        oneccl-devel==2021.14.1 |\
+        mkl-devel==2025.0.1 |\
+        onemkl-sycl-dft==2025.0.1 |\
         tcmlib==1.2.0 | umf==0.9.1 | intel-pti==0.10.2 \
     "
 else
