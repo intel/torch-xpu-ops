95f27bf12b36d7b7ee0101ed87c9a8f43dbc04f3
Cui, Yifeng <yifeng.cui@intel.com>
2025-04-17 07:28:30 -0700

    Remove fallback

--- src/ATen/native/xpu/mkl/BatchLinearAlgebra.h
+++ src/ATen/native/xpu/mkl/BatchLinearAlgebra.h
@@ -11,6 +11,7 @@ TORCH_XPU_API void svd_mkl(
     const c10::optional<c10::string_view>& driver,
     const Tensor& U,
     const Tensor& S,
-    const Tensor& Vh);
+    const Tensor& Vh,
+    const Tensor& info);
 
 } // namespace at::native::xpu
