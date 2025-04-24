65a1343c7eae78317b851da2630565d95fab953f
Yutao Xu <yutao.xu@intel.com>
2025-04-24 16:32:13 +0800

    Update test_softmax.py

--- test/regressions/test_softmax.py
+++ test/regressions/test_softmax.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from torch.testing._internal.common_utils import TestCase
 
 cpu_device = torch.device("cpu")
-dpcpp_device = torch.device("xpu")
+xpu_device = torch.device("xpu")
 
 
 class TestNNMethod(TestCase):
@@ -18,6 +18,7 @@ class TestNNMethod(TestCase):
             [7, 8, 11],
             [16, 7, 8, 512],
             [16, 7, 8, 512, 35],
+            [117, 7, 9, 513, 35],
         ]
         input_type = torch.float16
         output_type = torch.float
@@ -30,8 +31,8 @@ class TestNNMethod(TestCase):
                 y_cpu = F.softmax(x_cpu, dim, dtype=output_type)
                 y_cpu.backward(grad.clone())
 
-                x_dpcpp = x.clone().to(dpcpp_device).requires_grad_()
-                y_dpcpp = F.softmax(x_dpcpp, dim, dtype=output_type)
-                y_dpcpp.backward(grad.clone().to(dpcpp_device))
-                self.assertEqual(y_cpu, y_dpcpp.cpu())
-                self.assertEqual(x_cpu.grad, x_dpcpp.grad.cpu())
+                x_xou = x.clone().to(xpu_device).requires_grad_()
+                y_xpu = F.softmax(x_xpu, dim, dtype=output_type)
+                y_xpu.backward(grad.clone().to(xpu_device))
+                self.assertEqual(y_cpu, y_xpu.cpu())
+                self.assertEqual(x_cpu.grad, x_xpu.grad.cpu())
