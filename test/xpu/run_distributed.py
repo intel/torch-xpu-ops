bc8a43bad0ef013590e46dfa4e730799bd1ea06a
Han, Chao1 <chao1.han@intel.com>
2025-05-30 21:38:22 +0800

    update

--- test/xpu/run_distributed.py
+++ test/xpu/run_distributed.py
@@ -11,7 +11,7 @@ fail_test = []
 
 # libfabric WA to avoid hang issue
 os.environ["FI_PROVIDER"] = "tcp"
-os.environ["ZE_AFFINITY_MASK"] = "0,1,2,3"
+# os.environ["ZE_AFFINITY_MASK"] = "0,1,2,3"
 
 
 # run python test
