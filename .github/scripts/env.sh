e57c98166100fbea2be3456e3b99f5c8b88f604e
mengfei25 <mengfei.li@intel.com>
2025-06-27 14:40:59 +0800

    verify compiler

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-source /opt/intel/oneapi/compiler/latest/env/vars.sh
-source /opt/intel/oneapi/pti/latest/env/vars.sh
-source /opt/intel/oneapi/umf/latest/env/vars.sh
-source /opt/intel/oneapi/ccl/latest/env/vars.sh
-source /opt/intel/oneapi/mpi/latest/env/vars.sh
+source ${HOME}/intel/oneapi/compiler/latest/env/vars.sh
+source ${HOME}/intel/oneapi/pti/latest/env/vars.sh
+source ${HOME}/intel/oneapi/umf/latest/env/vars.sh
+source ${HOME}/intel/oneapi/ccl/latest/env/vars.sh
+source ${HOME}/intel/oneapi/mpi/latest/env/vars.sh
 icpx --version
 sycl-ls
