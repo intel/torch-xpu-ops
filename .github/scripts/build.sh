45292cf68fa3ca7a5fa6bf4592c7d315fd75bb3f
mengfei25 <mengfei.li@intel.com>
2025-06-27 15:29:15 +0800

    verify compiler

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -61,7 +61,7 @@ export PYTORCH_EXTRA_INSTALL_REQUIREMENTS=" \
     mkl-devel==2025.0.1 |\
     mkl==2025.0.1 | \
     onemkl-sycl-dft==2025.0.1 |\
-    tcmlib==1.2.0 | umf==0.9.1 | intel-pti==0.10.2 \
+    tcmlib==1.2.0 | umf==0.9.1 | intel-pti==0.10.2 | \
     onemkl-sycl-blas==2025.0.1 | \
     onemkl-sycl-dft==2025.0.1 | \
     onemkl-sycl-lapack==2025.0.1 | \
