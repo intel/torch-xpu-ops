a7d235760540375c89e241aaf4cd7e0cd920e4db
mengfei25 <mengfei.li@intel.com>
2025-07-01 17:33:44 +0800

    debug

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -48,16 +48,16 @@ python -m pip install requests
 python third_party/torch-xpu-ops/.github/scripts/apply_torch_pr.py
 git submodule sync && git submodule update --init --recursive
 python -m pip install -r requirements.txt
-python -m pip install mkl-static mkl-include
 export USE_STATIC_MKL=1
 export USE_XCCL=0
+if [[ "${BUILD_ONEAPI_NAME}"  == *"2025.0.2.9_offline"* ]];then
+python -m pip install mkl-static==2025.0.1 mkl-include==2025.0.1
 export PYTORCH_EXTRA_INSTALL_REQUIREMENTS=" \
     intel-cmplr-lib-rt==2025.0.5 |\
     intel-cmplr-lib-ur==2025.0.5 |\
     intel-cmplr-lic-rt==2025.0.5 |\
     intel-sycl-rt==2025.0.5 |\
     impi-devel==2021.14.2 |\
-    oneccl-devel==2021.14.1 |\
     mkl-devel==2025.0.1 |\
     mkl==2025.0.1 | \
     onemkl-sycl-dft==2025.0.1 |\
@@ -68,6 +68,53 @@ export PYTORCH_EXTRA_INSTALL_REQUIREMENTS=" \
     onemkl-sycl-rng==2025.0.1 | \
     onemkl-sycl-sparse==2025.0.1 \
 "
+elif [[ "${BUILD_ONEAPI_NAME}"  == *"2025.1.0.581_offline"* ]];then
+python -m pip install mkl-static==2025.1.0 mkl-include==2025.1.0
+export PYTORCH_EXTRA_INSTALL_REQUIREMENTS=" \
+    intel-cmplr-lib-rt==2025.1.0 | \
+    intel-cmplr-lib-ur==2025.1.0 | \
+    intel-cmplr-lic-rt==2025.1.0 | \
+    intel-sycl-rt==2025.1.0 | \
+    impi-rt==2021.15.0; platform_system == 'Linux' and platform_machine == 'x86_64' | \
+    onemkl-sycl-blas==2025.1.0 | \
+    onemkl-sycl-dft==2025.1.0 | \
+    onemkl-sycl-lapack==2025.1.0 | \
+    onemkl-sycl-rng==2025.1.0 | \
+    onemkl-sycl-sparse==2025.1.0 | \
+    dpcpp-cpp-rt==2025.1.0 | \
+    intel-opencl-rt==2025.1.0 | \
+    mkl==2025.1.0 | \
+    intel-openmp==2025.1.0 | \
+    tbb==2022.1.0 | \
+    tcmlib==1.3.0 | \
+    umf==0.10.0 | \
+    intel-pti==0.11.0
+"
+else
+python -m pip install mkl-static==2025.1.1 mkl-include==2025.1.1
+export PYTORCH_EXTRA_INSTALL_REQUIREMENTS=" \
+    intel-cmplr-lib-rt==2025.1.1 | \
+    intel-cmplr-lib-ur==2025.1.1 | \
+    intel-cmplr-lic-rt==2025.1.1 | \
+    intel-sycl-rt==2025.1.1 | \
+    oneccl-devel==2021.15.2; platform_system == 'Linux' and platform_machine == 'x86_64' | \
+    oneccl==2021.15.2; platform_system == 'Linux' and platform_machine == 'x86_64' | \
+    impi-rt==2021.15.0; platform_system == 'Linux' and platform_machine == 'x86_64' | \
+    onemkl-sycl-blas==2025.1.0 | \
+    onemkl-sycl-dft==2025.1.0 | \
+    onemkl-sycl-lapack==2025.1.0 | \
+    onemkl-sycl-rng==2025.1.0 | \
+    onemkl-sycl-sparse==2025.1.0 | \
+    dpcpp-cpp-rt==2025.1.1 | \
+    intel-opencl-rt==2025.1.1 | \
+    mkl==2025.1.0 | \
+    intel-openmp==2025.1.1 | \
+    tbb==2022.1.0 | \
+    tcmlib==1.3.0 | \
+    umf==0.10.0 | \
+    intel-pti==0.12.3
+"
+fi
 
 # Build
 sed -i "s/checkout --quiet \${TORCH_XPU_OPS_COMMIT}/log -n 1/g" caffe2/CMakeLists.txt
