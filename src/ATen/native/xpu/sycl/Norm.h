baecd96be8d89ed11693630c86e0cd0e48e3eb6e
guangyey <guangye.yu@intel.com>
2025-07-09 20:07:11 -0700

    Replace deprecated `[[intel::reqd_sub_group_size]]` with `[[sycl::reqd_sub_group_size]]`

--- src/ATen/native/xpu/sycl/Norm.h
+++ src/ATen/native/xpu/sycl/Norm.h
@@ -600,7 +600,7 @@ template <
     class Norm,
     bool one_moment = false>
 struct FusedNormKernelFunctor : public __SYCL_KER_CONFIG_CONVENTION__ {
-  [[intel::reqd_sub_group_size(SIMD)]] void operator()(
+  [[sycl::reqd_sub_group_size(SIMD)]] void operator()(
       sycl::nd_item<3> item_id) const {
     accscalar_t sum1 = 0;
     accscalar_t sum2 = 0;
@@ -747,7 +747,7 @@ template <
     class Norm,
     bool one_moment = false>
 struct RowwiseMomentsKernelFunctor : public __SYCL_KER_CONFIG_CONVENTION__ {
-  [[intel::reqd_sub_group_size(SIMD)]] void operator()(
+  [[sycl::reqd_sub_group_size(SIMD)]] void operator()(
       sycl::nd_item<3> item_id) const {
     index_t local_id = item_id.get_local_id(2);
 
