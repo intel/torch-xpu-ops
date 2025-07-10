ec803445df2cce6d5c53d02a1630789e8e13261f
mengfei25 <mengfei.li@intel.com>
2025-07-10 09:06:02 +0800

    debug

--- .github/scripts/guilty_search.sh
+++ .github/scripts/guilty_search.sh
@@ -4,9 +4,7 @@ export GIT_PAGER=cat
 
 # Init params
 WORKSPACE=$(realpath ${WORKSPACE:-"/tmp"})
-PYTORCH_REPO=${PYTORCH_REPO:-"https://github.com/pytorch/pytorch.git"}
 PYTORCH_VERSION=${PYTORCH_VERSION:-"main"}
-TORCH_XPU_OPS_REPO=${TORCH_XPU_OPS_REPO:-"https://github.com/intel/torch-xpu-ops.git"}
 TORCH_XPU_OPS_VERSION=${TORCH_XPU_OPS_VERSION:-"main"}
 for var; do
     eval "export $(echo ${var@Q} |sed "s/^'-*//g;s/=/='/")"
@@ -19,14 +17,12 @@ rm -rf ${WORKSPACE}/* || sudo rm -rf ${WORKSPACE}/*
 # Build pytorch
 pip uninstall -y torch
 source $(dirname $(realpath $0))/env.sh 2> /dev/null
-$(dirname $(realpath $0))/build.sh \
+build_status="$($(dirname $(realpath $0))/build.sh \
     --WORKSPACE="${WORKSPACE}" \
-    --PYTORCH_REPO="${PYTORCH_REPO}" \
     --PYTORCH_VERSION="${PYTORCH_VERSION}" \
-    --TORCH_XPU_OPS_REPO="${TORCH_XPU_OPS_REPO}" \
     --TORCH_XPU_OPS_VERSION="${TORCH_XPU_OPS_VERSION}" \
-    > ${GITHUB_WORKSPACE}/gs-logs/build-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1
-if [ $? -ne 0 ];then
+    > ${GITHUB_WORKSPACE}/gs-logs/build-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1 && echo $? || echo $?)"
+if [ ${build_status} -ne 0 ];then
     tail -n 100 ${GITHUB_WORKSPACE}/gs-logs/build-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log
     echo "Build got failed"
     exit 1
@@ -37,19 +33,21 @@ pip list |grep torch
 test_result=1
 if [ "${SEARCH_CHECK}" == "accuracy" ];then
     cd ${WORKSPACE}/pytorch
-    eval "${SEARCH_CASE} --output=${WORKSPACE}/tmp.csv" \
-        > ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1
-    if [ $? -eq 0 ];then
+    test_status="$(eval "${SEARCH_CASE} --output=${WORKSPACE}/tmp.csv" \
+        > ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1 && echo $? || echo $?)"
+    if [ ${test_status} -eq 0 ];then
         acc_result=$(tail -n 1 ${WORKSPACE}/tmp.csv |awk -F, '{print $4}')
         if [[ "${acc_result}" == "pass"* ]];then
             test_result=0
         fi
+    else
+        tail -n 100 ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log
     fi
 elif [ "${SEARCH_CHECK}" == "performance" ];then
     cd ${WORKSPACE}/pytorch
-    eval "${SEARCH_CASE} --output=${WORKSPACE}/tmp.csv" \
-        > ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1
-    if [ $? -eq 0 ];then
+    test_status="$(eval "${SEARCH_CASE} --output=${WORKSPACE}/tmp.csv" \
+        > ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1 && echo $? || echo $?)"
+    if [ ${test_status} -eq 0 ];then
         perf_result=$(tail -n 1 ${WORKSPACE}/tmp.csv |awk -F, '{print $5}')
         test_result=$(echo "${perf_result},${SEARCH_GOOD_VALUE},${SEARCH_CRITERIA}" |awk -F, '{
             if ($1/$2 > (1 - $3)){
@@ -58,29 +56,43 @@ elif [ "${SEARCH_CHECK}" == "performance" ];then
                 print "1";
             }
         }')
+    else
+        tail -n 100 ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log
     fi
 elif [ "${SEARCH_CHECK}" == "ut_regressions" ];then
     cd ${WORKSPACE}/pytorch/third_party/torch-xpu-ops/test/regressions
-    eval "${SEARCH_CASE}" > ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1
-    if [ $? -eq 0 ];then
+    test_status="$(eval "${SEARCH_CASE} \
+        > ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1 && echo $? || echo $?)"
+    if [ ${test_status} -eq 0 ];then
         test_result=0
+    else
+        tail -n 100 ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log
     fi
 elif [ "${SEARCH_CHECK}" == "ut_extended" ];then
     cd ${WORKSPACE}/pytorch/third_party/torch-xpu-ops/test/xpu/extended
-    eval "${SEARCH_CASE}" > ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1
-    if [ $? -eq 0 ];then
+    test_status="$(eval "${SEARCH_CASE} \
+        > ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1 && echo $? || echo $?)"
+    if [ ${test_status} -eq 0 ];then
         test_result=0
+    else
+        tail -n 100 ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log
     fi
 elif [ "${SEARCH_CHECK}" == "ut_xpu" ];then
     cd ${WORKSPACE}/pytorch/third_party/torch-xpu-ops/test/xpu
-    eval "${SEARCH_CASE}" > ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1
-    if [ $? -eq 0 ];then
+    test_status="$(eval "${SEARCH_CASE} \
+        > ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1 && echo $? || echo $?)"
+    if [ ${test_status} -eq 0 ];then
         test_result=0
+    else
+        tail -n 100 ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log
     fi
 else
-    eval "${SEARCH_CASE}" > ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1
-    if [ $? -eq 0 ];then
+    test_status="$(eval "${SEARCH_CASE} \
+        > ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log 2>&1 && echo $? || echo $?)"
+    if [ ${test_status} -eq 0 ];then
         test_result=0
+    else
+        tail -n 100 ${GITHUB_WORKSPACE}/gs-logs/test-${PYTORCH_VERSION}-${TORCH_XPU_OPS_VERSION}.log
     fi
 fi
 
