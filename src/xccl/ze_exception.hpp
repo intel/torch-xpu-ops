6a7057b32333908442fc181d6c56602a7751782e
Han, Chao1 <chao1.han@intel.com>
2025-07-31 15:48:49 +0800

    format

--- src/xccl/ze_exception.hpp
+++ src/xccl/ze_exception.hpp
@@ -2,29 +2,42 @@
 
 #include <level_zero/ze_api.h>
 #include <exception>
-#include <unordered_map>
 #include <iostream>
+#include <unordered_map>
 
 // Mapping from status to human readable string
 class zeException : std::exception {
-  const char * zeResultToString(ze_result_t status) const {
-    static const std::unordered_map<ze_result_t, const char *> zeResultToStringMap{
-      {ZE_RESULT_SUCCESS, "[Core] success"},
-      {ZE_RESULT_NOT_READY, "[Core] synchronization primitive not signaled"},
-      {ZE_RESULT_ERROR_DEVICE_LOST, "[Core] device hung, reset, was removed, or driver update occurred"},
-      {ZE_RESULT_ERROR_OUT_OF_HOST_MEMORY, "[Core] insufficient host memory to satisfy call"},
-      {ZE_RESULT_ERROR_OUT_OF_DEVICE_MEMORY, "[Core] insufficient device memory to satisfy call"},
-      {ZE_RESULT_ERROR_MODULE_BUILD_FAILURE, "[Core] error occurred when building module, see build log for details"},
-      {ZE_RESULT_ERROR_UNINITIALIZED, "[Validation] driver is not initialized"},
-      {ZE_RESULT_ERROR_INVALID_NULL_POINTER, "[Validation] pointer argument may not be nullptr"},
-      {ZE_RESULT_ERROR_HANDLE_OBJECT_IN_USE, "[Validation] object pointed to by handle still in-use by device"},
-      {ZE_RESULT_ERROR_INVALID_ENUMERATION, "[Validation] enumerator argument is not valid"},
-      {ZE_RESULT_ERROR_INVALID_SIZE, "[Validation] size argument is invalid"},
-      {ZE_RESULT_ERROR_UNSUPPORTED_SIZE, "[Validation] size argument is not supported by the device"},
-      {ZE_RESULT_ERROR_UNSUPPORTED_ALIGNMENT, "[Validation] alignment argument is not supported by the device"},
-      {ZE_RESULT_ERROR_INVALID_NULL_HANDLE, "[Validation] handle argument is not valid"},
-      {ZE_RESULT_ERROR_UNSUPPORTED_FEATURE, "[Validation] generic error code for unsupported features"},
-      {ZE_RESULT_ERROR_INVALID_NATIVE_BINARY, "[Validation] native binary is not supported by the device"},
+  const char* zeResultToString(ze_result_t status) const {
+    static const std::unordered_map<ze_result_t, const char*> zeResultToStringMap{
+        {ZE_RESULT_SUCCESS, "[Core] success"},
+        {ZE_RESULT_NOT_READY, "[Core] synchronization primitive not signaled"},
+        {ZE_RESULT_ERROR_DEVICE_LOST,
+         "[Core] device hung, reset, was removed, or driver update occurred"},
+        {ZE_RESULT_ERROR_OUT_OF_HOST_MEMORY,
+         "[Core] insufficient host memory to satisfy call"},
+        {ZE_RESULT_ERROR_OUT_OF_DEVICE_MEMORY,
+         "[Core] insufficient device memory to satisfy call"},
+        {ZE_RESULT_ERROR_MODULE_BUILD_FAILURE,
+         "[Core] error occurred when building module, see build log for details"},
+        {ZE_RESULT_ERROR_UNINITIALIZED,
+         "[Validation] driver is not initialized"},
+        {ZE_RESULT_ERROR_INVALID_NULL_POINTER,
+         "[Validation] pointer argument may not be nullptr"},
+        {ZE_RESULT_ERROR_HANDLE_OBJECT_IN_USE,
+         "[Validation] object pointed to by handle still in-use by device"},
+        {ZE_RESULT_ERROR_INVALID_ENUMERATION,
+         "[Validation] enumerator argument is not valid"},
+        {ZE_RESULT_ERROR_INVALID_SIZE, "[Validation] size argument is invalid"},
+        {ZE_RESULT_ERROR_UNSUPPORTED_SIZE,
+         "[Validation] size argument is not supported by the device"},
+        {ZE_RESULT_ERROR_UNSUPPORTED_ALIGNMENT,
+         "[Validation] alignment argument is not supported by the device"},
+        {ZE_RESULT_ERROR_INVALID_NULL_HANDLE,
+         "[Validation] handle argument is not valid"},
+        {ZE_RESULT_ERROR_UNSUPPORTED_FEATURE,
+         "[Validation] generic error code for unsupported features"},
+        {ZE_RESULT_ERROR_INVALID_NATIVE_BINARY,
+         "[Validation] native binary is not supported by the device"},
     };
     auto it = zeResultToStringMap.find(status);
     if (it != zeResultToStringMap.end())
@@ -33,7 +46,7 @@ class zeException : std::exception {
       return "Unknown Reason";
   }
 
-public:
+ public:
   zeException(ze_result_t ret) : result_(ret) {}
 
   ze_result_t result_;
@@ -43,9 +56,9 @@ public:
   }
 };
 
-#define zeCheck(x)             \
-  if (x != ZE_RESULT_SUCCESS)  {    \
-    auto e = zeException(x);  \
-    std::cout<<"Throw "<<e.what()<<std::endl; \
-    throw e;      \
+#define zeCheck(x)                                  \
+  if (x != ZE_RESULT_SUCCESS) {                     \
+    auto e = zeException(x);                        \
+    std::cout << "Throw " << e.what() << std::endl; \
+    throw e;                                        \
   }
