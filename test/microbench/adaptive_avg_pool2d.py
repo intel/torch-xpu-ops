cc882400ba8ab1dab78585a7d63e5e21a536379b
Zhong, Ruijie <ruijie.zhong@intel.com>
2025-08-07 03:01:37 -0700

    align the lint check

--- test/microbench/adaptive_avg_pool2d.py
+++ test/microbench/adaptive_avg_pool2d.py
@@ -6,8 +6,8 @@
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 
-import time
 import argparse
+import time
 
 import torch
 from torch.profiler import profile, ProfilerActivity
@@ -54,16 +54,19 @@ def Adaptive_AVGPool2d(shape, dtype, channels_last, backward, device):
     if backward:
         output[0].backward(grad)
 
+
 def run_profile(shape, dtype, channels_last, backward, device, num_iter):
     with profile(
-        activities=[ProfilerActivity.CPU,
-                  ProfilerActivity.XPU if device == "xpu" else ProfilerActivity.CUDA,
+        activities=[
+            ProfilerActivity.CPU,
+            ProfilerActivity.XPU if device == "xpu" else ProfilerActivity.CUDA,
         ],
         record_shapes=True,
     ) as prof:
         for i in range(num_iter):
             Adaptive_AVGPool2d(shape, dtype, channels_last, backward, device)
     print(prof.key_averages().table(sort_by=f"{device}_time_total"))
+
 
 def run_e2e(shape, dtype, channels_last, backward, device, num_iter):
     if device in ["xpu", "cuda"]:
@@ -76,6 +79,7 @@ def run_e2e(shape, dtype, channels_last, backward, device, num_iter):
     t2 = time.time()
     e2e_time = (t2 - t1) / num_iter
     print("E2E total time:", f"{float(e2e_time):.20f}")
+
 
 def benchmark(args):
     for shape in shape_list:
@@ -117,13 +121,14 @@ def benchmark(args):
                         args.num_iter,
                     )
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="OP Benchmark")
     parser.add_argument(
         "--device",
         type=str,
-        default='xpu',
-        help='Device to run on (e.g., "cpu", "cuda", "xpu")'
+        default="xpu",
+        help='Device to run on (e.g., "cpu", "cuda", "xpu")',
     )
     group = parser.add_mutually_exclusive_group()
     group.add_argument(
@@ -132,6 +137,7 @@ def parse_args():
     group.add_argument("--e2e-only", action="store_true", help="Only Run E2E timing")
     parser.add_argument("--num-iter", type=int, default=20, help="Number of iterations")
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     args = parse_args()
