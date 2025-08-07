490abb65d2dc6b6e79dc6caae7a2199ad5de1291
Zhong, Ruijie <ruijie.zhong@intel.com>
2025-08-07 03:43:34 -0700

    align the lint check

--- test/microbench/grid_sampler.grid_sampler_2d.py
+++ test/microbench/grid_sampler.grid_sampler_2d.py
@@ -1,5 +1,6 @@
-import time
 import argparse
+import time
+
 import torch
 from torch.profiler import profile, ProfilerActivity
 
@@ -9,6 +10,7 @@ shape_list = [
     (16, 128, 512, 512),
 ]
 backward = True
+
 
 def Grad_sample2d(input, grid, backward, mode, padding_mode, align_corners, device):
     output = torch.nn.functional.grid_sample(
@@ -21,27 +23,36 @@ def Grad_sample2d(input, grid, backward, mode, padding_mode, align_corners, devi
     if backward:
         output.sum().backward()
 
-def run_profile(input, grid, backward, mode, padding_mode, align_corners, device, num_iter):
+
+def run_profile(
+    input, grid, backward, mode, padding_mode, align_corners, device, num_iter
+):
     with profile(
-        activities=[ProfilerActivity.CPU,
-                  ProfilerActivity.XPU if device == 'xpu' else ProfilerActivity.CUDA],
+        activities=[
+            ProfilerActivity.CPU,
+            ProfilerActivity.XPU if device == "xpu" else ProfilerActivity.CUDA,
+        ],
         record_shapes=True,
     ) as prof:
         for i in range(num_iter):
-            Grad_sample2d(input, grid, backward, mode, padding_mode, align_corners, device)
-    print(prof.key_averages().table(sort_by="{}_time_total".format(device)))
+            Grad_sample2d(
+                input, grid, backward, mode, padding_mode, align_corners, device
+            )
+    print(prof.key_averages().table(sort_by=f"{device}_time_total"))
+
 
 def run_e2e(input, grid, backward, mode, padding_mode, align_corners, device, num_iter):
-    if device in ['xpu', 'cuda']:
-        torch.xpu.synchronize() if device == 'xpu' else torch.cuda.synchronize()
+    if device in ["xpu", "cuda"]:
+        torch.xpu.synchronize() if device == "xpu" else torch.cuda.synchronize()
     t1 = time.time()
     for i in range(num_iter):
         Grad_sample2d(input, grid, backward, mode, padding_mode, align_corners, device)
-    if device in ['xpu', 'cuda']:
-        torch.xpu.synchronize() if device == 'xpu' else torch.cuda.synchronize()
+    if device in ["xpu", "cuda"]:
+        torch.xpu.synchronize() if device == "xpu" else torch.cuda.synchronize()
     t2 = time.time()
     e2e_time = (t2 - t1) / num_iter
     print("E2E total time:", f"{float(e2e_time):.20f}")
+
 
 def benchmark(args):
     for shape in shape_list:
@@ -58,7 +69,15 @@ def benchmark(args):
                             grid.requires_grad_(True)
 
                         # warm up
-                        Grad_sample2d(input, grid, backward, mode, padding_mode, align_corners, args.device)
+                        Grad_sample2d(
+                            input,
+                            grid,
+                            backward,
+                            mode,
+                            padding_mode,
+                            align_corners,
+                            args.device,
+                        )
 
                         # go
                         print(
@@ -76,23 +95,46 @@ def benchmark(args):
                             backward,
                         )
                         if not args.e2e_only:
-                            run_profile(input, grid, backward, mode, padding_mode, align_corners, args.device, args.num_iter)
+                            run_profile(
+                                input,
+                                grid,
+                                backward,
+                                mode,
+                                padding_mode,
+                                align_corners,
+                                args.device,
+                                args.num_iter,
+                            )
 
                         if not args.profile_only:
-                            run_e2e(input, grid, backward, mode, padding_mode, align_corners, args.device, args.num_iter)
+                            run_e2e(
+                                input,
+                                grid,
+                                backward,
+                                mode,
+                                padding_mode,
+                                align_corners,
+                                args.device,
+                                args.num_iter,
+                            )
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='OP Benchmark')
-    parser.add_argument('--device', type=str, default='xpu',
-                        help='Device to run on (e.g., "cpu", "cuda", "xpu")')
+    parser = argparse.ArgumentParser(description="OP Benchmark")
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="xpu",
+        help='Device to run on (e.g., "cpu", "cuda", "xpu")',
+    )
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('--profile-only', action='store_true',
-                       help='Only Run profile timing')
-    group.add_argument('--e2e-only', action='store_true',
-                       help='Only Run E2E timing')
-    parser.add_argument('--num-iter', type=int, default=20,
-                        help='Number of iterations')
+    group.add_argument(
+        "--profile-only", action="store_true", help="Only Run profile timing"
+    )
+    group.add_argument("--e2e-only", action="store_true", help="Only Run E2E timing")
+    parser.add_argument("--num-iter", type=int, default=20, help="Number of iterations")
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     args = parse_args()
