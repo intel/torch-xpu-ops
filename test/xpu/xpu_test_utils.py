c17d557261f62e1be8c2d730df958b75437fd1b1
mengfei25 <mengfei.li@intel.com>
2025-08-23 11:46:50 +0800

    lint

--- test/xpu/xpu_test_utils.py
+++ test/xpu/xpu_test_utils.py
@@ -1170,8 +1170,7 @@ def launch_test(test_case, skip_list=None, exe_list=None):
             skip_options += skip_option
         skip_options += '"'
         test_command = (
-            f"pytest --junit-xml=./op_ut_with_skip_{test_case}.xml "
-            + test_case
+            f"pytest --junit-xml=./op_ut_with_skip_{test_case}.xml " + test_case
         )
         test_command += skip_options
     elif exe_list is not None:
@@ -1181,13 +1180,11 @@ def launch_test(test_case, skip_list=None, exe_list=None):
             exe_options += exe_option
         exe_options += '"'
         test_command = (
-            f"pytest --junit-xml=./op_ut_with_skip_{test_case}.xml "
-            + test_case
+            f"pytest --junit-xml=./op_ut_with_skip_{test_case}.xml " + test_case
         )
         test_command += exe_options
     else:
         test_command = (
-            f"pytest --junit-xml=./op_ut_with_skip_{test_case}.xml "
-            + test_case
+            f"pytest --junit-xml=./op_ut_with_skip_{test_case}.xml " + test_case
         )
     return os.system(test_command)
