c4311f83825022acff83c39a332e486f03689f3e
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2025-08-29 21:34:20 +0800

    Update skip_list_dist_local.py

--- test/xpu/skip_list_dist_local.py
+++ test/xpu/skip_list_dist_local.py
@@ -375,7 +375,7 @@ skip_dict = {
 }
 
 skip_dict_python = {
-    "distributed/test_c10d_ops_xccl.py": None,
+    #"distributed/test_c10d_ops_xccl.py": None,
     "distributed/test_c10d_xccl.py": None,
     "../../../../test/distributed/test_c10d_functional_native.py": None,
     # "../../../../test/distributed/pipelining/test_schedule_multiproc.py": None,  # Hang error.
