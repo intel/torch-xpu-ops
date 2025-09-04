e06c88fddaa4d6ca08594f7dade42893b2225e3d
Zhong, Ruijie <ruijie.zhong@intel.com>
2025-09-04 00:00:21 -0700

    update result check

--- .github/scripts/ut_result_check.sh
+++ .github/scripts/ut_result_check.sh
@@ -115,7 +115,7 @@ check_test_cases() {
 }
 
 
-if [[ "${ut_suite}" == 'op_regression' || "${ut_suite}" == 'op_regression_dev1' || "${ut_suite}" == 'op_extended' || "${ut_suite}" == 'op_transformers' || "${ut_suite}" == 'op_ut' ]]; then
+if [[ "${ut_suite}" == 'op_regression' || "${ut_suite}" == 'op_regression_dev1' || "${ut_suite}" == 'op_extended' || "${ut_suite}" == 'op_transformers' || "${ut_suite}" == 'op_ut' || "${ut_suite}" == 'torch_xpu' ]]; then
     echo -e "========================================================================="
     echo -e "Show Failed cases in ${ut_suite}"
     echo -e "========================================================================="
@@ -188,35 +188,6 @@ if [[ "${ut_suite}" == 'op_regression' || "${ut_suite}" == 'op_regression_dev1' 
     fi
 fi
 
-if [[ "${ut_suite}" == 'torch_xpu' ]]; then
-    echo "Pytorch XPU binary UT checking"
-    cd ../../pytorch || exit
-    for xpu_case in build/bin/*{xpu,sycl}*; do
-      if [[ "$xpu_case" != *"*"* && "$xpu_case" != *.so && "$xpu_case" != *.a ]]; then
-        case_name=$(basename "$xpu_case")
-        cd ../ut_log/torch_xpu || exit
-        grep -E "FAILED" binary_ut_"${ut_suite}"_"${case_name}"_test.log | awk '{print $2}' > ./binary_ut_"${ut_suite}"_"${case_name}"_failed.log
-        wc -l < "./binary_ut_${ut_suite}_${case_name}_failed.log" | tee -a ./binary_ut_"${ut_suite}"_failed_summary.log
-        grep -E "PASSED|Pass" binary_ut_"${ut_suite}"_"${case_name}"_test.log | awk '{print $2}' > ./binary_ut_"${ut_suite}"_"${case_name}"_passed.log
-        wc -l < "./binary_ut_${ut_suite}_${case_name}_passed.log" | tee -a ./binary_ut_"${ut_suite}"_passed_summary.log
-        cd - || exit
-      fi
-    done
-    echo -e "========================================================================="
-    echo -e "Show Failed cases in ${ut_suite}"
-    echo -e "========================================================================="
-    cd ../ut_log/torch_xpu || exit
-    cat "./binary_ut_${ut_suite}_${case_name}_failed.log"
-    num_failed_binary_ut=$(awk '{sum += $1};END {print sum}' binary_ut_"${ut_suite}"_failed_summary.log)
-    num_passed_binary_ut=$(awk '{sum += $1};END {print sum}' binary_ut_"${ut_suite}"_passed_summary.log)
-    ((num_failed=num_failed_binary_ut))
-    if [[ $num_failed -gt 0 ]] || [[ $num_passed_binary_ut -le 0 ]]; then
-      echo -e "[ERROR] UT ${ut_suite} test Fail"
-      exit 1
-    else
-      echo -e "[PASS] UT ${ut_suite} test Pass"
-    fi
-fi
 if [[ "${ut_suite}" == 'xpu_distributed' ]]; then
     grep -E "^FAILED" xpu_distributed_test.log | awk '{print $2}' > ./"${ut_suite}"_xpu_distributed_test_failed.log
     grep "PASSED" xpu_distributed_test.log | awk '{print $1}' > ./"${ut_suite}"_xpu_distributed_test_passed.log
