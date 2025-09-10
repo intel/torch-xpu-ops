8f8cb3a5664daf1e92a20fa0ab3cf46f3de2a3a1
Han Chao <chao1.han@intel.com>
2025-09-10 11:29:12 +0800

    refine

--- src/xccl/XPUSymmetricMemory.cpp
+++ src/xccl/XPUSymmetricMemory.cpp
@@ -153,7 +153,42 @@ at::Tensor XPUSymmetricMemory::get_signal_pad(
     c10::IntArrayRef sizes,
     std::optional<c10::ScalarType> dtype,
     int64_t storage_offset) {
-    LOG(ERROR) << "XPUSymmetricMemory::put_signal not supported";
+  // If the dtype is unspecified, default it to UInt32, as it
+  // is the most common type for signaling purposes.
+  if (!dtype.has_value()) {
+    dtype = c10::ScalarType::UInt32;
+  }
+
+  // If the shape is unspecified, treat the signal pad as a 1d tensor.
+  const auto element_size = c10::elementSize(*dtype);
+  std::vector<int64_t> shape;
+  if (sizes.size() != 0) {
+    shape = sizes.vec();
+  } else {
+    shape.push_back(signal_pad_size / element_size);
+  }
+
+  const size_t numel = std::accumulate(
+      shape.begin(),
+      shape.end(),
+      static_cast<size_t>(1),
+      std::multiplies<size_t>());
+  const auto req_size = (numel + storage_offset) * element_size;
+  TORCH_CHECK(
+      req_size <= signal_pad_size,
+      "XPUSymmetricMemory::get_signal_pad: the requested size (",
+      req_size,
+      " bytes) exceeds the allocated size (",
+      signal_pad_size,
+      " bytes)");
+  auto data_ptr = reinterpret_cast<uint8_t*>(signal_pads_[rank]) +
+      storage_offset * element_size;
+  auto device = c10::Device(c10::DeviceType::XPU, local_device_idx_);
+  auto options = at::TensorOptions().dtype(*dtype).device(device);
+  return at::for_blob(data_ptr, shape)
+      .options(options)
+      .target_device(device)
+      .make_tensor();
 }
 
 void check_channel(int channel, int world_size) {
@@ -176,7 +211,8 @@ void check_channel(int channel, int world_size) {
 void XPUSymmetricMemory::barrier(int channel, size_t timeout_ms) {
   check_channel(channel, world_size_);
 
-  // Currently, we leverage oneCCL for barrier. Later, we may move to SYCL implementation.
+  // Currently, we leverage oneCCL for barrier. Later, we may move to SYCL
+  // implementation.
   auto group = c10d::resolve_process_group(group_name_);
   if (group == nullptr) {
     TORCH_WARN(
@@ -367,8 +403,7 @@ c10::intrusive_ptr<SymmetricMemory> XPUSymmetricMemoryAllocator::rendezvous(
       .block_size = block->block_size,
       .buffer_size = block->buffer_size,
       .signal_pad_offset = block->signal_pad_offset,
-      .has_multicast_support = false
-      };
+      .has_multicast_support = false};
   auto reqs = storeExchange.all_gather(store, rank, world_size, local_req);
   validate_rendezvous_requests(reqs, world_size);
 
@@ -378,9 +413,10 @@ c10::intrusive_ptr<SymmetricMemory> XPUSymmetricMemoryAllocator::rendezvous(
   }
 
   // do IPC exchange for all peer ranks
-  ipc_channel.exchange_peer_ipc_mem(current_queue, ptr);
+  ipc_channel.exchange_peer_ipc_mem(current_queue, ptr, rank, world_size);
 
-  // no physical memory handle, so handles and buffers are both for virtual address
+  // no physical memory handle, so handles and buffers are both for virtual
+  // address
   std::vector<HandleType> handles(world_size);
   std::vector<void*> buffers(world_size, nullptr);
   std::vector<void*> signal_pads(world_size, nullptr);
