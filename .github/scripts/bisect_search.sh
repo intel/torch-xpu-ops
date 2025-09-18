0fee2a62640836b4aa9ecdde7f26effd13122104
mengfei25 <mengfei.li@intel.com>
2025-09-18 16:44:30 +0800

    debug

--- .github/scripts/bisect_search.sh
+++ .github/scripts/bisect_search.sh
@@ -45,8 +45,8 @@ git clone https://github.com/pytorch/vision gs-vision
 git clone https://github.com/pytorch/audio gs-audio
 cd gs-vision && git checkout ${TORCHVISION_COMMIT_ID} && python setup.py install && cd ..
 cd gs-audio && git checkout ${TORCHAUDIO_COMMIT_ID} && python setup.py install && cd ..
-cd .. && pip install git+https://github.com/intel/intel-xpu-backend-for-triton@{TRITON_COMMIT_ID} || \
-                pip install git+https://github.com/intel/intel-xpu-backend-for-triton@{TRITON_COMMIT_ID}#subdirectory=python
+cd .. && pip install git+https://github.com/intel/intel-xpu-backend-for-triton@${TRITON_COMMIT_ID} || \
+                pip install git+https://github.com/intel/intel-xpu-backend-for-triton@${TRITON_COMMIT_ID}#subdirectory=python
 
 # Test
 test_result=1
