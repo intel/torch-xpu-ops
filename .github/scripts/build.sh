fed7b5046eb023f576411938adc7034c6bd958ac
mengfei25 <mengfei.li@intel.com>
2025-09-19 13:59:19 +0800

    debug

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -47,7 +47,7 @@ python -m pip install requests
 python third_party/torch-xpu-ops/.github/scripts/apply_torch_pr.py
 git submodule sync && git submodule update --init --recursive
 python -m pip install -r requirements.txt
-python -m pip install mkl-static==2025.2.0 mkl-include==2025.2.0
+python -m pip install mkl-static==${XPU_MKL_VERSION:-"2025.2.0"} mkl-include==${XPU_MKL_VERSION:-"2025.2.0"}
 export USE_STATIC_MKL=1
 if [ "${XPU_ONEAPI_PATH}" == "" ];then
     export PYTORCH_EXTRA_INSTALL_REQUIREMENTS=" \
