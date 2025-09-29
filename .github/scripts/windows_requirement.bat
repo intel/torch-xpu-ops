1259cfba970ae4f1865d6a4e67da1b4cf82fa9de
Zhong, Ruijie <ruijie.zhong@intel.com>
2025-09-29 02:12:30 -0700

    fix the requirement issue

--- .github/scripts/windows_requirement.bat
+++ .github/scripts/windows_requirement.bat
@@ -1,23 +1,27 @@
 @echo off
-set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=intel-cmplr-lib-rt==2025.2.1 ^^^
-^^^| intel-cmplr-lib-ur==2025.2.1 ^^^
-^^^| intel-cmplr-lic-rt==2025.2.1 ^^^
-^^^| intel-sycl-rt==2025.2.1 ^^^
-^^^| oneccl-devel==2021.16.1; platform_system == 'Linux' and platform_machine == 'x86_64' ^^^
-^^^| oneccl==2021.16.1; platform_system == 'Linux' and platform_machine == 'x86_64' ^^^
-^^^| impi-rt==2021.16.1; platform_system == 'Linux' and platform_machine == 'x86_64' ^^^
-^^^| onemkl-sycl-blas==2025.2.0 ^^^
-^^^| onemkl-sycl-dft==2025.2.0 ^^^
-^^^| onemkl-sycl-lapack==2025.2.0 ^^^
-^^^| onemkl-sycl-rng==2025.2.0 ^^^
-^^^| onemkl-sycl-sparse==2025.2.0 ^^^
-^^^| dpcpp-cpp-rt==2025.2.1 ^^^
-^^^| intel-opencl-rt==2025.2.1 ^^^
-^^^| mkl==2025.2.0 ^^^
-^^^| intel-openmp==2025.2.1 ^^^
-^^^| tbb==2022.2.0 ^^^
-^^^| tcmlib==1.4.0 ^^^
-^^^| umf==0.11.0 ^^^
-^^^| intel-pti==0.13.1"
+setlocal EnableDelayedExpansion
 
-echo %PYTORCH_EXTRA_INSTALL_REQUIREMENTS%
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS="
+
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!intel-cmplr-lib-rt==2025.2.1"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;intel-cmplr-lib-ur==2025.2.1"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;intel-cmplr-lic-rt==2025.2.1"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;intel-sycl-rt==2025.2.1"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;oneccl-devel==2021.16.1; platform_system == 'Linux' and platform_machine == 'x86_64'"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;oneccl==2021.16.1; platform_system == 'Linux' and platform_machine == 'x86_64'"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;impi-rt==2021.16.1; platform_system == 'Linux' and platform_machine == 'x86_64'"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;onemkl-sycl-blas==2025.2.0"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;onemkl-sycl-dft==2025.2.0"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;onemkl-sycl-lapack==2025.2.0"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;onemkl-sycl-rng==2025.2.0"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;onemkl-sycl-sparse==2025.2.0"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;dpcpp-cpp-rt==2025.2.1"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;intel-opencl-rt==2025.2.1"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;mkl==2025.2.0"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;intel-openmp==2025.2.1"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;tbb==2022.2.0"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;tcmlib==1.4.0"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;umf==0.11.0"
+set "PYTORCH_EXTRA_INSTALL_REQUIREMENTS=!PYTORCH_EXTRA_INSTALL_REQUIREMENTS!;intel-pti==0.13.1"
+
+echo !PYTORCH_EXTRA_INSTALL_REQUIREMENTS!
