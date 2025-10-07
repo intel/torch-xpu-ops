953b49df6bbbbf896f80f192f39e835dcf08895a
leizhenyuan <zhenyuan.lei@intel.com>
2025-10-07 02:25:08 +0000

    refactor for index by free function

--- src/ATen/native/xpu/sycl/BatchKernel.h
+++ src/ATen/native/xpu/sycl/BatchKernel.h
@@ -69,6 +69,51 @@ class BatchKernelConfig {
     return cfg;
   }
 
+  // For SYCL free function
+  template <auto* kptr>
+  static BatchKernelConfig make_config(
+      int64_t batch,
+      int64_t problem,
+      int64_t stride,
+      int64_t problem_batch,
+      bool problem_along_x,
+      Policy policy = Policy::pSegment,
+      int64_t prefer_wg_size = 0) {
+    BatchKernelConfig cfg = {
+        batch,
+        problem,
+        stride,
+        problem_batch,
+        problem_along_x,
+        policy,
+        prefer_wg_size};
+    cfg.template build<kptr>();
+
+    return cfg;
+  }
+
+  template <auto* kptr>
+  static BatchKernelConfig make_config(
+      int64_t batch,
+      int64_t problem,
+      int64_t stride,
+      int64_t problem_batch,
+      bool problem_along_x,
+      std::vector<Policy> policies,
+      int64_t prefer_wg_size = 0) {
+    BatchKernelConfig cfg = {
+        batch,
+        problem,
+        stride,
+        problem_batch,
+        problem_along_x,
+        policies,
+        prefer_wg_size};
+    cfg.template build<kptr>();
+
+    return cfg;
+  }
+
   sycl::range<2> global_size() const {
     return {glb_range_y_, glb_range_x_};
   }
@@ -189,6 +234,109 @@ class BatchKernelConfig {
         glb_range_y_(0),
         wg_range_x_(0),
         wg_range_y_(0) {}
+
+  template <auto* kptr>
+  void build() {
+    size_t wg_size;
+    size_t sg_size = syclMaxSubGroupSize();
+    // Caller takes responsibility of if work group size is valid or compatible.
+    if (prefer_wg_size_ != 0 && prefer_wg_size_ % sg_size == 0 &&
+        prefer_wg_size_ <= syclDeviceMaxWorkGroupSize()) {
+      wg_size = prefer_wg_size_;
+    } else {
+      wg_size = syclMaxWorkGroupSize<kptr>();
+    }
+    wg_range_x_ = sg_size;
+    wg_range_y_ = wg_size / wg_range_x_;
+
+    size_t limit_x =
+        (uint8_t)policy_ & (uint8_t)Policy::pAggressiveSplit ? 1 : sg_size;
+
+    if (problem_batch_ == 0)
+      problem_batch_ = batch_ * stride_;
+
+    // ensure assigning successive work items along contiguous (small stride)
+    // dimension
+    auto range_bound_x = problem_along_x_ ? problem_ : problem_batch_;
+    auto range_bound_y = problem_along_x_ ? problem_batch_ : problem_;
+
+    // Implications,
+    // 1. assign proper x/y to accommodate workload exactly.
+    // 2. prefer enough x (at least limit_x) to access memory coalecsingly.
+    // Spare y for x if workload is not large along y.
+    wg_range_y_ = std::min<size_t>(
+        wg_range_y_, c10::llvm::PowerOf2Ceil((uint64_t)range_bound_y));
+    // Subscribe appropriate x at least limit_x.
+    wg_range_x_ = std::max<size_t>(
+        std::min<size_t>(
+            wg_size / wg_range_y_,
+            c10::llvm::PowerOf2Ceil((uint64_t)range_bound_x)),
+        limit_x);
+    // Retieve y if necessary, if x is not large.
+    wg_range_y_ = std::min<size_t>(
+        wg_size / wg_range_x_,
+        c10::llvm::PowerOf2Ceil((uint64_t)range_bound_y));
+
+    if ((uint8_t)policy_ & (uint8_t)Policy::pAdaptive) {
+      size_t target_glb_range = syclMaxWorkItemsPerTile() /
+          (wg_range_x_ * wg_range_y_) * (wg_range_x_ * wg_range_y_);
+      if (problem_along_x_) {
+        glb_range_y_ = wg_range_y_;
+
+        glb_range_x_ = target_glb_range / glb_range_y_;
+        if (glb_range_x_ > range_bound_x) {
+          auto wg_num_x = std::max<size_t>(range_bound_x / wg_range_x_, 1);
+          glb_range_x_ = wg_range_x_ * wg_num_x;
+        }
+
+        glb_range_y_ = target_glb_range / glb_range_x_;
+        if (glb_range_y_ > range_bound_y) {
+          auto wg_num_y = std::max<size_t>(range_bound_y / wg_range_y_, 1);
+          glb_range_y_ = wg_range_y_ * wg_num_y;
+        }
+      } else {
+        glb_range_x_ = wg_range_x_;
+
+        glb_range_y_ = target_glb_range / glb_range_x_;
+        if (glb_range_y_ > range_bound_y) {
+          auto wg_num_y = std::max<size_t>(range_bound_y / wg_range_y_, 1);
+          glb_range_y_ = wg_range_y_ * wg_num_y;
+        }
+
+        glb_range_x_ = target_glb_range / glb_range_y_;
+        if (glb_range_x_ > range_bound_x) {
+          auto wg_num_x = std::max<size_t>(range_bound_x / wg_range_x_, 1);
+          glb_range_x_ = wg_range_x_ * wg_num_x;
+        }
+      }
+    } else {
+      if (problem_along_x_) {
+        glb_range_x_ = (uint8_t)policy_ & (uint8_t)Policy::pLoop
+            ? wg_range_x_
+            : size_t((problem_ + wg_range_x_ - 1) / wg_range_x_) * wg_range_x_;
+        glb_range_y_ =
+            size_t((problem_batch_ + wg_range_y_ - 1) / wg_range_y_) *
+            wg_range_y_;
+      } else {
+        glb_range_x_ =
+            size_t((problem_batch_ + wg_range_x_ - 1) / wg_range_x_) *
+            wg_range_x_;
+        glb_range_y_ = (uint8_t)policy_ & (uint8_t)Policy::pLoop
+            ? wg_range_y_
+            : size_t((problem_ + wg_range_y_ - 1) / wg_range_y_) * wg_range_y_;
+      }
+    }
+
+    problem_wg_range_ = problem_along_x_ ? wg_range_x_ : wg_range_y_;
+    problem_glb_range_ = problem_along_x_ ? glb_range_x_ : glb_range_y_;
+    batch_glb_range_ = problem_along_x_ ? glb_range_y_ : glb_range_x_;
+    problem_range_ = problem_along_x_
+        ? (problem_ + glb_range_x_ - 1) / glb_range_x_ * glb_range_x_
+        : (problem_ + glb_range_y_ - 1) / glb_range_y_ * glb_range_y_;
+    batch_range_ = problem_along_x_
+        ? (problem_batch_ + glb_range_y_ - 1) / glb_range_y_ * glb_range_y_
+        : (problem_batch_ + glb_range_x_ - 1) / glb_range_x_ * glb_range_x_;
+  }
 
   template <class KernelClass>
   void build() {
