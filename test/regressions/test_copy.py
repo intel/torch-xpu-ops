c5251926777ddfcb58935434df2fbd77b447513b
Cui, Yifeng <yifeng.cui@intel.com>
2025-10-30 23:48:27 -0700

    Fix format and clean code

--- test/regressions/test_copy.py
+++ test/regressions/test_copy.py
@@ -1,15 +1,11 @@
 # Owner(s): ["module: intel"]
 import torch
-from torch.testing._internal.common_device_type import dtypes
 from torch.testing._internal.common_device_type import (
     dtypes,
     instantiate_device_type_tests,
 )
 from torch.testing._internal.common_dtype import float8_types_and
-from torch.testing._internal.common_utils import (
-    run_tests,
-    TestCase,
-)
+from torch.testing._internal.common_utils import run_tests, TestCase
 
 cpu_device = torch.device("cpu")
 xpu_device = torch.device("xpu")
@@ -31,9 +27,8 @@ class TestSimpleCopy(TestCase):
         b_xpu = a_xpu.clone(memory_format=torch.channels_last)
         self.assertEqual(b_cpu, b_xpu.to(cpu_device))
 
-instantiate_device_type_tests(
-    TestSimpleCopy, globals(), only_for="xpu", allow_xpu=True
-)
+
+instantiate_device_type_tests(TestSimpleCopy, globals(), only_for="xpu", allow_xpu=True)
 
 
 if __name__ == "__main__":
