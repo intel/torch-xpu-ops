24b3d2abd6581c9b472e5c8aeb55daa4ec957f9c
chunhuanMeng <105194461+chunhuanMeng@users.noreply.github.com>
2025-11-12 13:39:48 +0800

    fix lint error

--- src/ATen/native/xpu/DilatedMaxPool3d.cpp
+++ src/ATen/native/xpu/DilatedMaxPool3d.cpp
@@ -17,6 +17,7 @@ std::tuple<Tensor, Tensor> max_pool3d_with_indices_xpu(
     bool ceil_mode) {
   Tensor output = at::empty({0}, input.options());
   Tensor indices = at::empty({0}, input.options().dtype(kLong));
+
   NoNamesGuard guard;
   at::native::xpu::max_pool3d_with_indices_kernel(
       input,
