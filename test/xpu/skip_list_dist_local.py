5fddc503e01827fa0201d9475ead91956594a248
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2025-11-26 10:11:17 +0800

    Update skip_list_dist_local.py

--- test/xpu/skip_list_dist_local.py
+++ test/xpu/skip_list_dist_local.py
@@ -1,4 +1,6 @@
 skip_dict = {
+    "distributed/test_c10d_ops_xccl.py": None,
+    "distributed/test_c10d_xccl.py": None,
     "../../../../test/distributed/fsdp/test_checkpoint_wrapper.py": None,
     "../../../../test/distributed/fsdp/test_distributed_checkpoint.py": None,
     "../../../../test/distributed/fsdp/test_fsdp_apply.py": None,
@@ -162,8 +164,6 @@ skip_dict = {
     "../../../../test/distributed/optim/test_apply_optimizer_in_backward.py": None,
     "../../../../test/distributed/optim/test_named_optimizer.py": None,
     "../../../../test/distributed/optim/test_zero_redundancy_optimizer.py": None,
-    "distributed/test_c10d_ops_xccl.py": None,
-    "distributed/test_c10d_xccl.py": None,
     "../../../../test/distributed/test_c10d_functional_native.py": None,
     # "../../../../test/distributed/pipelining/test_stage.py": None,
     "../../../../test/distributed/pipelining/test_transformer.py": None,
