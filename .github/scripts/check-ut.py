5ac7bfb7c5448ceb08cef4af0ea306454fa36db5
Zhong, Ruijie <ruijie.zhong@intel.com>
2025-12-03 00:41:29 -0800

    update ut check

--- .github/scripts/check-ut.py
+++ .github/scripts/check-ut.py
@@ -131,6 +131,9 @@ def print_failures(failure_list=None):
     if not failures:
         return
 
+    if args.output_dir != '.':
+        return
+
     print("### Test Failures")
     print_header = True
     for case in failures:
@@ -373,6 +376,9 @@ def generate_category_totals_log():
             log_file.write(f"Errors: {totals['Errors']}\n")
 
 def print_summary():
+    if args.output_dir != '.':
+        return
+
     print("### Results Summary")
     print_header = True
 
@@ -428,7 +434,9 @@ def main():
     generate_passed_log()
     generate_all_cases_log()
     generate_category_totals_log()
-    print_summary()
+
+    if args.output_dir == '.':
+        print_summary()
 
 
 if __name__ == "__main__":
