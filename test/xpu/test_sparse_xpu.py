6e59b10b5a11a37b2f4632e6ff2cc6daa2b15665
jenniew <jenniewang123@gmail.com>
2025-12-11 13:48:57 -0800

    update

--- test/xpu/test_sparse_xpu.py
+++ test/xpu/test_sparse_xpu.py
@@ -2223,7 +2223,7 @@ class TestSparse(TestSparseBase):
     @expectedFailureMPS
     @dtypes(torch.double)
     @dtypesIfMPS(torch.float32)
-    @skipXPUIf(True, "https://github.com/intel/torch-xpu-ops/issues/2211")
+    @skipXPUIf(False, "https://github.com/intel/torch-xpu-ops/issues/2211")
     def test_hsmm(self, device, dtype, coalesced):
         def test_shape(di, dj, dk, nnz):
             x = self._gen_sparse(2, nnz, [di, dj], dtype, device, coalesced)[0]
