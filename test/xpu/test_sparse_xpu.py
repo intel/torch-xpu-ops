4d6757568f59a11fd92796d506c25623fd7936ca
jenniew <jenniewang123@gmail.com>
2025-12-12 16:58:00 -0800

    clean

--- test/xpu/test_sparse_xpu.py
+++ test/xpu/test_sparse_xpu.py
@@ -2095,12 +2095,8 @@ class TestSparse(TestSparseBase):
             S = self._gen_sparse(2, nnz, [n, m], dtype, device, coalesced)[0]
             S_dense = S.to_dense().requires_grad_(True)
             S.requires_grad_(True)
-            print(f"D1: {D1}")
-            print(f"D1.shape: {D1.shape}")
             Y = torch.sparse.addmm(D1, S, D2, beta=beta, alpha=alpha)
-            print(f"Y: {Y}")
             Y_dense = torch.addmm(D1, S_dense, D2, beta=beta, alpha=alpha)
-            print(f"Y_dense: {Y_dense}")
             self.assertEqual(Y, Y_dense)
 
             if dtype not in {torch.double, torch.cdouble}:
@@ -2109,7 +2105,6 @@ class TestSparse(TestSparseBase):
 
             def fn(S, D1, D2, beta=beta, alpha=alpha):
                 return torch.sparse.addmm(D1, S, D2, beta=beta, alpha=alpha)
-                # return torch.addmm(D1, S, D2, beta=beta, alpha=alpha)
 
             gradcheck(fn, (S, D1, D2), masked=True)
 
@@ -2223,15 +2218,13 @@ class TestSparse(TestSparseBase):
     @expectedFailureMPS
     @dtypes(torch.double)
     @dtypesIfMPS(torch.float32)
-    @skipXPUIf(False, "https://github.com/intel/torch-xpu-ops/issues/2211")
+    @skipXPUIf(True, "https://github.com/intel/torch-xpu-ops/issues/2211")
     def test_hsmm(self, device, dtype, coalesced):
         def test_shape(di, dj, dk, nnz):
             x = self._gen_sparse(2, nnz, [di, dj], dtype, device, coalesced)[0]
             y = self.randn(dj, dk, dtype=dtype, device=device)
             res = torch.hsmm(x, y)
             expected = torch.mm(self.safeToDense(x), y)
-            print(f"expected: {expected}")
-            print(f"res.to_dense(): {res.to_dense()}")
             self.assertEqual(res.to_dense(), expected)
 
         test_shape(7, 5, 3, 20)
@@ -3368,7 +3361,7 @@ class TestSparse(TestSparseBase):
     @expectedFailureMPS
     @dtypes(torch.double)
     @dtypesIfMPS(torch.float32)
-    @skipXPUIf(False, "https://github.com/intel/torch-xpu-ops/issues/2211")
+    @skipXPUIf(True, "https://github.com/intel/torch-xpu-ops/issues/2211")
     def test_mv(self, device, dtype, coalesced):
         def test_shape(di, dj, dk, nnz):
             x, _, _ = self._gen_sparse(2, nnz, [di, dj], dtype, device, coalesced)
