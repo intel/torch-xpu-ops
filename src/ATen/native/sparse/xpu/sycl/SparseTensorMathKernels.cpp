daded1407568c42325eabef1501080c89ae1b0b2
jenniew <jenniewang123@gmail.com>
2025-12-15 11:28:39 -0800

    update

--- src/ATen/native/sparse/xpu/sycl/SparseTensorMathKernels.cpp
+++ src/ATen/native/sparse/xpu/sycl/SparseTensorMathKernels.cpp
@@ -193,6 +193,20 @@ struct SparseElementwiseKernelScalarFunctor {
 
 } // namespace apply
 
+// Check if every tensor in a list of tensors matches the current
+// device.
+inline bool check_device(ArrayRef<Tensor> ts) {
+  if (ts.empty()) {
+    return true;
+  }
+  Device curDevice = Device(kXPU, c10::xpu::current_device());
+  for (const Tensor& t : ts) {
+    if (t.device() != curDevice)
+      return false;
+  }
+  return true;
+}
+
 Tensor& add_out_dense_sparse_kernel(
     Tensor& r_,
     const Tensor& dense,
