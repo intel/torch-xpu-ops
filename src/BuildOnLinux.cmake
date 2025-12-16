32980be62c428a20f241c0206eb4252331cf44df
Slawomir Siwek <slawomir.siwek@intel.com>
2025-12-16 15:20:01 +0100

    Leave default flags

--- src/BuildOnLinux.cmake
+++ src/BuildOnLinux.cmake
@@ -71,11 +71,6 @@ endif()
 if(USE_SYCLTLA)
   set(REPLACE_FLAGS_FOR_SYCLTLA TRUE)
   set_build_flags()
-  
-  set(CMAKE_C_FLAGS_BK "${CMAKE_C_FLAGS}")
-  set(CMAKE_CXX_FLAGS_BK "${CMAKE_CXX_FLAGS}")
-  string(APPEND CMAKE_C_FLAGS " -Werror=format -Werror=unused-variable")
-  string(APPEND CMAKE_CXX_FLAGS " -Werror=format -Werror=unused-variable")
 
   foreach(sycl_src ${ATen_XPU_SYCLTLA_SRCS})
     get_filename_component(name ${sycl_src} NAME_WLE REALPATH)
@@ -97,9 +92,6 @@ if(USE_SYCLTLA)
 
   set(REPLACE_FLAGS_FOR_SYCLTLA FALSE)
   set_build_flags()
-  
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAG_BK}")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_BK}")
 endif()
 
 set(SYCL_LINK_LIBRARIES_KEYWORD)
