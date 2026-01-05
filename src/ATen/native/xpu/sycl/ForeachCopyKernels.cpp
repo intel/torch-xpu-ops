f45b55030dd20c11919d92ee8280bce6e41d9f7e
Pawel Swider <pawel.swider@intel.com>
2026-01-05 10:08:55 +0000

    Performance improvements

--- src/ATen/native/xpu/sycl/ForeachCopyKernels.cpp
+++ src/ATen/native/xpu/sycl/ForeachCopyKernels.cpp
@@ -1,9 +1,5 @@
-#include <ATen/Dispatch.h>
-#include <comm/xpu_aten.h>
-
 #include <ATen/native/xpu/sycl/ForeachFunctors.h>
 #include <ATen/native/xpu/sycl/MultiTensorApply.h>
-
 #include <ATen/native/xpu/sycl/ForeachCopyKernels.h>
 
 namespace at::native::xpu {
@@ -48,46 +44,49 @@ struct CopyFunctor {
     const auto item_idx = item_id.get_local_id(0);
     const auto item_range = item_id.get_local_range(0);
     const auto group_idx = item_id.get_group(0);
-    const int tensor_loc = tlWGMeta[group_idx].wg_to_tensor;
-    const int chunk_idx = tlWGMeta[group_idx].wg_to_chunk;
-    const int64_t n =
-        tlAddress[tensor_loc].numel_to_tensor - chunk_idx * chunk_size;
+    const size_t tensor_loc = tlWGMeta[group_idx].wg_to_tensor;
+    const size_t chunk_idx = tlWGMeta[group_idx].wg_to_chunk;
+    const size_t chunk_offset = chunk_idx * chunk_size;
+    const size_t n =
+        tlAddress[tensor_loc].numel_to_tensor - chunk_offset;
 
     src_t* src_ptr = static_cast<src_t*>(tlAddress[tensor_loc].addresses[0]);
-    src_ptr += chunk_idx * chunk_size;
+    src_ptr += chunk_offset;
     dst_t* dst_ptr = static_cast<dst_t*>(tlAddress[tensor_loc].addresses[1]);
-    dst_ptr += chunk_idx * chunk_size;
+    dst_ptr += chunk_offset;
 
     const bool all_aligned = is_aligned(src_ptr) && is_aligned(dst_ptr);
 
-    src_t src_args[kILP];
-    dst_t r_args[kILP];
+    static constexpr size_t kILP2 = 256;
 
+    src_t src_args[kILP2];
+    dst_t r_args[kILP2];
+    
     // vec path
-    if (n % kILP == 0 && chunk_size % kILP == 0 && all_aligned) {
-      for (int64_t i = item_idx; i * kILP < n && i * kILP < chunk_size;
+    if (n % kILP2 == 0 && chunk_size % kILP2 == 0 && all_aligned) {
+      for (size_t i = item_idx; i * kILP2 < n && i * kILP2 < chunk_size;
            i += item_range) {
         load_store(src_args, src_ptr, 0, i);
 #pragma unroll
-        for (int64_t ii = 0; ii < kILP; ++ii) {
-          r_args[ii] = static_cast<dst_t>(op(src_args[ii]));
+        for (size_t ii = 0; ii < kILP2; ++ii) {
+          r_args[ii] = op(src_args[ii]);
         }
         load_store(dst_ptr, r_args, i, 0);
       }
       // non-vec path
     } else {
-      for (int64_t i = 0; i < n && i < chunk_size; i += item_range * kILP) {
+      for (size_t i = 0; i < n && i < chunk_size; i += item_range * kILP2) {
 #pragma unroll
-        for (int64_t ii = 0; ii < kILP; ++ii) {
-          const int64_t i_start = i + item_idx + ii * item_range;
+        for (size_t ii = 0; ii < kILP2; ++ii) {
+          const size_t i_start = i + item_idx + ii * item_range;
           src_args[ii] = src_t{};
           if (i_start < n && i_start < chunk_size) {
             src_args[ii] = src_ptr[i_start];
           }
         }
 #pragma unroll
-        for (int64_t ii = 0; ii < kILP; ++ii) {
-          r_args[ii] = static_cast<dst_t>(op(src_args[ii]));
+        for (size_t ii = 0; ii < kILP2; ++ii) {
+          r_args[ii] = op(src_args[ii]);
         }
         store_args(dst_ptr, r_args, i, chunk_size, n, item_idx, item_range);
       }
@@ -99,7 +98,6 @@ struct CopyFunctor {
 
 void foreach_copy_list_kernel_(TensorList self, TensorList src) {
   std::vector<std::vector<at::Tensor>> tensor_lists{src.vec(), self.vec()};
-
   AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(
       at::ScalarType::Half,
       at::ScalarType::BFloat16,
@@ -108,7 +106,6 @@ void foreach_copy_list_kernel_(TensorList self, TensorList src) {
       "foreach_tensor_copy",
       [&]() {
         using dst_t = scalar_t;
-        using dst_opmath_t = at::opmath_type<dst_t>;
         AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(
             at::ScalarType::Half,
             at::ScalarType::BFloat16,
@@ -117,7 +114,6 @@ void foreach_copy_list_kernel_(TensorList self, TensorList src) {
             "foreach_tensor_copy",
             [&]() {
               using src_t = scalar_t;
-              using src_opmath_t = at::opmath_type<src_t>;
               multi_tensor_apply<2>(
                   tensor_lists,
                   CopyFunctor<
@@ -126,7 +122,7 @@ void foreach_copy_list_kernel_(TensorList self, TensorList src) {
                       /* depth */ 2,
                       /* r_args_depth */ 1,
                       /* res_arg_index */ 1>(),
-                  Copy<dst_opmath_t, src_opmath_t>());
+                  Copy<dst_t, src_t>());
             });
       });
 }
