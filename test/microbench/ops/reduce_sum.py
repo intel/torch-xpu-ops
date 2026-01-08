362dd357c935e7b8bfe1c211196d288cd3bd9130
Zhong, Ruijie <ruijie.zhong@intel.com>
2026-01-07 21:19:23 -0800

    add more cases

---
+++ test/microbench/ops/reduce_sum.py
@@ -0,0 +1,52 @@
+# Copyright 2020-2025 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+import torch
+from core.runner import normalize_dtype
+
+
+def run_op(config, device):
+    """config keys:
+    - shape: list, e.g., [8192, 8192]
+    - dim: int (reduction axis)
+    - datatype: torch.dtype
+    - backward: bool
+    """
+    shape = config["shape"]
+    dim = config["dim"]
+    dtype = normalize_dtype(config.get("datatype", torch.float32))
+    backward = config.get("backward", False)
+
+    input = torch.randn(shape, device=device, dtype=dtype)
+    if dim is None:
+        output = torch.sum(input)
+    else:
+        output = torch.sum(input, dim)
+
+
+def get_default_cases():
+    base_shapes = [
+        [8192, 8192],
+    ]
+    dtypes = [torch.bfloat16, torch.float16, torch.float32]
+    # dim = None: reduce all
+    # dim = 0: reduce along strided dim
+    # dim = 1: reduce along contiguous dim
+    dims = [None, 0, 1]
+
+    cases = []
+    for shape in base_shapes:
+        for dtype in dtypes:
+            for dim in dims:
+                cases.append({
+                    "shape": shape,
+                    "datatype": dtype,
+                    "dim": dim,
+                    "backward": False,
+                })
+    return cases
