308d5389f001ff99f35381d8c80feace5a6091ac
Erxin Shang <erxin.shang@intel.com>
2026-02-06 12:02:04 +0800

    Add missing requires_cuda_and_triton due to merge issue

--- test/xpu/test_regional_inductor.py
+++ test/xpu/test_regional_inductor.py
@@ -29,7 +29,7 @@ from torch.testing._internal.common_utils import (
     parametrize,
     skipIfTorchDynamo,
 )
-from torch.testing._internal.triton_utils import requires_gpu_and_triton
+from torch.testing._internal.triton_utils import requires_gpu_and_triton, requires_cuda_and_triton
 
 device_type = acc.type if (acc := torch.accelerator.current_accelerator()) else "cpu"
 
