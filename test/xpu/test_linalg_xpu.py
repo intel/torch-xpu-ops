97edd9d5112340f8e2c036b34ab4894a59a1b09d
Wojciech Piętka <wojciechx.pietka@intel.com>
2026-02-25 12:03:37 +0000

    Fix linter issues

--- test/xpu/test_linalg_xpu.py
+++ test/xpu/test_linalg_xpu.py
@@ -360,9 +360,7 @@ def _int4_mm(self, device, m, k, n):
     for dtype in [torch.bfloat16] + (
         [torch.float16, torch.float32]
         if device == "cpu"
-        else [torch.float16]
-        if "xpu" in device
-        else []
+        else [torch.float16] if "xpu" in device else []
     ):
         a = a_bf16.to(dtype=dtype)
         b = b_bf16.to(dtype=dtype)
@@ -512,6 +510,7 @@ def __tunableop_ctx(self):
             except KeyError:
                 pass
 
+
 @dtypes(*floating_and_complex_types())
 def matrix_rank_out_errors_and_warnings(self, device, dtype):
     # dtypes should be safely castable
@@ -527,7 +526,9 @@ def matrix_rank_out_errors_and_warnings(self, device, dtype):
         torch.linalg.matrix_rank(a, out=out)
         # Expect resize warning, and possibly an XPU-to-CPU fallback warning
         self.assertTrue(len(w) in (1, 2))
-        self.assertTrue("An output with one or more elements was resized" in str(w[0].message))
+        self.assertTrue(
+            "An output with one or more elements was resized" in str(w[0].message)
+        )
         if len(w) == 2:
             self.assertTrue("Aten Op fallback from XPU to CPU" in str(w[1].message))
 
@@ -710,7 +711,9 @@ TestLinalg.test_pinv_errors_and_warnings = pinv_errors_and_warnings
 TestLinalg.test_rotating_buffer_tunableop = rotating_buffer_tunableop
 TestLinalg.test_cond_errors_and_warnings = cond_errors_and_warnings
 TestLinalg._tunableop_ctx = __tunableop_ctx
-TestLinalg.test_matrix_rank_out_errors_and_warnings = matrix_rank_out_errors_and_warnings
+TestLinalg.test_matrix_rank_out_errors_and_warnings = (
+    matrix_rank_out_errors_and_warnings
+)
 
 TestLinalg._default_dtype_check_enabled = True
 instantiate_device_type_tests(TestLinalg, globals(), only_for=("xpu"), allow_xpu=True)
