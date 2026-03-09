3e0232c88b51935deec675d04ee01af2df62a303
Jakub Kosno <jakubx.kosno@intel.com>
2026-03-09 12:05:52 +0000

    Fix dtype discrepancy in FFT (#2615)

--- src/ATen/native/xpu/SpectralOps.cpp
+++ src/ATen/native/xpu/SpectralOps.cpp
@@ -19,20 +19,48 @@
 
 namespace at::native {
 
+#if !defined(USE_ONEMKL_XPU)
+namespace {
+
+Tensor promote_fft_input_for_cpu_mkl(const Tensor& input) {
+  if (input.scalar_type() == ScalarType::Half) {
+    return input.to(ScalarType::Float);
+  }
+  if (input.scalar_type() == ScalarType::ComplexHalf) {
+    return input.to(ScalarType::ComplexFloat);
+  }
+  return input;
+}
+
+} // namespace
+#endif
+
 Tensor _fft_c2c_xpu(
     const Tensor& self,
     IntArrayRef dim,
     int64_t normalization,
     bool forward) {
   TORCH_CHECK(self.is_complex());
+  const auto expected_dtype = self.scalar_type();
+
+  Tensor result;
 
 #if defined(USE_ONEMKL_XPU)
-  return native::xpu::_fft_c2c_mkl(self, dim, normalization, forward);
+  result = native::xpu::_fft_c2c_mkl(self, dim, normalization, forward);
 #else
+  Tensor promoted_self = promote_fft_input_for_cpu_mkl(self);
   Tensor out_cpu = native::_fft_c2c_mkl(
-      self.to(Device(at::kCPU)), dim, normalization, forward);
-  return out_cpu.to(Device(at::kXPU));
+      promoted_self.to(Device(at::kCPU)), dim, normalization, forward);
+  if (self.scalar_type() == ScalarType::ComplexHalf) {
+    out_cpu = out_cpu.to(ScalarType::ComplexHalf);
+  }
+  result = out_cpu.to(Device(at::kXPU));
 #endif // USE_ONEMKL_XPU
+
+  if (result.scalar_type() != expected_dtype) {
+    result = result.to(expected_dtype);
+  }
+  return result;
 }
 
 Tensor& _fft_c2c_xpu_out(
@@ -46,8 +74,12 @@ Tensor& _fft_c2c_xpu_out(
 #if defined(USE_ONEMKL_XPU)
   return native::xpu::_fft_c2c_mkl_out(self, dim, normalization, forward, out);
 #else
+  Tensor promoted_self = promote_fft_input_for_cpu_mkl(self);
   Tensor out_cpu = native::_fft_c2c_mkl(
-      self.to(Device(at::kCPU)), dim, normalization, forward);
+      promoted_self.to(Device(at::kCPU)), dim, normalization, forward);
+  if (self.scalar_type() == ScalarType::ComplexHalf) {
+    out_cpu = out_cpu.to(ScalarType::ComplexHalf);
+  }
   at::native::resize_output(out, out_cpu.sizes());
   out.copy_(out_cpu);
   return out;
@@ -60,14 +92,26 @@ Tensor _fft_c2r_xpu(
     int64_t normalization,
     int64_t last_dim_size) {
   TORCH_CHECK(self.is_complex());
+  const auto expected_dtype = c10::toRealValueType(self.scalar_type());
+
+  Tensor result;
 
 #if defined(USE_ONEMKL_XPU)
-  return native::xpu::_fft_c2r_mkl(self, dim, normalization, last_dim_size);
+  result = native::xpu::_fft_c2r_mkl(self, dim, normalization, last_dim_size);
 #else
+  Tensor promoted_self = promote_fft_input_for_cpu_mkl(self);
   Tensor out_cpu = native::_fft_c2r_mkl(
-      self.to(Device(at::kCPU)), dim, normalization, last_dim_size);
-  return out_cpu.to(Device(at::kXPU));
+      promoted_self.to(Device(at::kCPU)), dim, normalization, last_dim_size);
+  if (self.scalar_type() == ScalarType::ComplexHalf) {
+    out_cpu = out_cpu.to(ScalarType::Half);
+  }
+  result = out_cpu.to(Device(at::kXPU));
 #endif // USE_ONEMKL_XPU
+
+  if (result.scalar_type() != expected_dtype) {
+    result = result.to(expected_dtype);
+  }
+  return result;
 }
 
 Tensor& _fft_c2r_xpu_out(
@@ -82,8 +126,12 @@ Tensor& _fft_c2r_xpu_out(
   return native::xpu::_fft_c2r_mkl_out(
       self, dim, normalization, last_dim_size, out);
 #else
+  Tensor promoted_self = promote_fft_input_for_cpu_mkl(self);
   Tensor out_cpu = native::_fft_c2r_mkl(
-      self.to(Device(at::kCPU)), dim, normalization, last_dim_size);
+      promoted_self.to(Device(at::kCPU)), dim, normalization, last_dim_size);
+  if (self.scalar_type() == ScalarType::ComplexHalf) {
+    out_cpu = out_cpu.to(ScalarType::Half);
+  }
   at::native::resize_output(out, out_cpu.sizes());
   out.copy_(out_cpu);
   return out;
@@ -96,14 +144,26 @@ Tensor _fft_r2c_xpu(
     int64_t normalization,
     bool onesided) {
   TORCH_CHECK(self.is_floating_point());
+  const auto expected_dtype = c10::toComplexType(self.scalar_type());
+
+  Tensor result;
 
 #if defined(USE_ONEMKL_XPU)
-  return native::xpu::_fft_r2c_mkl(self, dim, normalization, onesided);
+  result = native::xpu::_fft_r2c_mkl(self, dim, normalization, onesided);
 #else
+  Tensor promoted_self = promote_fft_input_for_cpu_mkl(self);
   Tensor out_cpu = native::_fft_r2c_mkl(
-      self.to(Device(at::kCPU)), dim, normalization, onesided);
-  return out_cpu.to(Device(at::kXPU));
+      promoted_self.to(Device(at::kCPU)), dim, normalization, onesided);
+  if (self.scalar_type() == ScalarType::Half) {
+    out_cpu = out_cpu.to(ScalarType::ComplexHalf);
+  }
+  result = out_cpu.to(Device(at::kXPU));
 #endif // USE_ONEMKL_XPU
+
+  if (result.scalar_type() != expected_dtype) {
+    result = result.to(expected_dtype);
+  }
+  return result;
 }
 
 Tensor& _fft_r2c_xpu_out(
@@ -117,8 +177,12 @@ Tensor& _fft_r2c_xpu_out(
 #if defined(USE_ONEMKL_XPU)
   return native::xpu::_fft_r2c_mkl_out(self, dim, normalization, onesided, out);
 #else
+  Tensor promoted_self = promote_fft_input_for_cpu_mkl(self);
   Tensor out_cpu = native::_fft_r2c_mkl(
-      self.to(Device(at::kCPU)), dim, normalization, onesided);
+      promoted_self.to(Device(at::kCPU)), dim, normalization, onesided);
+  if (self.scalar_type() == ScalarType::Half) {
+    out_cpu = out_cpu.to(ScalarType::ComplexHalf);
+  }
   at::native::resize_output(out, out_cpu.sizes());
   out.copy_(out_cpu);
   return out;
