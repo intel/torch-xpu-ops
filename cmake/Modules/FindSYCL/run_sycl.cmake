7d03af58a5aef3f0f01dfd954757bc8851e8d100
chunhuanMeng <chunhuan.meng@intel.com>
2026-03-18 06:31:44 +0000

    Remove dependency on intlc

--- cmake/Modules/FindSYCL/run_sycl.cmake
+++ cmake/Modules/FindSYCL/run_sycl.cmake
@@ -74,6 +74,8 @@ foreach(flag ${CMAKE_HOST_FLAGS})
   endif()
 endforeach()
 
+list(APPEND SYCL_HOST_FLAGS -no-intel-lib=libirc)
+
 # SYCL_execute_process - Executes a command with optional command echo and status message.
 #
 #   status  - Status message to print if verbose is true
