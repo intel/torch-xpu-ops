5ab2a420f3cfaa47302c612a2a62e288272acf63
chunhuanMeng <chunhuan.meng@intel.com>
2026-03-29 23:51:34 -0700

    Suppress warnings from SYCL TLA flash attention headers
    mha_fwd.h and mha_bwd.h include headers from the SYCL TLA (third-party)
    library that emit warnings under -Werror:
      -Wchanges-meaning, -Wsycl-strict, -Wunused-local-typedefs, -Wunused-variable
    
    Wrap the affected includes with clang/GCC diagnostic push/pop. The
    underlying issues are tracked in the SYCL TLA project and suppressions
    will be removed once upstream fixes land.

--- src/ATen/native/transformers/xpu/flash_attn/sycltla/mha_bwd.h
+++ src/ATen/native/transformers/xpu/flash_attn/sycltla/mha_bwd.h
@@ -9,11 +9,24 @@
  */
 
 #pragma once
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-warning-option"
+#pragma clang diagnostic ignored "-Wchanges-meaning"
+#pragma clang diagnostic ignored "-Wsycl-strict"
+#pragma clang diagnostic ignored "-Wunused-local-typedefs"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wchanges-meaning"
 #include <cute/tensor.hpp>
 #include <cute/util/compat.hpp>
 #include <cutlass/numeric_conversion.h>
 #include <sycl/sycl.hpp>
 #include <sycltla/mha_common.h>
+
+#pragma clang diagnostic pop
+#pragma GCC diagnostic pop
 
 namespace cute {
 template <
@@ -253,10 +266,10 @@ struct Param {
   // write
   float* odo_ptr;
   float* dqaccum_ptr;
-  T* pb_ptr;
   T* dq_ptr;
   T* dk_ptr;
   T* dv_ptr;
+  T* pb_ptr;
 
   // const dimension
   int batch;
