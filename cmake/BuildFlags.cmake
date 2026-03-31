8c9a1c7a1ffef63b2ca671e4421f43613903f8fa
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2026-03-31 16:32:52 -0700

    Enable dpclang sycl compiler
    `dpclang` compiler is an open source version of the closed source Intel
    DPC++ compiler. `dpclang` is being supported by Intel in the https://github.com/intel/llvm
    public repository and got recently packaged into upcoming Ubuntu 26.04 LTS.
    
    This commit enables minimal support of `dpclang` compiler in PyTorch building
    towards fully open source PyTorch stack on Linux. Limitations:
    
    * Kineto, XCCL and oneMKL paths were not enabled and might require additional work
    * SYCL-TLA, oneDNN and Pytorch itself might require additional changes to properly
      handle dpclang compiler as it does not support `__INTEL_LLVM_COMPILER` macro
    
    We will address above limitations in the follow up patches. At the moment
    the following procedure can be used to build PyTorch for XPU with `dpclang`
    compiler on Ubuntu 26.04:
    
    * Do NOT activate oneAPI environment
    * Install open source DPC++ compiler, OpenCL and few other tools with:
    ```
    apt-get install dpclang-6 ocl-icd-opencl-dev pkg-config libze-dev
    ```
    * Build PyTorch as follows:
    
    ```
    XPU_SYCL_COMPILER=dpclang USE_KINETO=0 USE_ONEMKL_XPU=0 \
      python3 setup.py develop
    ```
    
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- cmake/BuildFlags.cmake
+++ cmake/BuildFlags.cmake
@@ -110,8 +110,16 @@ macro(set_build_flags)
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
       set(SYCL_KERNEL_OPTIONS ${SYCL_KERNEL_OPTIONS} -Wno-absolute-value)
       set(SYCL_KERNEL_OPTIONS ${SYCL_KERNEL_OPTIONS} -fno-fast-math)
-      set(SYCL_KERNEL_OPTIONS ${SYCL_KERNEL_OPTIONS} -fma)
-      set(SYCL_KERNEL_OPTIONS ${SYCL_KERNEL_OPTIONS} -no-ftz)
+      # -fma which we used before is an alias used for -ffp-contract=fast for compatibility reasons
+      # with very old version of the ICX compiler. The -ffp-contract=fast is supported by both closed
+      # source and open source DPC++ compiler versions.
+      set(SYCL_KERNEL_OPTIONS ${SYCL_KERNEL_OPTIONS} -ffp-contract=fast)
+      # -no-ftz is supported only by ICX compiler shipped with oneAPI Toolkits. For the
+      # DPCLANG open source compiler that's the default mode and no option is needed.
+      CHECK_SYCL_FLAG("-no-ftz" SUPPORTS_NO_FTZ)
+      if(SUPPORTS_NO_FTZ)
+        set(SYCL_KERNEL_OPTIONS ${SYCL_KERNEL_OPTIONS} -no-ftz)
+      endif()
     endif()
 
     if(CMAKE_BUILD_TYPE MATCHES Debug)
