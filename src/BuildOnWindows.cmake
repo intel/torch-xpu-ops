7492e9d709e3ad578deeb4888584d4b82210357a
chunhuanMeng <105194461+chunhuanMeng@users.noreply.github.com>
2026-04-01 10:58:59 +0800

    Update BuildOnWindows.cmake

--- src/BuildOnWindows.cmake
+++ src/BuildOnWindows.cmake
@@ -11,118 +11,88 @@
 set(TORCH_XPU_OPS_LIBRARIES)
 set(SYCL_LINK_LIBRARIES_KEYWORD PRIVATE)
 
-macro(setup_common_libraries)
-  add_library(
-    torch_xpu_ops
-    STATIC
-    ${ATen_XPU_MKL_SRCS}
-    ${ATen_XPU_NATIVE_CPP_SRCS}
-    ${ATen_XPU_GEN_SRCS})
-  target_compile_definitions(torch_xpu_ops PRIVATE TORCH_XPU_BUILD_MAIN_LIB)
-  target_link_libraries(torch_xpu_ops PUBLIC torch_xpu)
-  target_link_libraries(torch_xpu_ops PUBLIC torch_cpu)
-  target_link_libraries(torch_xpu_ops PUBLIC c10)
-endmacro()
+# On Windows, it is not possible to combine all obj files into one library
+# because the obj files of kernels compiled on Windows are much larger than
+# those on Linux. If they are combined into one, the library size will exceed
+# 4GB, which conflicts with the size limit of a single library on Windows.
+# We will combine the libraries on Windows into one after the compiler is fixed.
+add_library(
+  torch_xpu_ops
+  STATIC
+  ${ATen_XPU_MKL_SRCS}
+  ${ATen_XPU_NATIVE_CPP_SRCS}
+  ${ATen_XPU_GEN_SRCS})
+target_compile_definitions(torch_xpu_ops PRIVATE TORCH_XPU_BUILD_MAIN_LIB)
+# Split SYCL kernels into 2 libraries as categories 1) Common (Unary+Binary+Reduce+Pow+Copy+Activation+Foreach) 2) Others.
+set(ATen_XPU_SYCL_COMMON_SRCS)
+set(ATen_XPU_SYCL_OTHERS_SRCS)
+foreach(sycl_src ${ATen_XPU_SYCL_SRCS})
+  string(REGEX MATCH "Binary" IS_BINARY ${sycl_src})
+  string(REGEX MATCH "Unary" IS_UNARY ${sycl_src})
+  string(REGEX MATCH "Pow" IS_POW ${sycl_src})
+  string(REGEX MATCH "Copy" IS_COPY ${sycl_src})
+  string(REGEX MATCH "Reduce" IS_REDUCE ${sycl_src})
+  string(REGEX MATCH "Activation" IS_ACTIVATION ${sycl_src})
+  string(REGEX MATCH "Foreach" IS_FOREACH ${sycl_src})
+  string(REGEX MATCH "Polynomial" IS_POLY ${sycl_src})
+  string(REGEX MATCH "Norm" IS_NORM ${sycl_src})
+  string(REGEX MATCH "Loss" IS_LOSS ${sycl_src})
+  string(REGEX MATCH "Resize" IS_RESIZE ${sycl_src})
+  string(REGEX MATCH "Distribution" IS_DISTRIBUTION ${sycl_src})
 
-if(BUILD_SEPARATE_OPS)
-  setup_common_libraries()
-  foreach(sycl_src ${ATen_XPU_SYCL_SRCS})
-    get_filename_component(name ${sycl_src} NAME_WLE REALPATH)
-    set(sycl_lib torch-xpu-ops-sycl-${name})
-    sycl_add_library(
-      ${sycl_lib}
-      SHARED
-      SYCL_SOURCES ${sycl_src})
-    target_link_libraries(torch_xpu_ops PUBLIC ${sycl_lib})
-    list(APPEND TORCH_XPU_OPS_LIBRARIES ${sycl_lib})
+  if(NOT IS_FOREACH STREQUAL "")
+    list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
+  elseif(NOT IS_REDUCE STREQUAL "")
+    list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
+  elseif(NOT IS_UNARY STREQUAL "" OR NOT IS_BINARY STREQUAL "")
+    list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
+  elseif(NOT IS_COPY STREQUAL "" OR NOT IS_POW STREQUAL "")
+    list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
+  elseif(NOT IS_ACTIVATION STREQUAL "")
+    list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
+  elseif(NOT IS_NORM STREQUAL "")
+    list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
+  elseif(NOT IS_LOSS STREQUAL "")
+    list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
+  elseif(NOT IS_RESIZE STREQUAL "")
+    list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
+  elseif(NOT IS_DISTRIBUTION STREQUAL "")
+    list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
+  elseif(NOT IS_POLY STREQUAL "")
+    list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
+  else()
+    list(APPEND ATen_XPU_SYCL_OTHERS_SRCS ${sycl_src})
+  endif()
+endforeach()
+# Common kernel lib
+set(sycl_common_lib torch_xpu_ops_sycl_common_kernels)
+sycl_add_library(
+  ${sycl_common_lib}
+  STATIC
+  SYCL_SOURCES ${ATen_XPU_SYCL_COMMON_SRCS})
+target_compile_definitions(${sycl_common_lib} PRIVATE TORCH_XPU_BUILD_MAIN_LIB)
+list(APPEND TORCH_XPU_OPS_LIBRARIES ${sycl_common_lib})
 
-    # Decouple with PyTorch cmake definition.
-    install(TARGETS ${sycl_lib} DESTINATION "${TORCH_INSTALL_LIB_DIR}")
-  endforeach()
-  list(APPEND TORCH_XPU_OPS_LIBRARIES torch_xpu_ops)
-else()
-  # On Windows, it is not possible to combine all obj files into one library
-  # because the obj files of kernels compiled on Windows are much larger than
-  # those on Linux. If they are combined into one, the library size will exceed
-  # 4GB, which conflicts with the size limit of a single library on Windows.
-  # We will combine the libraries on Windows into one after the compiler is fixed.
-  add_library(
-    torch_xpu_ops
-    STATIC
-    ${ATen_XPU_MKL_SRCS}
-    ${ATen_XPU_NATIVE_CPP_SRCS}
-    ${ATen_XPU_GEN_SRCS})
-  target_compile_definitions(torch_xpu_ops PRIVATE TORCH_XPU_BUILD_MAIN_LIB)
- # Split SYCL kernels into 2 libraries as categories 1) Common (Unary+Binary+Reduce+Pow+Copy+Activation+Foreach) 2) Others.
-  set(ATen_XPU_SYCL_COMMON_SRCS)
-  set(ATen_XPU_SYCL_OTHERS_SRCS)
-  foreach(sycl_src ${ATen_XPU_SYCL_SRCS})
-    string(REGEX MATCH "Binary" IS_BINARY ${sycl_src})
-    string(REGEX MATCH "Unary" IS_UNARY ${sycl_src})
-    string(REGEX MATCH "Pow" IS_POW ${sycl_src})
-    string(REGEX MATCH "Copy" IS_COPY ${sycl_src})
-    string(REGEX MATCH "Reduce" IS_REDUCE ${sycl_src})
-    string(REGEX MATCH "Activation" IS_ACTIVATION ${sycl_src})
-    string(REGEX MATCH "Foreach" IS_FOREACH ${sycl_src})
-    string(REGEX MATCH "Polynomial" IS_POLY ${sycl_src})
-    string(REGEX MATCH "Norm" IS_NORM ${sycl_src})
-    string(REGEX MATCH "Loss" IS_LOSS ${sycl_src})
-    string(REGEX MATCH "Resize" IS_RESIZE ${sycl_src})
-    string(REGEX MATCH "Distribution" IS_DISTRIBUTION ${sycl_src})
+# Other kernel lib
+set(sycl_lib torch_xpu_ops_sycl_kernels)
+sycl_add_library(
+  ${sycl_lib}
+  STATIC
+  SYCL_SOURCES ${ATen_XPU_SYCL_OTHERS_SRCS})
+target_compile_definitions(${sycl_lib} PRIVATE TORCH_XPU_BUILD_MAIN_LIB)
+list(APPEND TORCH_XPU_OPS_LIBRARIES ${sycl_lib})
 
-    if(NOT IS_FOREACH STREQUAL "")
-      list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
-    elseif(NOT IS_REDUCE STREQUAL "")
-      list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
-    elseif(NOT IS_UNARY STREQUAL "" OR NOT IS_BINARY STREQUAL "")
-      list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
-    elseif(NOT IS_COPY STREQUAL "" OR NOT IS_POW STREQUAL "")
-      list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
-    elseif(NOT IS_ACTIVATION STREQUAL "")
-      list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
-    elseif(NOT IS_NORM STREQUAL "")
-      list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
-    elseif(NOT IS_LOSS STREQUAL "")
-      list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
-    elseif(NOT IS_RESIZE STREQUAL "")
-      list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
-    elseif(NOT IS_DISTRIBUTION STREQUAL "")
-      list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
-    elseif(NOT IS_POLY STREQUAL "")
-      list(APPEND ATen_XPU_SYCL_COMMON_SRCS ${sycl_src})
-    else()
-      list(APPEND ATen_XPU_SYCL_OTHERS_SRCS ${sycl_src})
-    endif()
-  endforeach()
-  # Common kernel lib
-  set(sycl_common_lib torch_xpu_ops_sycl_common_kernels)
-  sycl_add_library(
+target_link_libraries(torch_xpu_ops
+    PUBLIC
     ${sycl_common_lib}
-    STATIC
-    SYCL_SOURCES ${ATen_XPU_SYCL_COMMON_SRCS})
-  target_compile_definitions(${sycl_common_lib} PRIVATE TORCH_XPU_BUILD_MAIN_LIB)
-  list(APPEND TORCH_XPU_OPS_LIBRARIES ${sycl_common_lib})
-
-  # Other kernel lib
-  set(sycl_lib torch_xpu_ops_sycl_kernels)
-  sycl_add_library(
     ${sycl_lib}
-    STATIC
-    SYCL_SOURCES ${ATen_XPU_SYCL_OTHERS_SRCS})
-  target_compile_definitions(${sycl_lib} PRIVATE TORCH_XPU_BUILD_MAIN_LIB)
-  list(APPEND TORCH_XPU_OPS_LIBRARIES ${sycl_lib})
+)
+target_link_options(torch_xpu_ops PUBLIC
+    "-WHOLEARCHIVE:$<TARGET_FILE:${sycl_common_lib}>"
+    "-WHOLEARCHIVE:$<TARGET_FILE:${sycl_lib}>"
+)
+list(APPEND TORCH_XPU_OPS_LIBRARIES torch_xpu_ops)
 
-  target_link_libraries(torch_xpu_ops
-      PUBLIC
-      ${sycl_common_lib}
-      ${sycl_lib}
-  )
-  target_link_options(torch_xpu_ops PUBLIC
-      "-WHOLEARCHIVE:$<TARGET_FILE:${sycl_common_lib}>"
-      "-WHOLEARCHIVE:$<TARGET_FILE:${sycl_lib}>"
-  )
-  list(APPEND TORCH_XPU_OPS_LIBRARIES torch_xpu_ops)
-endif()
 set(SYCL_LINK_LIBRARIES_KEYWORD)
 
 foreach(lib ${TORCH_XPU_OPS_LIBRARIES})
