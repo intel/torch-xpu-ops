11d006fe83bf0e87f27631ad42ace72b2b157a42
Benedykt Bela <benedykt.bela@intel.com>
2026-04-09 00:00:14 +0200

    Correct wrong test file path in skip_list_common.py (#3277)
    ## Summary
    
    This PR corrects the wrong test file path in `skip_list_common.py`.
    
    Currently there is the:
    ```python
    "test/xpu/dynamo/test_ctx_manager_xpu.py": None,
    ```
    while it should be
    ```python
    "dynamo/test_ctx_manager_xpu.py": None,
    ```
    as the path is relative to `torch-xpu-ops/test/xpu/`, not to
    `torch-xpu-ops/`
    
    I checked the logs from last Nightly / Build-from-source and haven't
    found any launched tests from the `test_ctx_manager_xpu.py` file, so it
    looks like this whole file isn't taken into consideration during tests.
    
    This PR enables running all tests from `test_ctx_manager_xpu.py` file.
    
    Signed-off-by: Benedykt Bela <benedykt.bela@intel.com>

--- test/xpu/skip_list_common.py
+++ test/xpu/skip_list_common.py
@@ -110,7 +110,7 @@ skip_dict = {
     "test_fake_tensor_xpu.py": None,
     "test_matmul_cuda_xpu.py": None,
     "functorch/test_vmap_xpu.py": None,
-    "test/xpu/dynamo/test_ctx_manager_xpu.py": None,
+    "dynamo/test_ctx_manager_xpu.py": None,
     "functorch/test_control_flow_xpu.py": None,
     "profiler/test_memory_profiler.py": None,
     "export/test_hop_xpu.py": None,
