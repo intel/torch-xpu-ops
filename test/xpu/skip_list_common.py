69cd7275d726140bdd8de27eadf20f64087d1a22
gta <gta@DUT6329BMGFRD.fm.intel.com>
2026-04-20 11:26:11 +0000

    Skip test_fake_crossref_backward_amp_nn_functional_bilinear_xpu_float32

--- test/xpu/skip_list_common.py
+++ test/xpu/skip_list_common.py
@@ -121,6 +121,9 @@ skip_dict = {
         # For CUDA it's skipped explicitly in common_methods_invocations.py in upstream. We can skip it here
         "test_out_histc_xpu_float32",
         "test_out_mean_xpu_float32",
+        # FakeTensor mismatch in outputs_alias_inputs for aten.view.default
+        # Known upstream issue: https://github.com/pytorch/pytorch/issues/159150
+        "test_fake_crossref_backward_amp_nn_functional_bilinear_xpu_float32",
     ),
     "test_optim_xpu.py": None,
     "test_reductions_xpu.py": None,
