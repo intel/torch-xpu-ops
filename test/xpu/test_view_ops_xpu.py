d87b2032ac25fea2491836819b17085e16aeb06d
Sławomir Siwek <slawomir.siwek@intel.com>
2026-04-22 06:47:51 +0000

    Remove quantized tensors from test

--- test/xpu/test_view_ops_xpu.py
+++ test/xpu/test_view_ops_xpu.py
@@ -8,6 +8,7 @@
 
 # Owner(s): ["module: intel"]
 
+import torch
 from torch.testing._internal.common_device_type import instantiate_device_type_tests
 from torch.testing._internal.common_utils import run_tests
 
@@ -36,7 +37,121 @@ with XPUPatchForImport(False):
 
         return True
 
+    def _test_ravel_xpu(self, device):
+        def _test_ravel(tensors, size, nc=False):
+            for src in tensors:
+                # Continuous Tensor -> View
+                flat = src.ravel()
+                self.assertEqual(flat.shape, torch.Size([size]))
+                self.assertEqual(src.view(-1), flat)
+                self.assertIs(flat._base, src)
+                self.assertTrue(flat.is_contiguous())
+
+                # Non-continuous Tensor -> Copy
+                if nc:
+                    nc_src = src.t()
+                    nc_flat = nc_src.ravel()
+                    self.assertEqual(nc_flat.shape, torch.Size([size]))
+                    self.assertEqual(nc_src.contiguous().view(-1), nc_flat)
+                    self.assertIsNot(nc_flat._base, src)
+                    self.assertTrue(nc_flat.is_contiguous())
+
+        # Test that flatten returns 1-dim tensor when given a 0-dim tensor
+        zero_dim_tensor = torch.tensor(123, device=device)
+        flat0 = zero_dim_tensor.ravel()
+        one_dim_tensor = torch.tensor([123], device=device)
+        flat1 = zero_dim_tensor.ravel()
+        nc_ones_tensor = torch.ones(10, device=device)[::2]
+        flat2 = nc_ones_tensor.ravel()
+
+        self.assertEqual(zero_dim_tensor.shape, torch.Size([]))
+        self.assertEqual(flat0.shape, torch.Size([1]))
+        self.assertEqual(one_dim_tensor.shape, torch.Size([1]))
+        self.assertEqual(flat1.shape, torch.Size([1]))
+        self.assertEqual(nc_ones_tensor.shape, torch.Size([5]))
+        self.assertEqual(flat2.shape, torch.Size([5]))
+        self.assertEqual(flat0, one_dim_tensor)
+        self.assertEqual(flat0, flat1)
+        self.assertEqual(flat0.shape, flat1.shape)
+        self.assertTrue(flat0.is_contiguous())
+        self.assertTrue(flat1.is_contiguous())
+        self.assertTrue(flat2.is_contiguous())
+
+        # Test both float tensor and quantized tensor
+        tensors = [
+            torch.randn(5, 5, 5, 5, device=device),
+        ]
+        _test_ravel(tensors, 625)
+
+        tensors = [
+            torch.randn(0, 2, 3, device=device),
+            torch.randn(3, 0, 2, device=device),
+        ]
+        _test_ravel(tensors, 0)
+
+        tensors = [
+            torch.randn(5, 5, device=device),
+        ]
+        _test_ravel(tensors, 25, True)
+
+    def _test_flatten_xpu(self, device):
+        # Test that flatten returns 1-dim tensor when given a 0-dim tensor
+        zero_dim_tensor = torch.tensor(123, device=device)
+        flat0 = zero_dim_tensor.flatten()
+        one_dim_tensor = torch.tensor([123], device=device)
+        flat1 = zero_dim_tensor.flatten()
+
+        self.assertEqual(zero_dim_tensor.shape, torch.Size([]))
+        self.assertEqual(flat0.shape, torch.Size([1]))
+        self.assertEqual(one_dim_tensor.shape, torch.Size([1]))
+        self.assertEqual(flat1.shape, torch.Size([1]))
+        self.assertEqual(flat0, one_dim_tensor)
+        self.assertEqual(flat0, flat1)
+        self.assertEqual(flat0.shape, flat1.shape)
+
+        # Test both float tensor and quantized tensor
+        tensors = [torch.randn(5, 5, 5, 5, device=device)]
+        for src in tensors:
+            flat = src.flatten(0, -1)
+            self.assertEqual(flat.shape, torch.Size([625]))
+            self.assertEqual(src.view(-1), flat.view(-1))
+
+            flat = src.flatten(0, 2)
+            self.assertEqual(flat.shape, torch.Size([125, 5]))
+            self.assertEqual(src.view(-1), flat.view(-1))
+
+            flat = src.flatten(0, 1)
+            self.assertEqual(flat.shape, torch.Size([25, 5, 5]))
+            self.assertEqual(src.view(-1), flat.view(-1))
+
+            flat = src.flatten(1, 2)
+            self.assertEqual(flat.shape, torch.Size([5, 25, 5]))
+            self.assertEqual(src.view(-1), flat.view(-1))
+
+            flat = src.flatten(2, 3)
+            self.assertEqual(flat.shape, torch.Size([5, 5, 25]))
+            self.assertEqual(src.view(-1), flat.view(-1))
+
+            flat = src.flatten(-2, -1)
+            self.assertEqual(flat.shape, torch.Size([5, 5, 25]))
+            self.assertEqual(src.view(-1), flat.view(-1))
+
+            flat = src.flatten(2, 2)
+            self.assertEqual(flat, src)
+
+            # out of bounds index
+            with self.assertRaisesRegex(IndexError, "Dimension out of range"):
+                src.flatten(5, 10)
+
+            # invalid start and end
+            with self.assertRaisesRegex(
+                RuntimeError, "start_dim cannot come after end_dim"
+            ):
+                src.flatten(2, 0)
+
     TestViewOps.is_view_of = is_view_of
+    TestOldViewOps.test_flatten = _test_flatten_xpu
+    TestOldViewOps.test_ravel = _test_ravel_xpu
 
 
 instantiate_device_type_tests(
