cb34db6e867037ced881dcb90af1cc8951287050
Konrad Drozd <konrad.drozd@intel.com>
2026-05-22 10:09:28 +0200

    ditto
    Co-authored-by: Slawomir Siwek <slawomir.siwek@intel.com>

--- src/ATen/native/xpu/sycl/UpSampleNearest3dKernels.cpp
+++ src/ATen/native/xpu/sycl/UpSampleNearest3dKernels.cpp
@@ -352,7 +352,7 @@ void upsample_nearest3d_backward_template(
           width_scale,
           index_bw_op);
   auto work_group_size = syclMaxWorkGroupSize(kfn);
-  int64_t work_group_num = at::ceil_div(n, (int64_t)work_group_size);
+  int64_t work_group_num = at::ceil_div(n, work_group_size);
   sycl_kernel_submit(
       work_group_num * work_group_size, work_group_size, queue, kfn);
 }
