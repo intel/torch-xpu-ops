3023933672c1f1b6eb3ee7976e68395a4ac7ada5
Konrad Drozd <konrad.drozd@intel.com>
2026-06-12 12:44:36 +0300

    Remove obsolete exec path

--- test/xpu/test_nn_xpu.py
+++ test/xpu/test_nn_xpu.py
@@ -9612,34 +9612,6 @@ tensor(..., device='meta', size=(1,), requires_grad=True)""",
         ):
             nn.Unflatten(dim=1, unflattened_size=(2, 5, 5.0))
 
-        # Wrong type for unflattened_size (list of lists and list of tuples)
-        for us in ([["C", 2], ["W", 5], ["H", 5]], [("C", 2), ("W", 5), ("H", 5)]):
-            with self.assertRaisesRegex(
-                TypeError,
-                r"unflattened_size must be tuple of ints",
-            ):
-                nn.Unflatten(dim="features", unflattened_size=us)
-
-        # Wrong type for unflattened_size (tuple of lists)
-
-        with self.assertRaisesRegex(
-            TypeError,
-            r"unflattened_size must be tuple of ints, but found element of type list at pos 0",
-        ):
-            nn.Unflatten(
-                dim="features", unflattened_size=(["C", 2], ["W", 5], ["H", 5])
-            )
-
-        # Wrong type for unflattened_size (tuple of dicts)
-
-        with self.assertRaisesRegex(
-            TypeError,
-            r"unflattened_size must be tuple of ints, but found element of type dict at pos 0",
-        ):
-            nn.Unflatten(
-                dim="features", unflattened_size=({"C": 2}, {"W": 5}, {"H": 5})
-            )
-
     def test_layer_norm_grads_with_create_graph_flag(self):
         atol = 1e-5
         rtol = 1e-3
