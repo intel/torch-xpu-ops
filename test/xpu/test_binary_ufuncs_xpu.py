acd77e4cbc5b12dca61bb7a7ae9d621efc07e42f
Slawomir Siwek <slawomir.siwek@intel.com>
2026-06-19 07:08:19 +0200

    Update test/xpu/test_binary_ufuncs_xpu.py
    Co-authored-by: Benedykt Bela <benedykt.bela@intel.com>

--- test/xpu/test_binary_ufuncs_xpu.py
+++ test/xpu/test_binary_ufuncs_xpu.py
@@ -18,7 +18,8 @@ from torch.testing._internal.common_device_type import (
     instantiate_device_type_tests,
 )
 from torch.testing._internal.common_dtype import integral_types
-from torch.testing._internal.common_utils import make_tensor, run_tests
+from torch.testing import make_tensor
+from torch.testing._internal.common_utils import run_tests
 
 try:
     from xpu_test_utils import XPUPatchForImport
