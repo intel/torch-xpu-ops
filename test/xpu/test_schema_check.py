9ec79d98bb455f96dffffd8a7fc6a2e4d060477c
Sławomir Siwek <slawomir.siwek@intel.com>
2026-07-02 14:11:22 +0000

    lint

--- test/xpu/test_schema_check.py
+++ test/xpu/test_schema_check.py
@@ -1,7 +1,7 @@
 # Owner(s): ["oncall: jit"]
 
-from torch.testing._internal.common_utils import run_tests
 from torch.testing._internal.common_device_type import instantiate_device_type_tests
+from torch.testing._internal.common_utils import run_tests
 
 try:
     from xpu_test_utils import XPUPatchForImport
@@ -11,7 +11,11 @@ except Exception:
 with XPUPatchForImport(False):
     from test_schema_check import TestSchemaCheckModeOpInfo
 
-instantiate_device_type_tests(TestSchemaCheckModeOpInfo, globals(), only_for=("xpu"), allow_xpu=True,
+instantiate_device_type_tests(
+    TestSchemaCheckModeOpInfo,
+    globals(),
+    only_for=("xpu"),
+    allow_xpu=True,
 )
 
 if __name__ == "__main__":
