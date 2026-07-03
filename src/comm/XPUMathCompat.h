3a752d4460ac08069176b334338523f3a5b7c893
Yang, Shuxin <shuxin.yang@intel.com>
2026-07-02 19:23:24 -0700

    Implement SYCL-compatible math functions in XPUMathCompat.h

--- src/comm/XPUMathCompat.h
+++ src/comm/XPUMathCompat.h
@@ -18,18 +18,90 @@
 
 namespace c10::xpu::compat {
 
-__MATH_FUNCTIONS_DECL__ float exp(float x) {
-  return ::expf(x);
+__MATH_FUNCTIONS_DECL__ float abs(float x) {
+  return sycl::fabs(x);
 }
-__MATH_FUNCTIONS_DECL__ double exp(double x) {
-  return ::exp(x);
+__MATH_FUNCTIONS_DECL__ double abs(double x) {
+  return sycl::fabs(x);
 }
 
-__MATH_FUNCTIONS_DECL__ float tanh(float x) {
-  return ::tanhf(x);
+__MATH_FUNCTIONS_DECL__ float exp(float x) {
+  return sycl::exp(x);
 }
-__MATH_FUNCTIONS_DECL__ double tanh(double x) {
-  return ::tanh(x);
+__MATH_FUNCTIONS_DECL__ double exp(double x) {
+  return sycl::exp(x);
+}
+
+__MATH_FUNCTIONS_DECL__ float ceil(float x) {
+  return sycl::ceil(x);
+}
+__MATH_FUNCTIONS_DECL__ double ceil(double x) {
+  return sycl::ceil(x);
+}
+
+__MATH_FUNCTIONS_DECL__ float copysign(float x, float y) {
+  return sycl::copysign(x, y);
+}
+__MATH_FUNCTIONS_DECL__ double copysign(double x, double y) {
+  return sycl::copysign(x, y);
+}
+
+__MATH_FUNCTIONS_DECL__ float floor(float x) {
+  return sycl::floor(x);
+}
+__MATH_FUNCTIONS_DECL__ double floor(double x) {
+  return sycl::floor(x);
+}
+
+__MATH_FUNCTIONS_DECL__ float log(float x) {
+  return sycl::log(x);
+}
+__MATH_FUNCTIONS_DECL__ double log(double x) {
+  return sycl::log(x);
+}
+
+__MATH_FUNCTIONS_DECL__ float log1p(float x) {
+  return sycl::log1p(x);
+}
+__MATH_FUNCTIONS_DECL__ double log1p(double x) {
+  return sycl::log1p(x);
+}
+
+__MATH_FUNCTIONS_DECL__ float max(float x, float y) {
+  return sycl::fmax(x, y);
+}
+__MATH_FUNCTIONS_DECL__ double max(double x, double y) {
+  return sycl::fmax(x, y);
+}
+
+__MATH_FUNCTIONS_DECL__ float min(float x, float y) {
+  return sycl::fmin(x, y);
+}
+__MATH_FUNCTIONS_DECL__ double min(double x, double y) {
+  return sycl::fmin(x, y);
+}
+
+__MATH_FUNCTIONS_DECL__ float pow(float x, float y) {
+  return sycl::pow(x, y);
+}
+__MATH_FUNCTIONS_DECL__ double pow(double x, double y) {
+  return sycl::pow(x, y);
+}
+
+__MATH_FUNCTIONS_DECL__ void sincos(float x, float* sptr, float* cptr) {
+  *sptr = sycl::sin(x);
+  *cptr = sycl::cos(x);
+}
+__MATH_FUNCTIONS_DECL__ void sincos(double x, double* sptr, double* cptr) {
+  *sptr = sycl::sin(x);
+  *cptr = sycl::cos(x);
+}
+
+__MATH_FUNCTIONS_DECL__ float sqrt(float x) {
+  return sycl::sqrt(x);
+}
+__MATH_FUNCTIONS_DECL__ double sqrt(double x) {
+  return sycl::sqrt(x);
 }
 
 __MATH_FUNCTIONS_DECL__ float rsqrt(float x) {
@@ -37,6 +109,27 @@ __MATH_FUNCTIONS_DECL__ float rsqrt(float x) {
 }
 __MATH_FUNCTIONS_DECL__ double rsqrt(double x) {
   return sycl::rsqrt(x);
+}
+
+__MATH_FUNCTIONS_DECL__ float tan(float x) {
+  return sycl::tan(x);
+}
+__MATH_FUNCTIONS_DECL__ double tan(double x) {
+  return sycl::tan(x);
+}
+
+__MATH_FUNCTIONS_DECL__ float tanh(float x) {
+  return sycl::tanh(x);
+}
+__MATH_FUNCTIONS_DECL__ double tanh(double x) {
+  return sycl::tanh(x);
+}
+
+__MATH_FUNCTIONS_DECL__ float normcdf(float x) {
+  return 0.5f * sycl::erfc(-x * static_cast<float>(M_SQRT1_2));
+}
+__MATH_FUNCTIONS_DECL__ double normcdf(double x) {
+  return 0.5 * sycl::erfc(-x * M_SQRT1_2);
 }
 
 // To walk around SYCL compiler optimization on data type promotion.
