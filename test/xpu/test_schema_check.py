f1f2d6aafe33f5a1c544d0fa6fae1852bb709bdd
Silv3S <slawomir.siwek@intel.com>
2026-07-07 08:23:35 +0000

    Use XPUImportCtx

--- test/xpu/test_schema_check.py
+++ test/xpu/test_schema_check.py
@@ -1,14 +1,22 @@
-# Owner(s): ["oncall: jit"]
+# Copyright 2020-2026 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Owner(s): ["module: intel"]
 
 from torch.testing._internal.common_device_type import instantiate_device_type_tests
 from torch.testing._internal.common_utils import run_tests
 
 try:
-    from xpu_test_utils import XPUPatchForImport
+    from xpu_test_utils import XPUImportCtx
 except Exception:
-    from .xpu_test_utils import XPUPatchForImport
+    from .xpu_test_utils import XPUImportCtx
 
-with XPUPatchForImport(False):
+with XPUImportCtx(False):
     from test_schema_check import TestSchemaCheckModeOpInfo
 
 instantiate_device_type_tests(
