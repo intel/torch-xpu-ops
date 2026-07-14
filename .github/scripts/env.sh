0999e065c4bba2d11c639cdde4e4b785ea4d0e1f
zengxian <xiangdong.zeng@intel.com>
2026-07-14 13:26:19 +0800

    test new oneai

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -3,6 +3,7 @@
 XPU_ONEAPI_PATH="${XPU_ONEAPI_PATH:-"/opt/intel/oneapi"}"
 
 source ${XPU_ONEAPI_PATH}/compiler/latest/env/vars.sh
+source ${XPU_ONEAPI_PATH}/mkl/latest/env/vars.sh
 source ${XPU_ONEAPI_PATH}/pti/latest/env/vars.sh
 source ${XPU_ONEAPI_PATH}/umf/latest/env/vars.sh
 source ${XPU_ONEAPI_PATH}/ccl/latest/env/vars.sh
