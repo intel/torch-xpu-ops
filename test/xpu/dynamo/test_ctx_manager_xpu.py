bade7b595e21813efa7cf086d1187c925d8a7111
shangerxin <shangerxin@hotmail.com>
2026-07-21 13:34:29 +0800

    Fix the lint issue

--- test/xpu/dynamo/test_ctx_manager_xpu.py
+++ test/xpu/dynamo/test_ctx_manager_xpu.py
@@ -17,7 +17,6 @@ from torch.testing._internal.common_utils import (
     instantiate_parametrized_tests,
     parametrize,
 )
-from torch.testing._internal.inductor_utils import GPU_TYPE
 
 try:
     from . import test_functions
