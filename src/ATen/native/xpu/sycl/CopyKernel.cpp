a25b79b787d23d2cef0218689e726474f778cd5e
Benedykt Bela <benedykt.bela@intel.com>
2026-07-24 14:14:02 +0000

    Add additional TORCH_CHECK to address review comments.
    Signed-off-by: Benedykt Bela <benedykt.bela@intel.com>

--- src/ATen/native/xpu/sycl/CopyKernel.cpp
+++ src/ATen/native/xpu/sycl/CopyKernel.cpp
@@ -152,6 +152,13 @@ void copy_kernel(TensorIteratorBase& iter) {
       gpu_kernel(iter, CopyScalarFunc<scalar_t>());
     });
   } else if (isBitsType(dtype)) {
+    TORCH_CHECK(
+        iter.dtype(0) == iter.dtype(1),
+        "copy_() does not support casting bits types to different bits types. "
+        "Source dtype is ",
+        iter.dtype(1),
+        ", target dtype is ",
+        iter.dtype(0));
     AT_DISPATCH_BIT_TYPES(dtype, "copy_xpu", [&] {
       gpu_kernel_nocast(iter, CopyScalarFunc<scalar_t>());
     });
