2445a7f9788a795b96d3b2886db32ef9d118428f
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2026-09-11 13:45:14 -0700

    build/dpclang: fix -Wsometimes-uninitialized warnings
    This commit fixes couple places where local function variables were
    not initialized on all execution paths. While there might have been
    no need to do so initially, it's a code maintenance concern as these
    variables get passed to other functions separated by hundereds of lines
    of code which can lead to undefined behavior using unitialized variables
    as code evolves.
    
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- src/ATen/native/xpu/sycl/SegmentReduceKernels.cpp
+++ src/ATen/native/xpu/sycl/SegmentReduceKernels.cpp
@@ -269,7 +269,7 @@ Tensor _segment_reduce_lengths_offsets_xpu_kernel(
               auto* output_data_ptr = output.mutable_data_ptr<scalar_t>();
 
               // initialize starting value
-              scalar_t initial_value;
+              scalar_t initial_value = 0;
               if (initial.has_value()) {
                 initial_value = initial.value().to<scalar_t>();
               } else if (reduction == native::ReductionType::MAX) {
