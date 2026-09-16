b3339d5614eaa3386a118dd7877177611e29e375
Neo Zhang <NA>
2026-09-16 10:59:34 +0800

    Refactor RoiPoolKernels by free function

--- src/ATen/native/xpu/sycl/RoiPoolKernels.cpp
+++ src/ATen/native/xpu/sycl/RoiPoolKernels.cpp
@@ -28,102 +28,75 @@ DISABLE_RETURN_TYPE_WARNING_BEGIN
 namespace at::native::xpu {
 
 template <typename T>
-struct RoiPoolForwardKernel {
-  void operator()(sycl::nd_item<1> item) const {
-    XPU_KERNEL_LOOP(item, index, nthreads_) {
-      int pw = index % pooled_width_;
-      int ph = (index / pooled_width_) % pooled_height_;
-      int c = (index / pooled_width_ / pooled_height_) % channels_;
-      int n = index / pooled_width_ / pooled_height_ / channels_;
+SYCL_EXT_ONEAPI_FUNCTION_PROPERTY((syclexp::nd_range_kernel<1>))
+void roi_pool_forward_kernel_fn(
+    int nthreads,
+    const T* input,
+    const T spatial_scale,
+    int channels,
+    int height,
+    int width,
+    int pooled_height,
+    int pooled_width,
+    const T* rois,
+    T* output,
+    int* argmax) {
+  auto item = syclext::this_work_item::get_nd_item<1>();
+  XPU_KERNEL_LOOP(item, index, nthreads) {
+    int pw = index % pooled_width;
+    int ph = (index / pooled_width) % pooled_height;
+    int c = (index / pooled_width / pooled_height) % channels;
+    int n = index / pooled_width / pooled_height / channels;
 
-      const T* offset_rois = rois_ + n * 5;
-      int roi_batch_ind = offset_rois[0];
-      int roi_start_w = std::round(offset_rois[1] * spatial_scale_);
-      int roi_start_h = std::round(offset_rois[2] * spatial_scale_);
-      int roi_end_w = std::round(offset_rois[3] * spatial_scale_);
-      int roi_end_h = std::round(offset_rois[4] * spatial_scale_);
+    const T* offset_rois = rois + n * 5;
+    int roi_batch_ind = offset_rois[0];
+    int roi_start_w = std::round(offset_rois[1] * spatial_scale);
+    int roi_start_h = std::round(offset_rois[2] * spatial_scale);
+    int roi_end_w = std::round(offset_rois[3] * spatial_scale);
+    int roi_end_h = std::round(offset_rois[4] * spatial_scale);
 
-      // Force malformed ROIs to be 1x1
-      int roi_width = std::max(roi_end_w - roi_start_w + 1, 1);
-      int roi_height = std::max(roi_end_h - roi_start_h + 1, 1);
-      T bin_size_h =
-          static_cast<T>(roi_height) / static_cast<T>(pooled_height_);
-      T bin_size_w = static_cast<T>(roi_width) / static_cast<T>(pooled_width_);
+    // Force malformed ROIs to be 1x1
+    int roi_width = std::max(roi_end_w - roi_start_w + 1, 1);
+    int roi_height = std::max(roi_end_h - roi_start_h + 1, 1);
+    T bin_size_h = static_cast<T>(roi_height) / static_cast<T>(pooled_height);
+    T bin_size_w = static_cast<T>(roi_width) / static_cast<T>(pooled_width);
 
-      using opmath_t = at::opmath_type<T>;
+    using opmath_t = at::opmath_type<T>;
+    int hstart = static_cast<int>(
+        sycl::floor(static_cast<opmath_t>(static_cast<T>(ph) * bin_size_h)));
+    int wstart = static_cast<int>(
+        sycl::floor(static_cast<opmath_t>(static_cast<T>(pw) * bin_size_w)));
+    int hend = static_cast<int>(sycl::ceil(
+        static_cast<opmath_t>(ph + 1) * static_cast<opmath_t>(bin_size_h)));
+    int wend = static_cast<int>(sycl::ceil(
+        static_cast<opmath_t>(pw + 1) * static_cast<opmath_t>(bin_size_w)));
 
-      int hstart = static_cast<int>(
-          sycl::floor(static_cast<opmath_t>(static_cast<T>(ph) * bin_size_h)));
-      int wstart = static_cast<int>(
-          sycl::floor(static_cast<opmath_t>(static_cast<T>(pw) * bin_size_w)));
-      int hend = static_cast<int>(sycl::ceil(
-          static_cast<opmath_t>(ph + 1) * static_cast<opmath_t>(bin_size_h)));
-      int wend = static_cast<int>(sycl::ceil(
-          static_cast<opmath_t>(pw + 1) * static_cast<opmath_t>(bin_size_w)));
+    // Add roi offsets and clip to input boundaries
+    hstart = sycl::clamp(hstart + roi_start_h, 0, height);
+    hend = sycl::clamp(hend + roi_start_h, 0, height);
+    wstart = sycl::clamp(wstart + roi_start_w, 0, width);
+    wend = sycl::clamp(wend + roi_start_w, 0, width);
+    bool is_empty = (hend <= hstart) || (wend <= wstart);
 
-      // Add roi offsets and clip to input boundaries
-      hstart = sycl::clamp(hstart + roi_start_h, 0, height_);
-      hend = sycl::clamp(hend + roi_start_h, 0, height_);
-      wstart = sycl::clamp(wstart + roi_start_w, 0, width_);
-      wend = sycl::clamp(wend + roi_start_w, 0, width_);
-      bool is_empty = (hend <= hstart) || (wend <= wstart);
-
-      // Define an empty pooling region to be zero
-      T maxval = is_empty ? 0.0 : std::numeric_limits<float>::lowest();
-      // If nothing is pooled, argmax = -1 causes nothing to be backprop'd
-      int maxidx = -1;
-      const T* offset_input =
-          input_ + (roi_batch_ind * channels_ + c) * height_ * width_;
-      for (int h = hstart; h < hend; ++h) {
-        for (int w = wstart; w < wend; ++w) {
-          int input_index = h * width_ + w;
-          if (offset_input[input_index] > maxval) {
-            maxval = offset_input[input_index];
-            maxidx = input_index;
-          }
+    // Define an empty pooling region to be zero
+    T maxval = is_empty ? 0.0 : std::numeric_limits<float>::lowest();
+    // If nothing is pooled, argmax = -1 causes nothing to be backprop'd
+    int maxidx = -1;
+    const T* offset_input =
+        input + (roi_batch_ind * channels + c) * height * width;
+    for (int h = hstart; h < hend; ++h) {
+      for (int w = wstart; w < wend; ++w) {
+        int input_index = h * width + w;
+        if (offset_input[input_index] > maxval) {
+          maxval = offset_input[input_index];
+          maxidx = input_index;
         }
       }
-      output_[index] = maxval;
-      argmax_[index] = maxidx;
     }
+    output[index] = maxval;
+    argmax[index] = maxidx;
   }
-  RoiPoolForwardKernel(
-      int nthreads,
-      const T* input,
-      const T spatial_scale,
-      int channels,
-      int height,
-      int width,
-      int pooled_height,
-      int pooled_width,
-      const T* rois,
-      T* output,
-      int* argmax)
-      : nthreads_(nthreads),
-        input_(input),
-        spatial_scale_(spatial_scale),
-        channels_(channels),
-        height_(height),
-        width_(width),
-        pooled_height_(pooled_height),
-        pooled_width_(pooled_width),
-        rois_(rois),
-        output_(output),
-        argmax_(argmax) {}
-
- private:
-  int nthreads_;
-  const T* input_;
-  const T spatial_scale_;
-  int channels_;
-  int height_;
-  int width_;
-  int pooled_height_;
-  int pooled_width_;
-  const T* rois_;
-  T* output_;
-  int* argmax_;
-};
+}
 
 std::tuple<Tensor, Tensor> roi_pool_kernel(
     const at::Tensor& input,
@@ -155,106 +128,71 @@ std::tuple<Tensor, Tensor> roi_pool_kernel(
   auto rois_ = rois.contiguous();
   AT_DISPATCH_FLOATING_TYPES_AND_HALF(
       input.scalar_type(), "roi_pool_forward_kernel_xpu", [&] {
-        auto kfn = RoiPoolForwardKernel<scalar_t>(
-            output_size,
-            input_.const_data_ptr<scalar_t>(),
-            spatial_scale,
-            channels,
-            height,
-            width,
-            pooled_height,
-            pooled_width,
-            rois_.const_data_ptr<scalar_t>(),
-            output.data_ptr<scalar_t>(),
-            argmax.data_ptr<int>());
-        sycl_kernel_submit(
+        constexpr auto kptr = roi_pool_forward_kernel_fn<scalar_t>;
+        sycl_kernel_submit<kptr>(
             global_range * local_range,
             local_range,
             at::xpu::getCurrentSYCLQueue(),
-            kfn);
+            0,
+            output_size,
+            input_.const_data_ptr<scalar_t>(),
+            static_cast<scalar_t>(spatial_scale),
+            static_cast<int>(channels),
+            static_cast<int>(height),
+            static_cast<int>(width),
+            static_cast<int>(pooled_height),
+            static_cast<int>(pooled_width),
+            rois_.const_data_ptr<scalar_t>(),
+            output.data_ptr<scalar_t>(),
+            argmax.data_ptr<int>());
       });
   return std::make_tuple(output, argmax);
 }
 
 template <typename T>
-struct RoiPoolBackwardKernel {
-  void operator()(sycl::nd_item<1> item) const {
-    XPU_KERNEL_LOOP(item, index, nthreads_) {
-      // (n, c, ph, pw) is an element in the pooled output
-      int pw = index % pooled_width_;
-      int ph = (index / pooled_width_) % pooled_height_;
-      int c = (index / pooled_width_ / pooled_height_) % channels_;
-      int n = index / pooled_width_ / pooled_height_ / channels_;
+SYCL_EXT_ONEAPI_FUNCTION_PROPERTY((syclexp::nd_range_kernel<1>))
+void roi_pool_backward_kernel_fn(
+    int nthreads,
+    const T* grad_output,
+    const int* argmax_data,
+    int num_rois,
+    const T spatial_scale,
+    int channels,
+    int height,
+    int width,
+    int pooled_height,
+    int pooled_width,
+    T* grad_input,
+    const T* rois,
+    int n_stride,
+    int c_stride,
+    int h_stride,
+    int w_stride) {
+  auto item = syclext::this_work_item::get_nd_item<1>();
+  XPU_KERNEL_LOOP(item, index, nthreads) {
+    // (n, c, ph, pw) is an element in the pooled output
+    int pw = index % pooled_width;
+    int ph = (index / pooled_width) % pooled_height;
+    int c = (index / pooled_width / pooled_height) % channels;
+    int n = index / pooled_width / pooled_height / channels;
 
-      const T* offset_rois = rois_ + n * 5;
-      int roi_batch_ind = offset_rois[0];
+    const T* offset_rois = rois + n * 5;
+    int roi_batch_ind = offset_rois[0];
 
-      const int output_offset = n * n_stride_ + c * c_stride_;
-      const int* argmax_data_offset =
-          argmax_data_ + (n * channels_ + c) * pooled_height_ * pooled_width_;
-      const int argmax = argmax_data_offset[ph * pooled_width_ + pw];
-      const int offset = (roi_batch_ind * channels_ + c) * height_ * width_;
+    const int output_offset = n * n_stride + c * c_stride;
+    const int* argmax_data_offset =
+        argmax_data + (n * channels + c) * pooled_height * pooled_width;
+    const int argmax = argmax_data_offset[ph * pooled_width + pw];
+    const int offset = (roi_batch_ind * channels + c) * height * width;
 
-      if (argmax != -1) {
-        atomicAdd(
-            (sycl_global_ptr<T>)(grad_input_ + offset + argmax),
-            static_cast<T>(
-                grad_output_[output_offset + ph * h_stride_ + pw * w_stride_]));
-      }
+    if (argmax != -1) {
+      atomicAdd(
+          (sycl_global_ptr<T>)(grad_input + offset + argmax),
+          static_cast<T>(
+              grad_output[output_offset + ph * h_stride + pw * w_stride]));
     }
   }
-  RoiPoolBackwardKernel(
-      int nthreads,
-      const T* grad_output,
-      const int* argmax_data,
-      int num_rois,
-      const T spatial_scale,
-      int channels,
-      int height,
-      int width,
-      int pooled_height,
-      int pooled_width,
-      T* grad_input,
-      const T* rois,
-      int n_stride,
-      int c_stride,
-      int h_stride,
-      int w_stride)
-      : nthreads_(nthreads),
-        grad_output_(grad_output),
-        argmax_data_(argmax_data),
-        num_rois_(num_rois),
-        spatial_scale_(spatial_scale),
-        channels_(channels),
-        height_(height),
-        width_(width),
-        pooled_height_(pooled_height),
-        pooled_width_(pooled_width),
-        grad_input_(grad_input),
-        rois_(rois),
-        n_stride_(n_stride),
-        c_stride_(c_stride),
-        h_stride_(h_stride),
-        w_stride_(w_stride) {}
-
- private:
-  int nthreads_;
-  const T* grad_output_;
-  const int* argmax_data_;
-  int num_rois_;
-  const T spatial_scale_;
-  int channels_;
-  int height_;
-  int width_;
-  int pooled_height_;
-  int pooled_width_;
-  T* grad_input_;
-  const T* rois_;
-  int n_stride_;
-  int c_stride_;
-  int h_stride_;
-  int w_stride_;
-};
+}
 
 Tensor roi_pool_backward_kernel(
     const at::Tensor& grad,
@@ -288,28 +226,28 @@ Tensor roi_pool_backward_kernel(
   auto rois_ = rois.contiguous();
   AT_DISPATCH_FLOATING_TYPES_AND_HALF(
       grad.scalar_type(), "roi_pool_backward_kernel_xpu", [&] {
-        auto kfn = RoiPoolBackwardKernel<scalar_t>(
-            grad.numel(),
+        constexpr auto kptr = roi_pool_backward_kernel_fn<scalar_t>;
+        sycl_kernel_submit<kptr>(
+            global_range * local_range,
+            local_range,
+            at::xpu::getCurrentSYCLQueue(),
+            0,
+            static_cast<int>(grad.numel()),
             grad.const_data_ptr<scalar_t>(),
             argmax_.const_data_ptr<int>(),
-            num_rois,
-            spatial_scale,
-            channels,
-            height,
-            width,
-            pooled_height,
-            pooled_width,
+            static_cast<int>(num_rois),
+            static_cast<scalar_t>(spatial_scale),
+            static_cast<int>(channels),
+            static_cast<int>(height),
+            static_cast<int>(width),
+            static_cast<int>(pooled_height),
+            static_cast<int>(pooled_width),
             grad_input.data_ptr<scalar_t>(),
             rois_.const_data_ptr<scalar_t>(),
             n_stride,
             c_stride,
             h_stride,
             w_stride);
-        sycl_kernel_submit(
-            global_range * local_range,
-            local_range,
-            at::xpu::getCurrentSYCLQueue(),
-            kfn);
       });
   return grad_input;
 }
