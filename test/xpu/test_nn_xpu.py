4c11b8ba654fa0af812a3ba9ece19d6e8736b691
reginaldalfret <reginaldalfret@gmail.com>
2026-09-25 20:54:48 +0530

    [UT] Align grid_sample error assertion with upstream
    Remove obsolete 5D bicubic grid_sample assertion in test_grid_sample_error_checking, aligning with upstream PyTorch commit 20fb7bd04e0b24cd4299fa34d767388daa6a26a4.
    
    Signed-off-by: reginaldalfret <reginaldalfret@gmail.com>

--- test/xpu/test_nn_xpu.py
+++ test/xpu/test_nn_xpu.py
@@ -7141,14 +7141,6 @@ tensor(..., device='meta', size=(1,), requires_grad=True)""",
         ):
             F.grid_sample(torch.empty(1, 1, 0, 2), grid, align_corners=False)
 
-        with self.assertRaisesRegex(
-            (RuntimeError, NotImplementedError),
-            "(bicubic interpolation only supports 4D input|bicubic interpolation with 5D input is not implemented)",
-        ):
-            F.grid_sample(
-                torch.empty(1, 1, 2, 2, 2), torch.empty(1, 1, 1, 1, 3), mode="bicubic"
-            )
-
         if TEST_GPU:
             with self.assertRaisesRegex(
                 RuntimeError, "Expected all tensors to be on the same device"
